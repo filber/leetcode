ca10acdbd9b80a7fe152a2204b34a418a6d05e34
filber <filber@foxmail.com>
2021-07-25 17:10:31 +0800

    121 Best Time to buy and sell stock

--- dynamic_programming/src/_121_BestTimeToBuyAndSellStock.java
+++ dynamic_programming/src/_121_BestTimeToBuyAndSellStock.java
@@ -1,62 +1,43 @@
 public class _121_BestTimeToBuyAndSellStock {
 
-  // Bad, O(n^2)
-  public int maxProfit(int[] prices) {
-    if (prices == null || prices.length < 2) {
-      return 0;
+    public static int maxProfitDP(int[] prices) {
+        if (prices.length < 2) {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        int[] dp = new int[prices.length];
+        dp[0] = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+          maxProfit = Math.max(maxProfit, prices[i] - dp[i-1]);
+          dp[i] = Math.min(dp[i-1],prices[i]);
+        }
+
+        return maxProfit;
     }
 
-    int profit = 0;
-    for (int start = 0; start < prices.length; start++) {
-      for (int end = start + 1; end < prices.length; end++) {
-        profit = Math.max(profit, prices[end] - prices[start]);
-      }
+
+    public int maxProfitDPImproved(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(min, prices[i - 1]);
+            max = Math.max(max, prices[i] - min);
+        }
+
+        return max;
     }
 
-    return profit;
-  }
+    public static void main(String[] args) {
+        _121_BestTimeToBuyAndSellStock instance = new _121_BestTimeToBuyAndSellStock();
 
-  public int maxProfitDP(int[] prices) {
-    if (prices == null || prices.length < 2) {
-      return 0;
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        int maxProfit = maxProfitDP(prices);
+        System.out.println(maxProfit);
     }
-
-    int[] lowest = new int[prices.length];
-    int p = prices[0];
-    for (int i = 0; i < prices.length; i++) {
-      p = Math.min(p,prices[i]);
-      lowest[i] = p;
-    }
-
-    int maxProfit = 0;
-    for (int i = 1; i < prices.length; i++) {
-      int previousLowest = lowest[i - 1];
-      maxProfit = Math.max(maxProfit, prices[i] - previousLowest);
-    }
-
-    return maxProfit;
-  }
-
-  public int maxProfitDPImproved(int[] prices) {
-    if (prices == null || prices.length < 2) {
-      return 0;
-    }
-
-    int min=Integer.MAX_VALUE;
-    int max = 0;
-    for (int i = 1; i < prices.length; i++) {
-      min = Math.min(min, prices[i - 1]);
-      max = Math.max(max,prices[i] - min);
-    }
-
-    return max;
-  }
-
-  public static void main(String[] args) {
-    _121_BestTimeToBuyAndSellStock instance = new _121_BestTimeToBuyAndSellStock();
-
-    int[] prices = new int[] {7, 1, 5, 3, 6, 4};
-    int maxProfit = instance.maxProfitDPImproved(prices);
-    assert maxProfit == 5;
-  }
 }
