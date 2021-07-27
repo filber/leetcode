0b9ea9030f0965179e7302866b72bcdb53b1e164
filber <filber@foxmail.com>
2021-07-27 18:48:59 +0800

    70 Climbing Stairs

--- dynamic_programming/src/_70_ClimbingStairs.java
+++ dynamic_programming/src/_70_ClimbingStairs.java
@@ -1,22 +1,27 @@
 public class _70_ClimbingStairs {
 
-  // how many distinct WAYS can you climb to the top?
-  public int climbStairs(int n) {
-    int[] dp = new int[n + 1];
-    dp[0]= 1;
-    dp[1]= 1;
-    for (int i = 2; i <= n ; i++) {
-      // add up different ways!
-      // don't need max or min when calculate WAYS
-      dp[i] = dp[i - 2] + dp[i - 1];
+  public static int climbStairs(int n) {
+    if (n==1) {
+      return 1;
+    } else if (n==2) {
+      return 2;
     }
-    return dp[n];
+
+    int previousTwo = 1;
+    int previousOne = 2;
+    int result = previousOne + previousTwo;
+    for (int i = 3; i <= n ; i++) {
+      result = previousOne + previousTwo;
+      previousTwo = previousOne;
+      previousOne = result;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
-    _70_ClimbingStairs instance = new _70_ClimbingStairs();
-    int result = instance.climbStairs(4);
-    assert result == 3;
+    System.out.println(climbStairs(2));
+    System.out.println(climbStairs(3));
+    System.out.println(climbStairs(4));
   }
 
 }
