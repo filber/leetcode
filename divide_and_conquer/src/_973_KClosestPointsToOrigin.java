1dc3a8c4d6dfa6d5ba2aa7b816b8347c036fc610
filber <filber@foxmail.com>
2021-07-28 20:28:13 +0800

    973 K closest points to origin

--- divide_and_conquer/src/_973_KClosestPointsToOrigin.java
+++ divide_and_conquer/src/_973_KClosestPointsToOrigin.java
@@ -1,40 +1,40 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class _973_KClosestPointsToOrigin {
-
-    public static class Item implements Comparable<Item> {
-        int[] point;
-        int distance;
+    public static class PointComparator implements Comparator<int[]> {
 
         @Override
-        public int compareTo(Item o) {
-            return Integer.compare(distance, o.distance);
+        public int compare(int[] point1, int[] point2) {
+            return Integer.compare(point1[0] * point1[0] + point1[1] * point1[1],
+                    point2[0] * point2[0] + point2[1] * point2[1]);
         }
     }
-    public static int[][] kClosest(int[][] points, int K) {
-        PriorityQueue<Item> priorityQueue = new PriorityQueue<>(points.length);
+
+    public static int[][] kClosest(int[][] points, int k) {
+        int[][] result = new int[k][2];
+        PriorityQueue<int[]> queue = new PriorityQueue<>(k,new PointComparator());
         for (int i = 0; i < points.length; i++) {
-            Item item = new Item();
-            item.distance = points[i][0] * points[i][0] +
-                            points[i][1] * points[i][1];
-            item.point = points[i];
-            priorityQueue.add(item);
+            queue.add(points[i]);
         }
 
-        int[][] results = new int[K][2];
-        for (int i = 0; i < K; i++) {
-            Item item = priorityQueue.poll();
-            results[i] = item.point;
+        for (int i = 0; i < k; i++) {
+            result[i] = queue.poll();
         }
-        return results;
+        return result;
     }
 
     public static void main(String[] args) {
 //        Input: points = [[1,3],[-2,2]], K = 1
 //        Output: [[-2,2]]
-        kClosest(new int[][]{{1, 3}, {-2, 2}}, 1);
+//        kClosest(new int[][]{{1, 3}, {-2, 2}}, 1);
+
 //        Input: points = [[3,3],[5,-1],[-2,4]], K = 2
 //        Output: [[3,3],[-2,4]]
-        kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2);
+//        kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2);
+
+//        Input: points = [[-5,4],[-6,-5],[4,6]], K = 2
+//        Output: [[-5,4],[4,6]]
+        kClosest(new int[][]{{-5, 4}, {-6, -5}, {4, 6}}, 2);
     }
 }
