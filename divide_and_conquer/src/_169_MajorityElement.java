caadf1ac5ea8acc089fa56813f2386d950b84405
filber <filber@foxmail.com>
2021-07-28 13:03:46 +0800

    169 Majority Element

--- divide_and_conquer/src/_169_MajorityElement.java
+++ divide_and_conquer/src/_169_MajorityElement.java
@@ -1,33 +1,29 @@
 public class _169_MajorityElement {
 
-    // solve the problem in linear time and in O(1) space
     public static int majorityElement(int[] nums) {
-        int item = 0;
-        int counter = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (counter==0) {
-                item = nums[i];
-                counter++;
+        int result = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count==0) {
+                result = nums[i];
+                count++;
+                continue;
+            }
+            if (nums[i] == result) {
+                count++;
             } else {
-                if (nums[i] == item) {
-                    counter++;
-                } else {
-                    counter--;
-                }
+                count--;
             }
         }
-        return item;
+
+        return result;
     }
 
     public static void main(String[] args) {
-//        Input: nums = [3,2,3]
-//        Output: 3
-//        int[] array1 = new int[]{3, 2, 3};
-//        majorityElement(array1);
+        // 3
+        System.out.println(majorityElement(new int[]{3, 2, 3}));
 
-//        Input: nums = [2,2,1,1,1,2,2]
-//        Output: 2
-        int[] array2 = new int[]{2, 2, 1, 1, 1, 2, 2};
-        majorityElement(array2);
+        // 2
+        System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
     }
 }
