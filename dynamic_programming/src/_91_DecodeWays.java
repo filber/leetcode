5fa96b2af1d4ec038c5b171cbe423009f93d9e2e
filber <filber@foxmail.com>
2021-07-28 12:43:29 +0800

    91 Decode Ways

--- dynamic_programming/src/_91_DecodeWays.java
+++ dynamic_programming/src/_91_DecodeWays.java
@@ -1,35 +1,74 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class _91_DecodeWays {
 
-  public int numDecodings(String s) {
-    final int length = s.length();
-    if (length == 0) {
-      return 0;
+  public static int numDecodings(String s) {
+    Set<String> set = new HashSet<>();
+    for (int i = 1; i <= 26; i++) {
+      set.add(String.valueOf(i));
     }
-    if (s.charAt(0) == '0') {
+
+    // initialize DP
+    int[] dp = new int[s.length()];
+    String self = s.substring(0, 1);
+    if (set.contains(self)) {
+      dp[0] = 1;
+    } else {
       return 0;
     }
 
-    int[] dp = new int[length + 1];
-    dp[0] = 1;
+    if (s.length()<2) {
+      return dp[0];
+    }
 
-    for (int i = 0; i < length; i++) {
-      // match by single character, ways don't increase! remain dp[i]
-      // 0 means we can't decode this way
-      dp[i + 1] = s.charAt(i) == '0' ? 0 : dp[i];
-      // match by double character
-      if (i > 0 && (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) <= '6'))) {
-        // ways increase by dp[i-1]
-        dp[i + 1] += dp[i - 1];
+    self = s.substring(1, 2);
+    String pre = s.substring(0, 2);
+    if (set.contains(self) && set.contains(pre)) {
+      dp[1] = 2;
+    } else if (set.contains(self)) {
+      dp[1] = 1;
+    } else if (set.contains(pre)) {
+      dp[1] = 1;
+    } else {
+      dp[1] = 0;
+    }
+
+    for (int i = 2; i < dp.length; i++) {
+      self = s.substring(i, i + 1);
+      pre = s.substring(i - 1, i + 1);
+      int ways = 0;
+      if (set.contains(self) && set.contains(pre)) {
+        ways = dp[i - 1] + dp[i - 2];
+      } else if (set.contains(self)) {
+        ways = dp[i-1];
+      } else if (set.contains(pre)) {
+        ways = dp[i-2];
+      } else {
+        return 0;
       }
+      dp[i] = ways;
     }
 
-    return dp[length];
+    return dp[dp.length - 1];
   }
 
   public static void main(String[] args) {
-    _91_DecodeWays instance = new _91_DecodeWays();
-    int ways = instance.numDecodings("10");
-    assert ways == 3;
+
+    // 0
+    System.out.println(numDecodings("06"));
+
+    // 0
+    System.out.println(numDecodings("0"));
+
+    // 3
+    System.out.println(numDecodings("226"));
+
+    // 2
+    System.out.println(numDecodings("12"));
+
+    // 2
+    System.out.println(numDecodings("11106"));
   }
 
 
