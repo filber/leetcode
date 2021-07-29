7efa80dbd3d04e83e4325e0b25d7febb9aacd96c
filber <filber@foxmail.com>
2021-07-29 17:12:41 +0800

    162 Find Peak Element

--- binary_search/src/_162_Find_Peak_Element.java
+++ binary_search/src/_162_Find_Peak_Element.java
@@ -1,47 +1,37 @@
 public class _162_Find_Peak_Element {
-  public int findPeakElement(int[] nums) {
+  public static int findPeakElement(int[] nums) {
     return findPeak(nums, 0, nums.length - 1);
   }
 
-  public int findPeak(int[] nums, int startIndex, int endIndex) {
-    if (endIndex == 0) {
-      return endIndex;
-    } else if (startIndex == (nums.length - 1)) {
-      return startIndex;
-    } else if (startIndex == endIndex) {
+  public static int findPeak(int[] nums, int startIndex, int endIndex) {
+    if (startIndex == endIndex) {
       return startIndex;
     }
 
-    int mid = (startIndex + endIndex) / 2;
-    boolean gtLeft = false;
-    if (mid == 0) {
-      gtLeft = true;
-    } else if (nums[mid] > nums[mid - 1]) {
-      gtLeft = true;
-    }
-
-    boolean gtRight = false;
-    if (mid == nums.length - 1) {
-      gtRight = true;
-    } else if (nums[mid] > nums[mid + 1]) {
-      gtRight = true;
-    }
-
-    if (gtLeft && gtRight) {
-      return mid;
-    } else if (gtLeft) {
-      // search in the right part
-      return findPeak(nums, mid + 1, endIndex);
+    int middleIndex = (startIndex + endIndex) / 2;
+    int middleValue = nums[middleIndex];
+    int middleLeft = middleIndex == 0 ? Integer.MIN_VALUE : nums[middleIndex - 1];
+    int middleRight = middleIndex == nums.length - 1 ? Integer.MIN_VALUE : nums[middleIndex + 1];
+    if ((middleLeft > middleValue) && middleIndex != 0) {
+      return findPeak(nums, startIndex, middleIndex - 1);
+    } else if ((middleRight > middleValue) && middleIndex != (nums.length - 1)) {
+      return findPeak(nums, middleIndex + 1, endIndex);
     } else {
-      // search in the left part
-      return findPeak(nums, startIndex, mid - 1);
+      return middleIndex;
     }
   }
 
   public static void main(String[] args) {
-    _162_Find_Peak_Element instance = new _162_Find_Peak_Element();
-    int[] nums = {1,2,1,3,5,6,4};
-    int result = instance.findPeakElement(nums);
-    assert result == 5;
+    // 2
+    System.out.println(findPeakElement(new int[]{1, 2, 3, 1}));
+
+    // 1 or 5
+    System.out.println(findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4}));
+
+    // 0
+    System.out.println(findPeakElement(new int[]{-2147483648}));
+
+    // 1
+    System.out.println(findPeakElement(new int[]{1,2}));
   }
 }
