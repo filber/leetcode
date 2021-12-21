1284df8acb38886f87dc2e0499c242482c7ad9f2
filber <filber@foxmail.com>
2021-12-21 19:51:04 +0800

    215. Kth Largest Element in an Array

--- binary_search/src/_215_KthLargestElementInAnArray.java
+++ binary_search/src/_215_KthLargestElementInAnArray.java
@@ -1,22 +1,24 @@
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+// TIPS: Using Priority Queue
 public class _215_KthLargestElementInAnArray {
 
-    public static class ElementComparator implements Comparator<Integer> {
+    public static class KComparator implements Comparator<Integer> {
+
         @Override
-        public int compare(Integer first, Integer second) {
-            return Integer.compare(second,first);
+        public int compare(Integer o1, Integer o2) {
+            return o2.compareTo(o1);
         }
     }
 
     public static int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k, new ElementComparator());
-        for (int num : nums) {
-            priorityQueue.add(num);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k, new KComparator());
+        for (int i = 0; i < nums.length; i++) {
+            priorityQueue.add(nums[i]);
         }
-        int result = 0;
 
+        int result = priorityQueue.peek();
         for (int i = 0; i < k; i++) {
             result = priorityQueue.poll();
         }
@@ -24,12 +26,12 @@ public class _215_KthLargestElementInAnArray {
     }
 
     public static void main(String[] args) {
-//        Input: nums = [3,2,1,5,6,4], k = 2
-//        Output: 5
-        System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        // Input: nums = [3,2,1,5,6,4], k = 2
+        // Output: 5
+        int r1 = findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2);
 
-//        Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
-//        Output: 4
+        // Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+        // Output: 4
         System.out.println(findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
 
     }
