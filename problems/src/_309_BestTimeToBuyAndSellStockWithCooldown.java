97d5dc1b84fd78a774c3b1d19b2e633188168bd5
filber <filber@foxmail.com>
2022-01-06 14:35:12 +0800

    309. Best Time to Buy and Sell Stock with Cooldown

---
+++ problems/src/_309_BestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
+//[Dynamic Programming]
+public class _309_BestTimeToBuyAndSellStockWithCooldown {
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n==1) {
+            return 0;
+        }
+
+        int[] buy = new int[n];
+        buy[0] = -prices[0];
+        int[] sell = new int[n];
+        int[] cool = new int[n];
+        for (int i = 1; i < n; i++) {
+            sell[i] = buy[i - 1] + prices[i];
+            cool[i] = Math.max(sell[i - 1], cool[i - 1]);
+            buy[i] = Math.max(cool[i - 1] - prices[i], buy[i - 1]);
+        }
+
+        return Math.max(sell[n - 1], cool[n - 1]);
+    }
+
+    public static void main(String[] args) {
+        int m1 = maxProfit(new int[]{1, 2, 3, 0, 2});
+        int m2 = maxProfit(new int[]{1});
+    }
+}
