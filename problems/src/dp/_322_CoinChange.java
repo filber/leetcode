882760d9480980fc81b44e760d5ae4320fdbe899
filber <filber@foxmail.com>
2022-01-09 21:46:43 +0800

    Review

--- problems/src/dp/_322_CoinChange.java
+++ problems/src/dp/_322_CoinChange.java
@@ -2,34 +2,53 @@ package dp;
 
 //https://leetcode.com/problems/coin-change/
 
+import java.util.Arrays;
+
 public class _322_CoinChange {
     // Constraints:
     // 1 <= coins.length <= 12
     // 1 <= coins[i] <= 2^31 - 1
     // 0 <= amount <= 10^4
+
     public static int coinChange(int[] coins, int amount) {
         if (amount == 0) {
             return 0;
         }
-//        int n = coins.length;
-//        int[] dp = new int[amount + 1];
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 1; j <= amount; j++) {
-//                int w = coins[i];
-//                int v = coins[i];
-//                int notPut = dp[j];
-//                int put = 0;
-//                if (j >= w) {
-//                    put = dp[j - w] + v;
-//                }
-//                dp[j] = Math.max(notPut, put);
-//            }
-//        }
-//        if (dp[amount] != amount) {
-//            return -1;
-//        }
 
-//        return dp[amount];
+        int[] amtDP = new int[amount + 1];
+        Arrays.fill(amtDP,-amount);
+        amtDP[0] = 0;
+        int[] cntDP = new int[amount + 1];
+        Arrays.fill(cntDP,amount);
+        cntDP[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                int coin = coins[j];
+                if (i >= coin) {
+                    cntDP[i] = Math.min(cntDP[i], cntDP[i - coin] + 1);
+                }
+
+                int put = 0;
+                if (i >= coin) {
+                    put = amtDP[i - coin] + coin;
+                }
+                amtDP[i] = Math.max(amtDP[i], put);
+            }
+        }
+
+        if (amtDP[amount] == amount) {
+            return cntDP[amount];
+        } else {
+            return -1;
+        }
+    }
+
+    public static int coinChangeDFS(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
         int[] dp = new int[10001];
         int cnt = dfs(coins, dp, amount);
         if (cnt>100000) {
@@ -63,5 +82,6 @@ public class _322_CoinChange {
         int c1 = coinChange(new int[]{1, 2, 5}, 11);
         int c2 = coinChange(new int[]{2}, 3);
         int c3 = coinChange(new int[]{1}, 0);
+        int c4 = coinChange(new int[]{186,419,83,408}, 6249);
     }
 }
