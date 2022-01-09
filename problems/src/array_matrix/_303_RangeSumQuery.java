c42fbf1a215d7f8ac55098ba24e0b0f24f5dabd7
filber <filber@foxmail.com>
2022-01-09 15:57:08 +0800

    Review

--- problems/src/array_matrix/_303_RangeSumQuery.java
+++ problems/src/array_matrix/_303_RangeSumQuery.java
@@ -1,45 +1,12 @@
+package array_matrix;
+
+// https://leetcode.com/problems/range-sum-query-immutable/
 public class _303_RangeSumQuery {
 
 //  Constraints:
 //      0 <= nums.length <= 10^4
 //      -10^5 <= nums[i] <= 10^5
 //      0 <= i <= j < nums.length
-//  At most 10^4 calls will be made to sumRange.
-
-//  public static class NumArray {
-//    /**
-//     * Your NumArray object will be instantiated and called as such:
-//     * NumArray obj = new NumArray(nums);
-//     * int param_1 = obj.sumRange(i,j);
-//     */
-//
-//    int[][] sum = null;
-//
-//    public NumArray(int[] nums) {
-//      if (nums.length == 0) {
-//        return;
-//      }
-//
-//      sum = new int[nums.length][nums.length];
-//
-//      for (int i = 0; i < nums.length; i++) {
-//        for (int j = i; j < nums.length; j++) {
-//          if (i == j) {
-//            sum[i][j] = nums[i];
-//          } else {
-//            sum[i][j] = nums[j] + sum[i][j - 1];
-//          }
-//        }
-//      }
-//    }
-//
-//    public int sumRange(int i, int j) {
-//      if (sum == null) {
-//        return 0;
-//      }
-//      return sum[i][j];
-//    }
-//  }
 
   public static class NumArray {
 
