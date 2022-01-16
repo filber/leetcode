4fcabf8ad62f82db820de6707aed5f6cf8f418da
filber <filber@foxmail.com>
2022-01-16 15:41:29 +0800

    Review

--- problems/src/array_matrix/_1010_PairsOfSongsWithTotalDurationsDivisibleBy60.java
+++ problems/src/array_matrix/_1010_PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -1,29 +1,31 @@
+package array_matrix;
+
+// https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
 //TOPICS:
 //[Hash Table]
 public class _1010_PairsOfSongsWithTotalDurationsDivisibleBy60 {
     public static int numPairsDivisibleBy60(int[] time) {
-        if (time.length<=1) {
-            return 0;
+        int[] bucket = new int[60];
+        int cnt = 0;
+        for (int t : time) {
+            bucket[t % 60]++;
         }
-        int[] remainder = new int[60];
-        // convert to remainder
-        for (int i = 0; i < time.length; i++) {
-            remainder[time[i] % 60]++;
-        }
+        cnt += bucket[0]*(bucket[0]-1)/2;
+        cnt += bucket[30]*(bucket[30]-1)/2;
 
-        int pairCnt = 0;
-        pairCnt += remainder[0] * (remainder[0] - 1) / 2;
-        pairCnt += remainder[30] * (remainder[30] - 1) / 2;
         for (int i = 1; i <= 29; i++) {
-            pairCnt += remainder[i] * remainder[60 - i];
+            cnt += bucket[i] * bucket[60 - i];
         }
 
-        return pairCnt;
+        return cnt;
     }
 
     public static void main(String[] args) {
+        // 3 [30-150],[20-100],[20-40]
         int n1 = numPairsDivisibleBy60(new int[]{30, 20, 150, 100, 40});
+        // 3
         int n2 = numPairsDivisibleBy60(new int[]{60, 60, 60});
+        // 4
         int n3 = numPairsDivisibleBy60(new int[]{60, 60, 60, 1, 59});
     }
 }
