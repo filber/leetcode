3706c826968a1aaaf3ad2469b52d5a0c8e814d99
filber <filber@foxmail.com>
2022-01-22 11:36:49 +0800

    Review

--- problems/src/math/_1720_DecodeXORedArray.java
+++ problems/src/math/_1720_DecodeXORedArray.java
@@ -1,18 +1,22 @@
-// TOPICS:
-// [Bit Manipulation], [Array]
+package math;
+
+// https://leetcode.com/problems/decode-xored-array/
+
 public class _1720_DecodeXORedArray {
 
     public static int[] decode(int[] encoded, int first) {
-        int[] arr = new int[encoded.length + 1];
+        int n = encoded.length + 1;
+        int[] arr = new int[n];
         arr[0] = first;
-        for (int i = 0; i < arr.length-1; i++) {
-            arr[i + 1] = arr[i] ^ encoded[i];
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i - 1];
         }
+
         return arr;
     }
 
     public static void main(String[] args) {
-        int[] a1 = decode(new int[]{1,0}, 1);
+        int[] a1 = decode(new int[]{1, 0}, 1);
         int[] a2 = decode(new int[]{1, 2, 3}, 1);
         int[] a3 = decode(new int[]{6, 2, 7, 3}, 4);
     }
