4d745f2097a5036efa068cc8526c08b91408c15e
filber <filber@foxmail.com>
2022-01-23 11:44:49 +0800

    Review

--- problems/src/dfs/_2044_CountNumberOfMaximumBitwiseORSubsets.java
+++ problems/src/dfs/_2044_CountNumberOfMaximumBitwiseORSubsets.java
@@ -1,46 +1,46 @@
-// TOPICS:
-// [Bit Manipulation], [Backtracking], [DFS]
+package dfs;
 
-// TIPS:
-// dfs(int[] nums, int tempResult, int i)
+// https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
 public class _2044_CountNumberOfMaximumBitwiseORSubsets {
-    private int max = 0;
+
+    private int maxOr = 0;
     private int maxCnt = 0;
 
     public int countMaxOrSubsets(int[] nums) {
-        if (nums.length == 1) {
-            return 1;
-        }
         dfs(nums, 0, 0);
         return maxCnt;
     }
 
-    public void dfs(int[] nums, int bitwiseOr, int i) {
-        if (i == nums.length) {
-            if (bitwiseOr > max) {
+    private void dfs(int[] nums, int or, int pos) {
+        if (pos == nums.length) {
+            if (or > maxOr) {
+                maxOr = or;
                 maxCnt = 1;
-                max = bitwiseOr;
-            } else if (bitwiseOr == max) {
+            } else if (or == maxOr) {
                 maxCnt++;
             }
             return;
         }
 
-        // include i
-        dfs(nums, bitwiseOr | nums[i], i + 1);
-        // exclude i
-        dfs(nums, bitwiseOr, i + 1);
+        // use nums[pos]
+        dfs(nums, or | nums[pos], pos + 1);
+
+        // not use nums[pos]
+        dfs(nums, or, pos + 1);
     }
 
     public static void main(String[] args) {
+        // 7
         int c1 = new _2044_CountNumberOfMaximumBitwiseORSubsets().countMaxOrSubsets(
-                new int[]{2,2,2}
+                new int[]{2, 2, 2}
         );
+        // 2
         int c2 = new _2044_CountNumberOfMaximumBitwiseORSubsets().countMaxOrSubsets(
-                new int[]{3,1}
+                new int[]{3, 1}
         );
+        // 6
         int c3 = new _2044_CountNumberOfMaximumBitwiseORSubsets().countMaxOrSubsets(
-                new int[]{3,2,1,5}
+                new int[]{3, 2, 1, 5}
         );
     }
 
