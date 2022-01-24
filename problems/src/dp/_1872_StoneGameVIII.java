cc638292e0be6a3a071292a3aa85e8da48274b67
filber <filber@foxmail.com>
2022-01-24 15:43:21 +0800

    1872. Stone Game VIII

--- problems/src/dp/_1872_StoneGameVIII.java
+++ problems/src/dp/_1872_StoneGameVIII.java
@@ -3,22 +3,32 @@ package dp;
 // https://leetcode.com/problems/stone-game-viii/
 public class _1872_StoneGameVIII {
 
+//    There are n stones arranged in a row.
+//    On each player's turn, while the number of stones is more than one,
+//    they will do the following:
+//    Choose an integer x > 1, and remove the leftmost x stones from the row.
+//    Add the sum of the removed stones' values to the player's score.
+//    Place a new stone, whose value is equal to that sum, on the left side of the row.
+
     public static int stoneGameVIII(int[] stones) {
         int n = stones.length;
-        int[] dp = new int[n];
-        int[] prefixSum = new int[n];
-        prefixSum[0] = stones[0];
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + stones[i];
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + stones[i];
         }
 
-        dp[n - 1] = prefixSum[n - 1];
+        // dp[i] represent maximum value of {Alice-Bob} when x in [i,n-1]
+        // that is dp[i] = max(ps[i]-dp[i+1],ps[i+1]-dp[i+2],....,ps[n-2] - dp[n-1])
+        // since  dp[i+1] = max(ps[i+1]-dp[i+2],ps[i+2]-dp[i+3],....,ps[n-2] - dp[n-1])
+        // then dp[i] = max(ps[i] - dp[i+1],dp[i+1])
+        int[] dp = new int[n + 1];
 
-        //dp[i], max relative score of choosing [i,n-1]
-        for (int i = n - 2; i > 0; --i) {
+        // take all stones
+        dp[n] = prefixSum[n];
+        for (int i = n - 1; i >= 2; i--) {
             dp[i] = Math.max(dp[i + 1], prefixSum[i] - dp[i + 1]);
         }
-        return dp[1];
+        return dp[2];
     }
 
     public static void main(String[] args) {
