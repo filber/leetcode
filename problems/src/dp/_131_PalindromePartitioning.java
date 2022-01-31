740737c60cbc8c2d44216d360dd12a42603457e9
filber <filber@foxmail.com>
2022-01-31 18:18:21 +0800

    Review

--- problems/src/dp/_131_PalindromePartitioning.java
+++ problems/src/dp/_131_PalindromePartitioning.java
@@ -9,50 +9,59 @@ import java.util.List;
 
 public class _131_PalindromePartitioning {
 
-    private boolean[][] dp;
-    private List<List<String>> result = new ArrayList<>();
-    private int n;
-    private String s;
+    boolean[][] dp;
+
+    List<List<String>> ans = new ArrayList<>();
+
     public List<List<String>> partition(String s) {
-        this.s = s;
-        n = s.length();
-        constructDP();
-        dfs(0,new LinkedList<>());
-        return result;
-    }
-
-    private void constructDP() {
+        int n = s.length();
         dp = new boolean[n][n];
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i < n; i++) {
-                int j = len - 1 + i;
-                if (j>=n) {
-                    break;
-                }
-                if (len == 1) {
-                    dp[i][i] = true;
-                } else if (len == 2) {
-                    dp[i][j] = s.charAt(i) == s.charAt(j);
-                } else {
-                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i+1][j-1];
-                }
-            }
-        }
+        char[] chars = s.toCharArray();
+        initDP(chars);
+
+        List<String> list = new ArrayList<>();
+        backtracking(chars, list, 0);
+        return ans;
     }
 
-    private void dfs(int start,List<String> list) {
-        if (start == n) {
-            result.add(new ArrayList<>(list));
+    private void backtracking(char[] chars, List<String> list, int i) {
+        if (i == chars.length) {
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for (int i = start; i < n; i++) {
-            if (dp[start][i]) {
-                list.add(s.substring(start, i + 1));
-                dfs(i + 1, list);
+
+        for (int j = i; j < chars.length; j++) {
+            if (dp[i][j]) {
+                // set state
+                list.add(new String(chars, i, j - i + 1));
+                backtracking(chars,list,j+1);
+                // reverse state
                 list.remove(list.size() - 1);
             }
         }
     }
+
+    private void initDP(char[] chars) {
+        int n = chars.length;
+        // len = 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        // len = 2
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][i + 1] = chars[i] == chars[i + 1];
+        }
+        // len = [3,n]
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i < n && i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (chars[i] == chars[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         List<List<String>> p1 = new _131_PalindromePartitioning().partition("aab");
