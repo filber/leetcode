0eee05d39d8bdd13937f0025a331bef4b16a14f7
filber <filber@foxmail.com>
2022-01-31 13:48:42 +0800

    4. Median of Two Sorted Arrays

--- problems/src/array_matrix/_4_MedianOfTwoSortedArrays.java
+++ problems/src/array_matrix/_4_MedianOfTwoSortedArrays.java
@@ -3,43 +3,36 @@ package array_matrix;
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 public class _4_MedianOfTwoSortedArrays {
 
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int left = (m + n + 1) / 2;
-        int right = (m + n + 2) / 2;
-        return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
+    public static double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int leftIdx = (m + n + 1) / 2;
+        int rightIdx = (m + n + 2) / 2;
+        int leftVal = findKth(A, 0, B, 0, leftIdx);
+        int rightVal = findKth(A, 0, B, 0, rightIdx);
+        return (leftVal + rightVal) / 2.0;
     }
 
     // find Kth Smallest Element in A&B
-    public static int findKth(int[] A, int i, int[] B, int j, int k){
-        // Beyond A's range, skip it all
-        if( i >= A.length) {
+    private static int findKth(int[] A, int i, int[] B, int j, int k) {
+        if (i == A.length) {
             return B[j + k - 1];
         }
-        // Beyond B's range, skip it all
-        if( j >= B.length){
+        if (j == B.length) {
             return A[i + k - 1];
         }
-        // Return the smaller first element
         if (k == 1) {
             return Math.min(A[i], B[j]);
         }
 
-
-        // Find k/2 separately in each array
-        // If one Array didn't contain enough elements,
-        // we drop first 2/k elements of other the Array.
-        int midA = (i + k / 2 - 1 < A.length) ? A[i + k / 2 - 1] : Integer.MAX_VALUE;
-        int midB = (j + k / 2 - 1 < B.length) ? B[j + k / 2 - 1] : Integer.MAX_VALUE;
-        if(midA < midB){
-            // k/2 of A is smaller, we drop the first 2/k elements of A.
-            // continue to search the rest k/2 elements.
-            // ensure k won't be 0
-            return findKth(A, i + k / 2, B, j , k - k / 2);
-        }else{
-            // k/2 of A is smaller, we drop the first 2/k elements of A.
-            return findKth(A, i, B, j + k / 2 , k - k / 2);
+        int midAIdx = i + k / 2 - 1;
+        int midA = midAIdx < A.length ? A[midAIdx] : Integer.MAX_VALUE;
+        int midBIdx = j + k / 2 - 1;
+        int midB = midBIdx < B.length ? B[midBIdx] : Integer.MAX_VALUE;
+        if (midA < midB) {
+            return findKth(A, i + k / 2, B, j, (k + 1) / 2);
+        } else {
+            return findKth(A, i, B, j + k / 2, (k + 1) / 2);
         }
     }
 
