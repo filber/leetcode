846e2ee52fc3f44903f7684547070ec30b5c6e48
filber <filber@foxmail.com>
2022-02-01 10:44:30 +0800

    Review

--- problems/src/dp/_121_BestTimeToBuyAndSellStock.java
+++ problems/src/dp/_121_BestTimeToBuyAndSellStock.java
@@ -3,44 +3,23 @@ package dp;
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 public class _121_BestTimeToBuyAndSellStock {
 
-    public static int maxProfitDP(int[] prices) {
-        if (prices.length < 2) {
-            return 0;
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int min = Integer.MAX_VALUE;
+        int profit = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
         }
-
-        int maxProfit = 0;
-        int[] dp = new int[prices.length];
-        dp[0] = prices[0];
-
-        for (int i = 1; i < prices.length; i++) {
-            maxProfit = Math.max(maxProfit, prices[i] - dp[i - 1]);
-            dp[i] = Math.min(dp[i - 1], prices[i]);
-        }
-
-        return maxProfit;
-    }
-
-
-    public static int maxProfitDPImproved(int[] prices) {
-        if (prices == null || prices.length < 2) {
-            return 0;
-        }
-
-        int buy = Integer.MIN_VALUE;
-        int sell = 0;
-        for (int i = 0; i < prices.length; i++) {
-            buy = Math.max(buy, -prices[i]);
-            sell = Math.max(sell, buy + prices[i]);
-        }
-
-        return sell;
+        return profit;
     }
 
     public static void main(String[] args) {
         // 5
-        int m1 = maxProfitDPImproved(new int[]{7, 1, 5, 3, 6, 4});
+        int m1 = maxProfit(new int[]{7, 1, 5, 3, 6, 4});
         // 0
-        int m2 = maxProfitDPImproved(new int[]{7, 6, 4, 3, 1});
-
+        int m2 = maxProfit(new int[]{7, 6, 4, 3, 1});
+        // 0
+        int m3 = maxProfit(new int[]{1});
     }
 }
---
+++ problems/src/dp/_1278_PalindromePartitioningIII.java
@@ -0,0 +1,12 @@
+package dp;
+
+// https://leetcode.com/problems/palindrome-partitioning-iii/
+public class _1278_PalindromePartitioningIII {
+//    public int palindromePartition(String s, int k) {
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
