a5c6a676ebfb1b8ead0eabd921c09630401e0f24
filber <filber@foxmail.com>
2022-02-04 20:05:52 +0800

    Review

--- problems/src/array_matrix/_1052_Grumpy_Bookstore_Owner.java
+++ problems/src/array_matrix/_1052_Grumpy_Bookstore_Owner.java
@@ -2,52 +2,44 @@ package array_matrix;
 
 // https://leetcode.com/problems/grumpy-bookstore-owner/
 
+import java.util.Map;
+
 //[Sliding Window],[Prefix Sum]
 public class _1052_Grumpy_Bookstore_Owner {
 
     public static int maxSatisfied(int[] customers, int[] grumpy, int m) {
         int n = customers.length;
-        if (n == 1) {
-            return customers[0];
-        }
-
-        int winSum = 0;
-        winSum += customers[0];
-
         int[] prefixSum = new int[n];
-        prefixSum[0] = grumpy[0] == 1 ? 0 : customers[0];
+        prefixSum[0] = grumpy[0] == 0 ? customers[0] : 0;
         for (int i = 1; i < n; i++) {
-            if (grumpy[i] == 1) {
-                prefixSum[i] = prefixSum[i - 1];
-            } else {
+            if (grumpy[i] == 0) {
                 prefixSum[i] = prefixSum[i - 1] + customers[i];
-            }
-
-            if (i < m) {
-                winSum += customers[i];
-            }
-        }
-
-        int left = 0;
-        int right = m - 1;
-        int maxSum = 0;
-        while (true) {
-            int leftSum = left > 0 ? prefixSum[left - 1] : 0;
-            int rightSum = prefixSum[n - 1] - prefixSum[right];
-            int curSum = leftSum + winSum + rightSum;
-            maxSum = Math.max(maxSum, curSum);
-
-            winSum -= customers[left];
-            left++;
-            right++;
-            if (right < n) {
-                winSum += customers[right];
             } else {
-                break;
+                prefixSum[i] = prefixSum[i - 1];
             }
         }
 
-        return maxSum;
+        int maxSatisfied = 0;
+        int leftSum = 0;
+        int winSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < n && i + m - 1 < n; i++) {
+            if (i == 0) {
+                leftSum = 0;
+                for (int j = 0; j < m; j++) {
+                    winSum += customers[j];
+                }
+                rightSum = prefixSum[n - 1] - prefixSum[m - 1];
+            } else {
+                leftSum = prefixSum[i-1];
+                winSum -= customers[i-1];
+                winSum += customers[i + m - 1];
+                rightSum = prefixSum[n - 1] - prefixSum[i + m - 1];
+            }
+            maxSatisfied = Math.max(maxSatisfied, leftSum + winSum + rightSum);
+        }
+
+        return maxSatisfied;
     }
 
     public static void main(String[] args) {
