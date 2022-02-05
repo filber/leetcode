e29f476768222980c6cdb02f3333cedf3e4972a3
filber <filber@foxmail.com>
2022-02-05 22:58:52 +0800

    Review

--- problems/src/array_matrix/_34_FindFirstAndLastPositionOfElementInSortedArray.java
+++ problems/src/array_matrix/_34_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -4,64 +4,73 @@ package array_matrix;
 
 public class _34_FindFirstAndLastPositionOfElementInSortedArray {
 
-  public static int binarySearch(int[] nums, int target, int start, int end) {
-    if (nums.length==0){
-      return -1;
-    } else if (start>end) {
-      return -1;
+//  Constraints:
+//          0 <= nums.length <= 10^5
+//          -10^9 <= nums[i] <= 10^9
+//          nums is a non-decreasing array.
+//          -10^9 <= target <= 10^9
+
+    public static int[] searchRange(int[] nums, int target) {
+        int idx = bs(nums, target);
+        if (idx == -1) {
+            return new int[]{-1, -1};
+        }
+        int fIdx = searchFirst(nums, target, 0, idx);
+        int lIdx = searchLast(nums, target, idx, nums.length - 1);
+        return new int[]{fIdx, lIdx};
     }
 
-    int middleIndex = (start + end) / 2;
-    int middleValue = nums[middleIndex];
-    if (middleValue == target) {
-      return middleIndex;
-    } else if (target < middleValue) {
-      return binarySearch(nums,target,start,middleIndex-1);
-    } else {
-      return binarySearch(nums, target, middleIndex + 1, end);
+    private static int searchFirst(int[] nums, int target, int start, int end) {
+        while (start < end) {
+            int mid = (start + end) / 2;
+            int midVal = nums[mid];
+            if (midVal == target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
     }
-  }
 
-  public static int searchFirst(int[] nums, int target, int start, int end) {
-    if (start == end) {
-      return start;
+    private static int searchLast(int[] nums, int target, int start, int end) {
+        while (start < end) {
+            int mid = (start + end + 1) / 2;
+            int midVal = nums[mid];
+            if (midVal == target) {
+                start = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
     }
-    int middleIndex = (start + end) / 2;
-    int middleValue = nums[middleIndex];
-    if (middleValue == target) {
-      return searchFirst(nums, target, start, middleIndex);
-    } else {
-      return searchFirst(nums, target, middleIndex + 1, end);
-    }
-  }
 
-  public static int searchLast(int[] nums, int target, int start, int end) {
-    if (start == end) {
-      return start;
+    private static int bs(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int midVal = nums[mid];
+            if (target == midVal) {
+                return mid;
+            } else if (target > midVal) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
     }
-    int middleIndex = (start + end + 1) / 2;
-    int middleValue = nums[middleIndex];
-    if (middleValue == target) {
-      return searchLast(nums, target, middleIndex, end);
-    } else {
-      return searchLast(nums, target, start, middleIndex - 1);
-    }
-  }
 
-  public static int[] searchRange(int[] nums, int target) {
-    int targetIndex = binarySearch(nums, target, 0, nums.length - 1);
-    if (targetIndex<0) {
-      return new int[]{-1, -1};
+    public static void main(String[] args) {
+        // [3,4]
+        int[] p1 = searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8);
+        // [-1,-1]
+        int[] p2 = searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6);
+        // [-1,-1]
+        int[] p3 = searchRange(new int[]{}, 0);
+        // [1,4]
+        int[] p4 = searchRange(new int[]{5, 7, 7, 7, 7, 8, 8, 10}, 7);
     }
-    int firstIndex = searchFirst(nums, nums[targetIndex], 0, targetIndex);
-    int lastIndex = searchLast(nums, nums[targetIndex], targetIndex,nums.length-1);
-    return new int[]{firstIndex,lastIndex};
-  }
-
-  public static void main(String[] args) {
-    int[] p1 = searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8); // 3,4
-    int[] p2 = searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6); // -1,-1
-    int[] p3 = searchRange(new int[]{}, 0); // -1,-1
-    int[] p4 = searchRange(new int[]{5, 7, 7,7,7,8, 8, 10}, 7); // 1,1
-  }
 }
