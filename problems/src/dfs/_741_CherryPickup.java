b9f1c2d13151b9d605b51cf7fe28919c924a5110
filber <filber@foxmail.com>
2022-02-06 21:50:34 +0800

    Review

--- problems/src/dfs/_741_CherryPickup.java
+++ problems/src/dfs/_741_CherryPickup.java
@@ -7,48 +7,44 @@ public class _741_CherryPickup {
 
     public static int cherryPickup(int[][] grid) {
         int n = grid.length;
-        if (n == 1) {
-            return grid[0][0];
-        }
-        int[][][][] dp = new int[n][n][n][n];
-        int cnt = Math.max(0, dfs(grid,dp,0, 0, 0, 0));
-        return cnt;
+        int[][][] mem = new int[n][n][n];
+
+        int gridValue = dfs(grid, mem, 0, 0, 0);
+        return Math.max(0, gridValue);
     }
 
-    public static int dfs(int[][] grid,int[][][][] dp, int x1, int y1, int x2, int y2) {
+    private static int dfs(int[][] grid, int[][][] mem, int x1, int y1, int x2) {
         int n = grid.length;
+        // total steps : x1 + y1
+        // y2 = ts - x2
+        int y2 = x1 + y1 - x2;
         if (x1 == n - 1 && y1 == n - 1 && x2 == n - 1 && y2 == n - 1) {
-            return grid[n-1][n-1];
-        }
-        if (x1 >= n || y1 >= n || x2 >= n || y2 >= n) {
+            return grid[n - 1][n - 1];
+        } else if (x1 < 0 || x1 >= n || y1 >= n || x2 < 0 || x2 >= n || y2 >= n) {
             return Integer.MIN_VALUE;
-        }
-        if (grid[x1][y1] == -1 || grid[x2][y2] == -1) {
-            dp[x1][y1][x2][y2] = Integer.MIN_VALUE;
-            dp[x2][y2][x1][y1] = Integer.MIN_VALUE;
+        } else if (grid[x1][y1] == -1 || grid[x2][y2] == -1) {
             return Integer.MIN_VALUE;
-        }
-        if (dp[x1][y1][x2][y2] != 0) {
-            return dp[x1][y1][x2][y2];
+        } else if (mem[x1][y1][x2] != 0) {
+            return mem[x1][y1][x2];
         }
 
-        int gridValue = x1 == x2 && y1 == y2
-                ? grid[x1][y1]
-                : grid[x1][y1] + grid[x2][y2];
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < direction.length; i++) {
-            int[] d1 = direction[i];
-            for (int j = 0; j < direction.length; j++) {
-                int[] d2 = direction[j];
-                int nextValue = dfs(grid,dp,
-                        x1 + d1[0], y1 + d1[1],
-                        x2 + d2[0], y2 + d2[1]);
-                max = Math.max(max, gridValue + nextValue);
+        // 1. Calculate grid Value
+        int gridVal = (x1 == x2 && y1 == y2) ? grid[x1][y1] : grid[x1][y1] + grid[x2][y2];
+
+        // 2. Cartesian product for next moves
+        int maxValue = Integer.MIN_VALUE;
+        for (int[] firstMove : direction) {
+            for (int[] secondMove : direction) {
+                int nextValue = dfs(grid, mem,
+                        x1 + firstMove[0],
+                        y1 + firstMove[1],
+                        x2 + secondMove[0]);
+                maxValue = Math.max(maxValue, nextValue + gridVal);
             }
         }
-        dp[x1][y1][x2][y2] = max;
-        dp[x2][y2][x1][y1] = max;
-        return max;
+
+        mem[x1][y1][x2] = maxValue;
+        return maxValue;
     }
 
     public static void main(String[] args) {
@@ -64,13 +60,13 @@ public class _741_CherryPickup {
                 {1, 1, 1}});
         // 15
         int c3 = cherryPickup(new int[][]{
-                {1,1,1,1,0,0,0},
-                {0,0,0,1,0,0,0},
-                {0,0,0,1,0,0,1},
-                {1,0,0,1,0,0,0},
-                {0,0,0,1,0,0,0},
-                {0,0,0,1,0,0,0},
-                {0,0,0,1,1,1,1}
+                {1, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 1},
+                {1, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 1}
         });
         // 23
         int c4 = cherryPickup(new int[][]{
