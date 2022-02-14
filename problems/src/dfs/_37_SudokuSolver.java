4769952e9d7fc1498a3855676759dc96a2edc331
filber <filber@foxmail.com>
2022-02-14 18:38:04 +0800

    Review

--- problems/src/dfs/_37_SudokuSolver.java
+++ problems/src/dfs/_37_SudokuSolver.java
@@ -21,6 +21,8 @@ public class _37_SudokuSolver {
     private boolean[][] rowCandidate;
     private boolean[][] colCandidate;
     private boolean[][][] boxCandidate;
+    private int[][] emptyLocations;
+    private int emptyCnt;
 
     public void solveSudoku(char[][] board) {
 
@@ -29,7 +31,10 @@ public class _37_SudokuSolver {
         n = board[0].length;
         rowCandidate = new boolean[m][9];
         colCandidate = new boolean[n][9];
-        boxCandidate = new boolean[m/3][n/3][9];
+        boxCandidate = new boolean[m / 3][n / 3][9];
+        emptyLocations = new int[m * n][2];
+        emptyCnt = 0;
+
         // 1. Traverse for candidates
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -38,31 +43,29 @@ public class _37_SudokuSolver {
                     rowCandidate[i][ch - '1'] = true;
                     colCandidate[j][ch - '1'] = true;
                     boxCandidate[i / 3][j / 3][ch - '1'] = true;
+                } else {
+                    emptyLocations[emptyCnt][0] = i;
+                    emptyLocations[emptyCnt][1] = j;
+                    emptyCnt++;
                 }
             }
         }
 
         // 2. Backtracking from (0,0)
-        boolean res = backtracking(0, 0);
+        boolean res = backtracking(0);
     }
 
-    private boolean backtracking(int i, int j) {
-        // Attention! Search row by row, find next empty cell
-        while (board[i][j] != '.') {
-            if (++j >= n) {
-                i++; // scan next row
-                j = 0;
-            }
-            if (i >= m) {
-                return true;
-            }
+    private boolean backtracking(int pos) {
+        if (pos == emptyCnt) {
+            return true;
         }
-
+        int i = emptyLocations[pos][0];
+        int j = emptyLocations[pos][1];
 
         // 1. Retrieve candidate for (i,j)
         boolean[] rowCand = rowCandidate[i];
         boolean[] colCand = colCandidate[j];
-        boolean[] boxCand = boxCandidate[i/3][j/3];
+        boolean[] boxCand = boxCandidate[i / 3][j / 3];
 
         // 2. Traverse all candidates and backtrack next 2 positions
         for (int k = 0; k < 9; k++) {
@@ -78,7 +81,7 @@ public class _37_SudokuSolver {
             rowCand[k] = true;
             colCand[k] = true;
             boxCand[k] = true;
-            if (backtracking(i, j)) {
+            if (backtracking(pos + 1)) {
                 return true;
             } else {
                 // Reverse states
