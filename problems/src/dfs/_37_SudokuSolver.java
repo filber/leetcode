cca5f82881d3cde111ee80f54957ab9a4ef294f1
filber <filber@foxmail.com>
2022-02-14 18:23:02 +0800

    37. Sudoku Solver

---
+++ problems/src/dfs/_37_SudokuSolver.java
@@ -0,0 +1,95 @@
+package dfs;
+
+//https://leetcode.com/problems/sudoku-solver/
+
+public class _37_SudokuSolver {
+
+    // A sudoku solution must satisfy all of the following rules:
+    //    Each of the digits 1-n must occur exactly once in each row.
+    //    Each of the digits 1-n must occur exactly once in each column.
+    //    Each of the digits 1-n must occur exactly once in each of the n 3x3 sub-boxes of the grid.
+
+    // Constraints:
+    //    board.length == n
+    //    board[i].length == n
+    //    board[i][j] is a digit or '.'.
+    //    It is guaranteed that the input board has only one solution.
+
+    private int m;
+    private int n;
+    private char[][] board;
+    private boolean[][] rowCandidate;
+    private boolean[][] colCandidate;
+    private boolean[][][] boxCandidate;
+
+    public void solveSudoku(char[][] board) {
+
+        this.board = board;
+        m = board.length;
+        n = board[0].length;
+        rowCandidate = new boolean[m][9];
+        colCandidate = new boolean[n][9];
+        boxCandidate = new boolean[m/3][n/3][9];
+        // 1. Traverse for candidates
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char ch = board[i][j];
+                if (ch != '.') {
+                    rowCandidate[i][ch - '1'] = true;
+                    colCandidate[j][ch - '1'] = true;
+                    boxCandidate[i / 3][j / 3][ch - '1'] = true;
+                }
+            }
+        }
+
+        // 2. Backtracking from (0,0)
+        boolean res = backtracking(0, 0);
+    }
+
+    private boolean backtracking(int i, int j) {
+        // Attention! Search row by row, find next empty cell
+        while (board[i][j] != '.') {
+            if (++j >= n) {
+                i++; // scan next row
+                j = 0;
+            }
+            if (i >= m) {
+                return true;
+            }
+        }
+
+
+        // 1. Retrieve candidate for (i,j)
+        boolean[] rowCand = rowCandidate[i];
+        boolean[] colCand = colCandidate[j];
+        boolean[] boxCand = boxCandidate[i/3][j/3];
+
+        // 2. Traverse all candidates and backtrack next 2 positions
+        for (int k = 0; k < 9; k++) {
+            // intersect with boolean bitwise operations
+            boolean inter = !rowCand[k] && !colCand[k] && !boxCand[k];
+            if (!inter) {
+                continue;
+            }
+
+            // Set states
+            board[i][j] = (char) ('1' + k); // update board
+            // update candidate
+            rowCand[k] = true;
+            colCand[k] = true;
+            boxCand[k] = true;
+            if (backtracking(i, j)) {
+                return true;
+            } else {
+                // Reverse states
+                board[i][j] = '.';
+                rowCand[k] = false;
+                colCand[k] = false;
+                boxCand[k] = false;
+            }
+        }
+
+        // 3. No valid candidates found
+        return false;
+    }
+}
---
+++ test/src/dfs/_37_SudokuSolverTest.java
@@ -0,0 +1,102 @@
+package dfs;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _37_SudokuSolverTest {
+
+    _37_SudokuSolver target = null;
+
+    @Before
+    public void before() throws Exception {
+        target = new _37_SudokuSolver();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void testSolveSudokuOneBox() throws Exception {
+        char[][] board = new char[][]{
+                {'5', '3', '.'},
+                {'6', '.', '.'},
+                {'.', '9', '8'}
+        };
+        target.solveSudoku(board);
+        char[][] solution = new char[][]{
+                {'5', '3', '1'},
+                {'6', '2', '4'},
+                {'7', '9', '8'}
+        };
+        assertSudoku(solution, board);
+    }
+
+    @Test
+    public void testSolveSudokuTwoBox() throws Exception {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.'},
+                {'6', '.', '.', '1', '9', '5'},
+                {'.', '9', '8', '.', '.', '.'}
+        };
+        target.solveSudoku(board);
+        char[][] solution = new char[][]{
+                {'5', '3', '1', '2', '7', '8'},
+                {'6', '2', '4', '1', '9', '5'},
+                {'7', '9', '8', '3', '4', '6'}
+        };
+        assertSudoku(solution, board);
+    }
+
+    @Test
+    public void testSolveSudokuThreeBox() throws Exception {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+        };
+        target.solveSudoku(board);
+        char[][] solution = new char[][]{
+                {'5', '3', '1', '6', '7', '8', '2', '4', '9'},
+                {'6', '2', '4', '1', '9', '5', '3', '7', '8'},
+                {'7', '9', '8', '2', '3', '4', '1', '6', '5'}
+        };
+        assertSudoku(solution, board);
+    }
+
+    @Test
+    public void testSolveSudokuNineBoxes() throws Exception {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        target.solveSudoku(board);
+        char[][] solution = new char[][]{
+                {'5', '3', '4', '6', '7', '8', '9', '1', '2'},
+                {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+                {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
+                {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+                {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+                {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+                {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+                {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+                {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
+
+        assertSudoku(solution, board);
+    }
+
+    private void assertSudoku(char[][] expected, char[][] actual) {
+        for (int i = 0; i < expected.length; i++) {
+            assertArrayEquals(expected[i], actual[i]);
+        }
+    }
+}
