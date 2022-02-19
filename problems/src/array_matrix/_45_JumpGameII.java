d06572b9a5c4b6f86f1e53f41f98faddf7c05669
filber <filber@foxmail.com>
2022-02-19 16:42:33 +0800

    Review

--- problems/src/array_matrix/_45_JumpGameII.java
+++ problems/src/array_matrix/_45_JumpGameII.java
@@ -2,15 +2,12 @@ package array_matrix;
 
 // https://leetcode.com/problems/jump-game-ii/
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class _45_JumpGameII {
 
     // two pointers
     public static int jump(int[] nums) {
         int l = 0, r = 0, max = 0, count = 0;
-        while (r < nums.length -1 ) {
+        while (r < nums.length - 1) {
             for (int i = l; i <= r; i++) {
                 if (nums[i] + i > max) {
                     max = nums[i] + i;
@@ -23,78 +20,22 @@ public class _45_JumpGameII {
         return count;
     }
 
-    // greedy
-    public static int jumpGreedy(int[] nums) {
-
+    public int jumpGreedy(int[] nums) {
         int n = nums.length;
+        int mostRightPosition = 0;
+        int mostRightJump = 0;
         int jumps = 0;
-        int mostRightPos = 0;
-        // current end position of last jump
-        int curEnd = 0;
-
         for (int i = 0; i < n - 1; i++) {
-            mostRightPos = Math.max(mostRightPos, i + nums[i]);
-            if (i == curEnd) {
+            mostRightPosition = Math.max(mostRightPosition, i + nums[i]);
+            if (i == mostRightJump) {
+                mostRightJump = mostRightPosition;
                 jumps++;
-                curEnd = mostRightPos;
-                // jump out early
-                if (curEnd >= nums.length - 1) {
+                if (mostRightJump >= n - 1) {
                     return jumps;
                 }
             }
         }
-        return jumps;
-    }
 
-    public static int jumpBFS(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return 0;
-        }
-        boolean[] mem = new boolean[n];
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        int jumpCnt = 1;
-        int levelCnt = 1;
-        int tmpCnt = 0;
-
-        while (!queue.isEmpty()) {
-            int pos = queue.poll();
-            levelCnt--;
-            for (int i = nums[pos]; i >= 1; i--) {
-                int targetPos = pos + i;
-                if (targetPos >= n - 1) {
-                    return jumpCnt;
-                }
-                if (targetPos < n && !mem[targetPos]) {
-                    mem[targetPos] = true;
-                    queue.add(targetPos);
-                    tmpCnt++;
-                }
-            }
-            if (levelCnt == 0) {
-                levelCnt = tmpCnt;
-                tmpCnt = 0;
-                jumpCnt++;
-            }
-        }
-
-        return jumpCnt;
-    }
-
-    public static void main(String[] args) {
-        // 2[0-->1-->4]
-        int j1 = jump(new int[]{2, 3, 1, 1, 4});
-        // 2[0-->1-->4]
-        int j2 = jump(new int[]{2, 3, 0, 1, 4});
-        // 1[0-->2]
-        int j3 = jump(new int[]{2, 0, 0});
-        // 3[0-->1-->8-->11]
-        int j4 = jump(new int[]{5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0});
-        // 0
-        int j5 = jump(new int[]{1});
-        // 2 []
-        int j6 = jump(new int[]{7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3});
+        return 0;
     }
 }
---
+++ problems/src/array_matrix/_45_JumpGameIITest.java
@@ -0,0 +1,63 @@
+package array_matrix;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _45_JumpGameIITest {
+
+    _45_JumpGameII target = null;
+
+    @Before
+    public void before() throws Exception {
+        target = new _45_JumpGameII();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+
+    @Test
+    public void testJump1() throws Exception {
+        // 2[0-->1-->4]
+        int jumps = target.jump(new int[]{2, 3, 1, 1, 4});
+        assertEquals(2, jumps);
+    }
+
+    @Test
+    public void testJump2() throws Exception {
+        // 2[0-->1-->4]
+        int jumps = target.jump(new int[]{2, 3, 0, 1, 4});
+        assertEquals(2, jumps);
+    }
+
+    @Test
+    public void testJump3() throws Exception {
+        // 1[0-->2]
+        int jumps = target.jump(new int[]{2, 0, 0});
+        assertEquals(1, jumps);
+    }
+
+    @Test
+    public void testJump4() throws Exception {
+        // 3[0-->1-->8-->11]
+        int jumps = target.jump(new int[]{5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0});
+        assertEquals(3, jumps);
+    }
+
+    @Test
+    public void testJump5() throws Exception {
+        int jumps = target.jump(new int[]{1});
+        assertEquals(0, jumps);
+    }
+
+    @Test
+    public void testJump6() throws Exception {
+        // 2 [0-7-14]
+        int jumps = target.jump(new int[]{7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3});
+        assertEquals(2, jumps);
+    }
+}
