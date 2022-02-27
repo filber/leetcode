cf8d5638d410ff4dcc18c00417459ece2f68d9fe
filber <filber@foxmail.com>
2022-02-27 22:10:26 +0800

    Review

--- problems/src/dfs/_51_NQueens.java
+++ problems/src/dfs/_51_NQueens.java
@@ -1,67 +1,57 @@
 package dfs;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class _51_NQueens {
 
-    boolean[] row;
+    List<List<String>> ans = new ArrayList<>();
     boolean[] col;
     boolean[] slash;
     boolean[] backslash;
-    char[][] board;
     int n;
-    List<List<String>> ans = new ArrayList<>();
+    char[][] board;
 
     public List<List<String>> solveNQueens(int n) {
         this.n = n;
-        row = new boolean[n];
         col = new boolean[n];
         slash = new boolean[2 * n - 1];
         backslash = new boolean[2 * n - 1];
+
         board = new char[n][n];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
         }
 
         dfs(0);
+
         return ans;
     }
 
     private void dfs(int i) {
         if (i == n) {
-            // add to ans
-            List<String> list = new ArrayList<>(n);
-            for (int r = 0; r < n; r++) {
-                list.add(new String(board[r]));
+            List<String> list = new ArrayList<>();
+            for (int k = 0; k < n; k++) {
+                list.add(new String(board[k]));
             }
             ans.add(list);
             return;
         }
 
         for (int j = 0; j < n; j++) {
-            int slashIdx = i + j;
-            int backslashIdx = i - j + n - 1;
-            if (row[i] || col[j] || slash[slashIdx] || backslash[backslashIdx]) {
+            if (col[j] || slash[i + j] || backslash[j - i + n - 1]) {
                 continue;
             }
-
-            // set states
             board[i][j] = 'Q';
-            row[i] = true;
-            col[j] = true;
-            slash[slashIdx] = true;
-            backslash[backslashIdx] = true;
+            col[j] = slash[i + j] = backslash[j - i + n - 1] = true;
 
             dfs(i + 1);
 
-            // reverse states
             board[i][j] = '.';
-            row[i] = false;
-            col[j] = false;
-            slash[slashIdx] = false;
-            backslash[backslashIdx] = false;
+            col[j] = slash[i + j] = backslash[j - i + n - 1] = false;
         }
     }
+
 }
