8f45c1d3a5b253a5565d1a599fe3e9069cf8564c
filber <filber@foxmail.com>
2022-02-27 20:42:10 +0800

    Review

--- problems/src/string/_2014_LongestSubsequenceRepeatedKTimes.java
+++ problems/src/string/_2014_LongestSubsequenceRepeatedKTimes.java
@@ -5,70 +5,65 @@ import java.util.*;
 
 public class _2014_LongestSubsequenceRepeatedKTimes {
 
-    String ans = "";
-    int k;
     char[] chars;
-    int n = 0;
-    int[] charCnt = new int[26];
+    int n;
+    int k;
 
     public String longestSubsequenceRepeatedK(String s, int k) {
         this.k = k;
-        char[] sChars = s.toCharArray();
-
-        for (int i = 0; i < sChars.length; i++) {
-            charCnt[sChars[i] - 'a']++;
+        char[] originChars = s.toCharArray();
+        int[] charCnt = new int[26];
+        for (char ch : originChars) {
+            charCnt[ch - 'a']++;
         }
-        chars = new char[sChars.length];
-        // skip invalid chars
-        for (int i = 0; i < sChars.length; i++) {
-            if (charCnt[sChars[i] - 'a'] >= k) {
-                chars[n++] = sChars[i];
+        chars = new char[originChars.length];
+        n = 0;
+        for (char ch : originChars) {
+            if (charCnt[ch - 'a'] >= k) {
+                chars[n++] = ch;
             }
         }
 
+        List<Character> letter = new ArrayList<>();
         Queue<String> queue = new ArrayDeque<>();
-
-        List<Character> alphaList = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < charCnt.length; i++) {
             if (charCnt[i] >= k) {
                 char ch = (char) ('a' + i);
-                alphaList.add(ch);
+                letter.add(ch);
                 queue.add(String.valueOf(ch));
             }
         }
 
+        String ans = "";
         while (!queue.isEmpty()) {
-            String p = queue.poll();
-            ans = p;
-            char[] candChars = new char[p.length() + 1];
-            char[] pChars = p.toCharArray();
-            for (int i = 0; i < pChars.length; i++) {
-                candChars[i] = pChars[i];
+            ans = queue.poll();
+            char[] candidate = new char[ans.length() + 1];
+            for (int i = 0; i < candidate.length - 1; i++) {
+                candidate[i] = ans.charAt(i);
             }
-            for (char ch : alphaList) {
-                candChars[candChars.length - 1] = ch;
-                if (checkCount(candChars) == k) {
-                    queue.add(new String(candChars));
+            for (char ch : letter) {
+                candidate[candidate.length - 1] = ch;
+                if (count(candidate) >= k) {
+                    queue.add(new String(candidate));
                 }
             }
         }
-
         return ans;
     }
 
-    private int checkCount(char[] candidate) {
-        int i = 0, j = 0, count = 0;
-        while (j < n && count < k) {
-            if (candidate[i] == chars[j]) {
-                i++;
+    private int count(char[] candidate) {
+        int l = 0, r = 0;
+        int cnt = 0;
+        while (l < chars.length && r < candidate.length && cnt < k) {
+            if (chars[l] == candidate[r]) {
+                r++;
+                if (r == candidate.length) {
+                    r = 0;
+                    cnt++;
+                }
             }
-            j++;
-            if (i == candidate.length) {
-                i = 0;
-                count++;
-            }
+            l++;
         }
-
-        return count;
+        return cnt;
     }
 }
