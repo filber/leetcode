ebe34b562ec918ee4e3d270152b933086685598e
filber <filber@foxmail.com>
2022-03-01 22:51:04 +0800

    Review

--- problems/src/array_matrix/_1001_GridIllumination.java
+++ problems/src/array_matrix/_1001_GridIllumination.java
@@ -9,59 +9,58 @@ import java.util.Set;
 
 public class _1001_GridIllumination {
 
-    Set<Integer> lampsSet = new HashSet<>();
-    Map<Integer, Integer> row = new HashMap<>();
-    Map<Integer, Integer> col = new HashMap<>();
-    Map<Integer, Integer> slash = new HashMap<>();
-    Map<Integer, Integer> backslash = new HashMap<>();
+    private Map<Integer, Set<Integer>> lampsPos = new HashMap<>();
+    private Map<Integer, Integer> row = new HashMap<>(); // i
+    private Map<Integer, Integer> col = new HashMap<>(); // j
+    private Map<Integer, Integer> slash = new HashMap<>(); // i+j
+    private Map<Integer, Integer> backslash = new HashMap<>(); // j-i
 
     public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
         for (int i = 0; i < lamps.length; i++) {
-            // store to lamps map
-            int r = lamps[i][0];
-            int c = lamps[i][1];
+            int[] lamp = lamps[i];
+            int R = lamp[0];
+            int C = lamp[1];
+            Set<Integer> set = lampsPos.get(R);
+            if (set == null) {
+                set = new HashSet<>();
+                lampsPos.put(R, set);
+            }
 
-            int pos = r * n + c;
-            if (!lampsSet.contains(pos)) {
-                lampsSet.add(pos);
-
-                // illuminate grid
-                row.put(r, row.getOrDefault(r, 0) + 1);
-                col.put(c, col.getOrDefault(c, 0) + 1);
-                slash.put(r + c, slash.getOrDefault(r + c, 0) + 1);
-                backslash.put(c - r, backslash.getOrDefault(c - r, 0) + 1);
+            // skip duplicate lamps
+            if (!set.contains(C)) {
+                set.add(C);
+                row.put(R, row.getOrDefault(R, 0) + 1);
+                col.put(C, col.getOrDefault(C, 0) + 1);
+                slash.put(R + C, slash.getOrDefault(R + C, 0) + 1);
+                backslash.put(C - R, backslash.getOrDefault(C - R, 0) + 1);
             }
         }
 
         int[] ans = new int[queries.length];
+
         for (int i = 0; i < queries.length; i++) {
-            int r = queries[i][0];
-            int c = queries[i][1];
-            // check if illuminated
-            if (row.getOrDefault(r, 0) > 0 ||
-                    col.getOrDefault(c, 0) > 0 ||
-                    slash.getOrDefault(r + c, 0) > 0 ||
-                    backslash.getOrDefault(c - r, 0) > 0
-            ) {
+            int[] query = queries[i];
+            int R = query[0];
+            int C = query[1];
+            if (row.getOrDefault(R, 0) > 0 ||
+                    col.getOrDefault(C, 0) > 0 ||
+                    slash.getOrDefault(R + C, 0) > 0 ||
+                    backslash.getOrDefault(C - R, 0) > 0) {
                 ans[i] = 1;
-            } else {
-                ans[i] = 0;
             }
 
-            // turn off lights
-            for (int k = -1; k <= 1; k++) {
-                for (int m = -1; m <= 1; m++) {
-                    int lightR = r + k;
-                    int lightC = c + m;
-                    int pos = lightR * n + lightC;
-                    if (lightR >= 0 && lightR < n && lightC >= 0 && lightC < n && lampsSet.contains(pos)) {
-                        // turn off lights
-                        row.put(lightR, row.getOrDefault(lightR, 1) - 1);
-                        col.put(lightC, col.getOrDefault(lightC, 1) - 1);
-                        slash.put(lightR + lightC, slash.getOrDefault(lightR + lightC, 1) - 1);
-                        backslash.put(lightC - lightR, backslash.getOrDefault(lightC - lightR, 1) - 1);
-                        // remove lamps
-                        lampsSet.remove(pos);
+            for (int j = -1; j <= 1; j++) {
+                for (int k = -1; k <= 1; k++) {
+                    int nR = R + j;
+                    int nC = C + k;
+                    Set<Integer> set = lampsPos.get(nR);
+                    if (set != null && set.contains(nC)) {
+                        // found lamp!
+                        set.remove(nC); // remove lamp
+                        row.put(nR, row.getOrDefault(nR, 1) - 1);
+                        col.put(nC, col.getOrDefault(nC, 1) - 1);
+                        slash.put(nR + nC, slash.getOrDefault(nR + nC, 1) - 1);
+                        backslash.put(nC - nR, backslash.getOrDefault(nC - nR, 1) - 1);
                     }
                 }
             }
