121a76414b6c7110996040b387f597ef49a0e3fb
filber <filber@foxmail.com>
2022-03-01 12:46:31 +0800

    Review

--- problems/src/dp/_338_CountingBits.java
+++ problems/src/dp/_338_CountingBits.java
@@ -7,11 +7,7 @@ public class _338_CountingBits {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
         for (int i = 0; i <= n; i++) {
-            if (i % 2 == 0) {
-                ans[i] = ans[i >> 1];
-            } else {
-                ans[i] = ans[(i - 1) >> 1] + 1;
-            }
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
