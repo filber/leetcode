68430da3c933d5e0c1c6c26e1b9da3379e682b50
filber <filber@foxmail.com>
2022-03-12 00:33:36 +0800

    Review

--- problems/src/list/_445_AddTwoNumbersII.java
+++ problems/src/list/_445_AddTwoNumbersII.java
@@ -25,9 +25,10 @@ public class _445_AddTwoNumbersII {
             return l1;
         }
 
+        // 3. add two list
         ListNode ans = add(l1, m - 1, l2, n - 1);
         if (ans.val >= 10) {
-            // handle head if it's GE 10
+            // 4. handle head if it's GE 10
             ListNode head = new ListNode(ans.val / 10, ans);
             ans.val %= 10;
             return head;
@@ -39,24 +40,28 @@ public class _445_AddTwoNumbersII {
     public ListNode add(ListNode left, int l, ListNode right, int r) {
         if (l == 0 && r == 0) {
             // reaching the end
-            return new ListNode(left.val + right.val);
+            left.val += right.val;
+            return left;
         }
 
-        int sum;
-        ListNode next;
         if (l == r) {
-            next = add(left.next, l - 1, right.next, r - 1);
-            sum = left.val + right.val;
+            ListNode next = add(left.next, l - 1, right.next, r - 1);
+            left.val += right.val + next.val / 10;
+            left.next = next;
+            next.val %= 10;
+            return left;
         } else if (l > r) {
-            next = add(left.next, l - 1, right, r);
-            sum = left.val;
+            ListNode next = add(left.next, l - 1, right, r);
+            left.val += next.val / 10;
+            left.next = next;
+            next.val %= 10;
+            return left;
         } else {
-            next = add(left, l, right.next, r - 1);
-            sum = right.val;
+            ListNode next = add(left, l, right.next, r - 1);
+            right.val += next.val / 10;
+            right.next = next;
+            next.val %= 10;
+            return right;
         }
-        sum += next.val / 10;
-        next.val %= 10;
-        return new ListNode(sum, next);
     }
-
 }
