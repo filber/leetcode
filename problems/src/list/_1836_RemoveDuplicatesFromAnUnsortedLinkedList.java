16a96d9e58300052ecddae1f1dc55b862b21ee35
filber <filber@foxmail.com>
2022-03-11 15:03:05 +0800

    Review

--- problems/src/list/_1836_RemoveDuplicatesFromAnUnsortedLinkedList.java
+++ problems/src/list/_1836_RemoveDuplicatesFromAnUnsortedLinkedList.java
@@ -10,34 +10,37 @@ public class _1836_RemoveDuplicatesFromAnUnsortedLinkedList {
     //For example if the linked list is 12->11->12->21->41->43->21
     //then you should convert the list to 41->43.
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode cur = head;
-        Set<Integer> reduceSet = new HashSet<>();
+        if (head == null) {
+            return null;
+        }
+        Set<Integer> numSet = new HashSet<>();
         Set<Integer> removeSet = new HashSet<>();
+        ListNode cur = head;
+        // 1. Detect duplicates
         while (cur != null) {
-            if (reduceSet.contains(cur.val)) {
+            if (numSet.contains(cur.val)) {
                 removeSet.add(cur.val);
             } else {
-                reduceSet.add(cur.val);
+                numSet.add(cur.val);
             }
             cur = cur.next;
         }
 
+        // 2. Move head to skip duplicates
         while (head != null && removeSet.contains(head.val)) {
-            ListNode tmp = head;
             head = head.next;
-            tmp.next = null;
         }
 
-        ListNode pre = head;
-        cur = head == null ? null : head.next;
-
-        while (cur != null) {
-            if (removeSet.contains(cur.val)) {
-                pre.next = cur.next;
+        // 3. Using Two Pointers to remove duplicates
+        ListNode l = head;
+        ListNode r = head == null ? null : head.next;
+        while (r != null) {
+            if (removeSet.contains(r.val)) {
+                l.next = r.next;
             } else {
-                pre = cur;
+                l = r;
             }
-            cur = cur.next;
+            r = r.next;
         }
 
         return head;
