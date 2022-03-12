cf7fac873c07a4fe420a782d86080d2c7852d7ab
filber <filber@foxmail.com>
2022-03-12 21:07:45 +0800

    Review

--- problems/src/tree/_218_TheSkylineProblem.java
+++ problems/src/tree/_218_TheSkylineProblem.java
@@ -12,103 +12,108 @@ public class _218_TheSkylineProblem {
 
     public List<List<Integer>> getSkyline(int[][] buildings) {
         int n = buildings.length;
+        // 1. Add the range of all buildings to SegmentTree
         for (int i = 0; i < n; i++) {
             int[] building = buildings[i];
             root = addRange(root, building[0], building[1], building[2]);
         }
 
+        // 2. Middle-Order traverse the SegmentTree for silhouette
         List<SegmentTree> list = new ArrayList<>();
-        traverse(list, root);
-
         List<List<Integer>> ans = new ArrayList<>();
-        for (SegmentTree tree : list) {
-            ans.add(Arrays.asList(tree.begin,tree.height));
-        }
-        if (!list.isEmpty()) {
-            SegmentTree last = list.get(list.size() - 1);
-            ans.add(Arrays.asList(last.end, 0));
-        }
+
+        traverse(list, ans, root);
+
+        // 3. Append sentry value of the last node
+        SegmentTree lastNode = list.get(list.size() - 1);
+        ans.add(Arrays.asList(lastNode.end, 0));
 
         return ans;
     }
 
-    private void traverse(List<SegmentTree> list, SegmentTree node) {
+    private void traverse(List<SegmentTree> list, List<List<Integer>> ans, SegmentTree node) {
+        // 0. Guard condition
         if (node == null) {
             return;
         }
-        traverse(list, node.left);
 
-        if (!list.isEmpty()) {
-            SegmentTree last = list.get(list.size() - 1);
-            if (last.end == node.begin) {
+        // 1. Traverse left
+        traverse(list, ans, node.left);
+
+        // 2. Handle current node
+        if (list.isEmpty()) {
+            list.add(node);
+            ans.add(Arrays.asList(node.begin, node.height));
+        } else {
+            SegmentTree lastNode = list.get(list.size() - 1);
+            if (lastNode.end == node.begin) {
                 // consecutive
-                if (last.height == node.height) {
+                if (lastNode.height == node.height) {
                     // same height
-                    last.end = node.end; // expand;
+                    lastNode.end = node.end;
                 } else {
                     // different height
                     list.add(node);
+                    ans.add(Arrays.asList(node.begin, node.height));
                 }
             } else {
-                list.add(new SegmentTree(last.end, last.end, 0));
+                // not consecutive
+                list.add(new SegmentTree(lastNode.end, lastNode.end, 0));
+                ans.add(Arrays.asList(lastNode.end, 0));
+
                 list.add(node);
+                ans.add(Arrays.asList(node.begin, node.height));
             }
-        } else {
-            list.add(node);
         }
 
-        traverse(list, node.right);
+        // 3. Traverse right
+        traverse(list, ans, node.right);
     }
 
     private SegmentTree addRange(SegmentTree node, int L, int R, int H) {
+        // 0. Guard condition
         if (L >= R) {
             return node;
         } else if (node == null) {
             return new SegmentTree(L, R, H);
+        } else if (node.begin <= L && R <= node.end && H <= node.height) {
+            return node; // won't change the skyline
         } else if (node.begin == L && R == node.end) {
-            if (H > node.height) {
-                node.height = H;
-            }
-            return node;
-        }
+            // H > node.height
+            node.height = H;
+        } else if (node.begin < L && R < node.end) {
+            // Split the node
+            SegmentTree leftTree = new SegmentTree(node.begin, L, node.height);
+            leftTree.left = node.left;
+            node.left = leftTree;
 
-        if (L <= node.begin && node.end <= R) {
-            node.left = addRange(node.left, L, node.begin, H);
-            node.right = addRange(node.right, node.end, R, H);
-            if (H > node.height) {
-                node.height = H;
-            }
+            SegmentTree rightTree = new SegmentTree(R, node.end, node.height);
+            rightTree.right = node.right;
+            node.right = rightTree;
+
+            node.height = H;
+            node.begin = L;
+            node.end = R;
         } else if (R <= node.begin) {
             node.left = addRange(node.left, L, R, H);
         } else if (node.end <= L) {
             node.right = addRange(node.right, L, R, H);
         } else {
-            // node.begin<R && L<node.end && (node.begin<L || R<node.end)
             node.left = addRange(node.left, L, node.begin, H);
             node.right = addRange(node.right, node.end, R, H);
-            if (node.begin < L && R < node.end && node.height < H) {
-                SegmentTree lTree = new SegmentTree(node.begin, L, node.height);
-                lTree.left = node.left;
-                node.left = lTree;
-
-                SegmentTree rTree = new SegmentTree(R, node.end, node.height);
-                rTree.right = node.right;
-                node.right = rTree;
-
-                node.begin = L;
-                node.end = R;
+            if (L <= node.begin && node.end <= R && node.height < H) {
                 node.height = H;
-            } else if (R < node.end && node.height < H) {
-                SegmentTree lTree = new SegmentTree(node.begin, R, H);
-                lTree.left = node.left;
-                node.left = lTree;
-
+            } else if (L <= node.begin && node.height < H) {
+                // L <= node.begin < R < node.end
+                SegmentTree leftTree = new SegmentTree(node.begin, R, H);
+                leftTree.left = node.left;
+                node.left = leftTree;
                 node.begin = R;
-            } else if (node.begin < L && node.height < H) {
-                SegmentTree rTree = new SegmentTree(L, node.end, H);
-                rTree.right = node.right;
-                node.right = rTree;
-
+            } else if (node.end <= R && node.height < H) {
+                // node.begin < L < R <= node.end
+                SegmentTree rightTree = new SegmentTree(L, node.end, H);
+                rightTree.right = node.right;
+                node.right = rightTree;
                 node.end = L;
             }
         }
