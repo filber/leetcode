51fb919ba144f3c068b277c759a17701c2da08f7
filber <filber@foxmail.com>
2022-03-15 16:42:27 +0800

    Review

--- problems/src/dfs/_78_Subsets.java
+++ problems/src/dfs/_78_Subsets.java
@@ -1,33 +1,38 @@
 package dfs;
 
+//https://leetcode.com/problems/subsets/
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class _78_Subsets {
 
-    // Backtracking
+    List<List<Integer>> ans = new ArrayList<>();
+    int[] nums;
+    int n;
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        backtracking(ans, new ArrayList<>(), nums, 0);
+        this.nums = nums;
+        this.n = nums.length;
+        int[] list = new int[n];
+
+        backtracking(list, 0, 0);
         return ans;
     }
 
-    private void backtracking(List<List<Integer>> ans, List<Integer> list, int[] nums, int i) {
-        // 0. Guard condition
-        if (i == nums.length) {
-            ans.add(new ArrayList<>(list));
+    private void backtracking(int[] list, int len, int i) {
+        if (i == n) {
+            List<Integer> l = new ArrayList<>(len);
+            for (int j = 0; j < len; j++) {
+                l.add(list[j]);
+            }
+            ans.add(l);
             return;
         }
+        list[len++] = nums[i];
+        backtracking(list, len, i + 1);
+        len--;
 
-        // 1. Not use Ni
-        backtracking(ans, list, nums, i + 1);
-
-        // 2. Use Ni
-        // set states
-        list.add(nums[i]);
-        // backtracking
-        backtracking(ans, list, nums, i + 1);
-        // reverse states
-        list.remove(list.size() - 1);
+        backtracking(list, len, i + 1);
     }
 }
