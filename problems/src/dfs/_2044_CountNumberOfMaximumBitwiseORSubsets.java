1b4efd74fefc9e828ad14151e5e01670202bff31
filber <filber@foxmail.com>
2022-03-15 16:24:40 +0800

    Review

--- problems/src/dfs/_2044_CountNumberOfMaximumBitwiseORSubsets.java
+++ problems/src/dfs/_2044_CountNumberOfMaximumBitwiseORSubsets.java
@@ -3,25 +3,26 @@ package dfs;
 // https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
 public class _2044_CountNumberOfMaximumBitwiseORSubsets {
 
-    int maxOr = 0;
-    int maxCnt = 0;
+
+    int maxOr = Integer.MIN_VALUE;
+    int cnt = 0;
     int[] nums;
     int n;
 
     public int countMaxOrSubsets(int[] nums) {
         this.nums = nums;
-        n = nums.length;
+        this.n = nums.length;
         dfs(0, 0);
-        return maxCnt;
+        return cnt;
     }
 
     private void dfs(int or, int i) {
         if (i == n) {
             if (or > maxOr) {
                 maxOr = or;
-                maxCnt = 1;
+                cnt = 1;
             } else if (or == maxOr) {
-                maxCnt++;
+                cnt++;
             }
             return;
         }
