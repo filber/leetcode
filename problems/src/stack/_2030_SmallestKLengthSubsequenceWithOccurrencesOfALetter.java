f5d7a804c7cb9041159c5228560f82c3485d01bb
filber <filber@foxmail.com>
2022-03-18 18:50:23 +0800

    2030. Smallest K Length Subsequence with Occurences of a letter

--- problems/src/stack/_2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter.java
+++ problems/src/stack/_2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter.java
@@ -1,41 +1,39 @@
 package stack;
 
-import java.util.Stack;
-
 public class _2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter {
 
     public String smallestSubsequence(String s, int k, char letter, int repetition) {
-        Stack<Character> stack = new Stack<>();
         char[] arr = s.toCharArray();
+        int n = arr.length;
+        char[] stack = new char[k];
+        int top = -1;
         int letterCount = 0;
         for (char ch : arr) {
             letterCount += ch == letter ? 1 : 0;
         }
 
-        int n = arr.length;
-
         int curLetter = 0; // count of letter in stack
         for (int i = 0; i < arr.length; i++) {
             while (
-                    // 1. could pop
-                    (!stack.isEmpty() // stack is not empty
-                    && stack.peek() > arr[i] // arr[i] is better
-                    && n - i + stack.size() > k // have enough remaining chars
-                    && curLetter - (stack.peek() == letter ? 1 : 0) + letterCount >= repetition // have enough letters
+                // 1. could pop
+                    (
+                    top >= 0 // stack is not empty
+                            && stack[top] > arr[i] // arr[i] is better
+                            && n - i + top + 1 > k // have enough remaining chars
+                            && curLetter - (stack[top] == letter ? 1 : 0) + letterCount >= repetition // have enough letters
                     )
-                    ||
-                    // 2. must pop
-                    curLetter + k - stack.size() < repetition // no enough position for letter
+                ||
+                // 2. must pop
+                curLetter + k - top - 1 < repetition // no enough position for letter
             ) {
-                Character pop = stack.pop();
-                if (pop == letter) {
-                    curLetter--;
-                }
+                char peek = stack[top];
+                top--;
+                curLetter -= peek == letter ? 1 : 0;
             }
 
-            if (stack.size() < k) {
-                if (arr[i] == letter) curLetter++;
-                stack.push(arr[i]);
+            if (top + 1 < k) {
+                curLetter += arr[i] == letter ? 1 : 0;
+                stack[++top] = arr[i];
             }
 
             if (arr[i] == letter) {
@@ -43,10 +41,6 @@ public class _2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter {
             }
         }
 
-        StringBuffer sb = new StringBuffer();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
+        return new String(stack, 0, top + 1);
     }
 }
