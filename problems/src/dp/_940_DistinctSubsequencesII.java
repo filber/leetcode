02781ad84f245eeeaa8b14f9b0a4c46be3f2c757
filber <filber@foxmail.com>
2022-03-21 18:21:12 +0800

    Review

--- problems/src/dp/_940_DistinctSubsequencesII.java
+++ problems/src/dp/_940_DistinctSubsequencesII.java
@@ -6,22 +6,21 @@ public class _940_DistinctSubsequencesII {
 
     public int distinctSubseqII(String s) {
         long modulo = (long) (1e9 + 7);
+        int[] lastPos = new int[26];
         char[] chars = s.toCharArray();
         int n = chars.length;
         long[] dp = new long[n + 1];
         dp[0] = 1;
-        int[] lastIdx = new int[26];
         for (int i = 1; i <= n; i++) {
-            int idx = chars[i - 1] - 'a';
-            int j = lastIdx[idx];
-            if (j == 0) {
-                dp[i] = (dp[i - 1] << 1) % modulo;
+            char ch = chars[i - 1];
+            int lastIdx = lastPos[ch - 'a'];
+            if (lastIdx == 0) {
+                dp[i] = (dp[i - 1] * 2) % modulo;
             } else {
-                dp[i] = ((dp[i - 1] << 1) - dp[j - 1] + modulo) % modulo;
+                dp[i] = (dp[i - 1] * 2 - dp[lastIdx - 1] + modulo) % modulo;
             }
-            lastIdx[idx] = i;
+            lastPos[ch - 'a'] = i;
         }
-
         return (int) (dp[n] - 1);
     }
 }
