3c2a33b5ad10df1a56322002151a81ed7ca16393
filber <filber@foxmail.com>
2022-03-24 23:26:58 +0800

    Review

--- problems/src/dfs/_90_SubsetsII.java
+++ problems/src/dfs/_90_SubsetsII.java
@@ -5,45 +5,46 @@ import java.util.*;
 public class _90_SubsetsII {
 
     List<List<Integer>> ans = new ArrayList<>();
+    int[] nums;
     int[] path;
     int len = 0;
     boolean[] used;
     int n;
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        this.n = nums.length;
-        path = new int[n];
+        n = nums.length;
         used = new boolean[n];
+        path = new int[n];
+        this.nums = nums;
         Arrays.sort(nums);
 
-        backtracking(nums, 0);
-
+        backtracking(0);
         return ans;
     }
 
-    private void backtracking(int[] nums, int i) {
-        List<Integer> list = new ArrayList<>(len);
-        for (int j = 0; j < len; j++) {
-            list.add(path[j]);
-        }
-
-        ans.add(list);
-
+    private void backtracking(int i) {
         if (i == n) {
+            List<Integer> list = new ArrayList<>();
+            for (int k = 0; k < len; k++) {
+                list.add(path[k]);
+            }
+            ans.add(list);
             return;
         }
 
-        for (int k = i; k < n; k++) {
-            if (k > 0 && nums[k - 1] == nums[k] && !used[k - 1]) {
-                continue;
-            }
-            used[k] = true;
-            path[len++] = nums[k];
+        // NOT USE
+        backtracking(i + 1);
 
-            backtracking(nums, k + 1);
+        // USE
+        // set constraints for USE
+        if (i == 0 || nums[i] != nums[i - 1] || used[i - 1]) {
+            used[i] = true;
+            path[len++] = nums[i];
 
+            backtracking(i + 1);
+
+            used[i] = false;
             len--;
-            used[k] = false;
         }
     }
 }
