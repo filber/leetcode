ef32ac5c373be303e21445324b68cd5c96df76d3
filber <filber@foxmail.com>
2022-03-24 15:12:17 +0800

    Review

--- problems/src/greedy/_881_BoatsToSavePeople.java
+++ problems/src/greedy/_881_BoatsToSavePeople.java
@@ -9,52 +9,46 @@ public class _881_BoatsToSavePeople {
 //  each boat carries at most two people
 
     public static int numRescueBoats(int[] people, int limit) {
-        int[] cnt = new int[30001];
+        int[] bucket = new int[30001];
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for (int p : people) {
-            cnt[p]++;
-        }
-
-        int l = 1;
-        while (l <= limit && cnt[l] == 0) {
-            l++;
-        }
-        int r = limit;
-        while (r >= 1 && cnt[r] == 0) {
-            r--;
-        }
-        int boats = 0;
-        while (l <= r) {
-            if (l==r) {
-                if (l * 2 <= limit) {
-                    boats += cnt[l] / 2;
-                    boats += cnt[l] % 2;
-                } else {
-                    boats += cnt[l];
-                }
-                l++;
-            } else {
-                // l<r
-                if (l + r > limit) {
-                    boats += cnt[r];
-                    cnt[r] = 0;
-                    while (r >= l && cnt[r] == 0) {
-                        r--;
-                    }
-                } else {
-                    // l + r <= limit
-                    int mP = Math.min(cnt[l], cnt[r]);
-                    boats += mP;
-                    cnt[l] -= mP;
-                    cnt[r] -= mP;
-                    while (r >= l && cnt[r] == 0) {
-                        r--;
-                    }
-                    while (l <= r && cnt[l] == 0) {
-                        l++;
-                    }
-                }
+            bucket[p]++;
+            if (p < min) {
+                min = p;
+            }
+            if (p > max) {
+                max = p;
             }
         }
+
+        int boats = 0;
+        int l = min, r = max;
+        while (l < r) {
+            if ((l + r) <= limit) {
+                int cnt = Math.min(bucket[l], bucket[r]);
+                bucket[l] -= cnt;
+                bucket[r] -= cnt;
+                boats += cnt;
+            } else {
+                boats += bucket[r];
+                bucket[r] = 0;
+            }
+
+            while (l < r && bucket[l] == 0) {
+                l++;
+            }
+            while (l < r && bucket[r] == 0) {
+                r--;
+            }
+        }
+
+        if (2 * l <= limit) {
+            boats += bucket[l] / 2 + bucket[l] % 2;
+        } else {
+            boats += bucket[l];
+        }
+        bucket[l] = 0;
+
         return boats;
     }
 }
