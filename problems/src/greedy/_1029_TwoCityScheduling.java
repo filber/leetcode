a8418c193c95caac1c1dc821ce160220fffbec7b
filber <filber@foxmail.com>
2022-03-25 13:18:54 +0800

    Review

--- problems/src/greedy/_1029_TwoCityScheduling.java
+++ problems/src/greedy/_1029_TwoCityScheduling.java
@@ -5,17 +5,12 @@ import java.util.Arrays;
 public class _1029_TwoCityScheduling {
 
     public int twoCitySchedCost(int[][] costs) {
+        int n = costs.length / 2;
         Arrays.sort(costs, (a, b) -> a[0] - a[1] - b[0] + b[1]);
-        int cost = 0;
-        final int n = costs.length;
-        final int threshold = n / 2;
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            if (i < threshold) {
-                cost += costs[i][0];
-            } else {
-                cost += costs[i][1];
-            }
+            ans += costs[i][0] + costs[i + n][1];
         }
-        return cost;
+        return ans;
     }
 }
