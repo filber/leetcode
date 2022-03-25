d32bcc25e1b2419926fd3d43283bfa2e8486a727
filber <filber@foxmail.com>
2022-03-25 17:45:49 +0800

    Review

--- problems/src/stack/_946_ValidateStackSequences.java
+++ problems/src/stack/_946_ValidateStackSequences.java
@@ -1,21 +1,24 @@
 package stack;
 
+//https://leetcode.com/problems/validate-stack-sequences/
+
 public class _946_ValidateStackSequences {
 
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        int r = 0;
         int n = pushed.length;
+        int l = 0, r = 0;
         int[] stack = new int[n];
         int top = -1;
-        for (int l = 0; l < n; l++) {
-            if (top >= 0 && popped[r] == stack[top]) {
-                l--;
+        while (l < n) {
+            if (top >= 0 && stack[top] == popped[r]) {
                 top--;
                 r++;
             } else if (pushed[l] == popped[r]) {
                 r++;
+                l++;
             } else {
                 stack[++top] = pushed[l];
+                l++;
             }
         }
 
