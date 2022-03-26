ea60f9f8c7422efd6e3095b126371cce9a8775dd
filber <filber@foxmail.com>
2022-03-26 23:28:06 +0800

    Review

--- problems/src/stack/_2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter.java
+++ problems/src/stack/_2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter.java
@@ -1,5 +1,7 @@
 package stack;
 
+//https://leetcode.com/problems/smallest-k-length-subsequence-with-occurrences-of-a-letter/
+
 public class _2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter {
 
     public String smallestSubsequence(String s, int k, char letter, int repetition) {
@@ -12,7 +14,7 @@ public class _2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter {
             letterCount += ch == letter ? 1 : 0;
         }
 
-        int curLetter = 0; // count of letter in stack
+        int stackLetter = 0; // count of letter in stack
         for (int i = 0; i < arr.length; i++) {
             while (
                 // 1. could pop
@@ -20,19 +22,19 @@ public class _2030_SmallestKLengthSubsequenceWithOccurrencesOfALetter {
                     top >= 0 // stack is not empty
                             && stack[top] > arr[i] // arr[i] is better
                             && n - i + top + 1 > k // have enough remaining chars
-                            && curLetter - (stack[top] == letter ? 1 : 0) + letterCount >= repetition // have enough letters
+                            && stackLetter - (stack[top] == letter ? 1 : 0) + letterCount >= repetition // have enough letters
                     )
                 ||
                 // 2. must pop
-                curLetter + k - top - 1 < repetition // no enough position for letter
+                stackLetter + k - top - 1 < repetition // no enough position for letter
             ) {
                 char peek = stack[top];
                 top--;
-                curLetter -= peek == letter ? 1 : 0;
+                stackLetter -= peek == letter ? 1 : 0;
             }
 
             if (top + 1 < k) {
-                curLetter += arr[i] == letter ? 1 : 0;
+                stackLetter += arr[i] == letter ? 1 : 0;
                 stack[++top] = arr[i];
             }
 
