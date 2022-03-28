1393cd3ec60582020cce74f327222dcc85eb01fd
filber <filber@foxmail.com>
2022-03-28 23:19:54 +0800

    Review

--- problems/src/dp/_1537_GetTheMaximumScore.java
+++ problems/src/dp/_1537_GetTheMaximumScore.java
@@ -5,18 +5,20 @@ package dp;
 public class _1537_GetTheMaximumScore {
 
     public int maxSum(int[] nums1, int[] nums2) {
-        int modulo = (int) (1e9 + 7);
-        int m = nums1.length, n = nums2.length;
+        int modulo = (int) 1e9 + 7;
+        int m = nums1.length;
+        int n = nums2.length;
         int l = 0, r = 0;
         long dp1 = 0, dp2 = 0;
         while (l < m && r < n) {
             if (nums1[l] < nums2[r]) {
-                dp1 = dp1 + nums1[l++];
+                dp1 += nums1[l++];
             } else if (nums1[l] > nums2[r]) {
-                dp2 = dp2 + nums2[r++];
+                dp2 += nums2[r++];
             } else {
                 long max = Math.max(dp1, dp2) + nums1[l];
-                dp1 = dp2 = max;
+                dp1 = max;
+                dp2 = max;
                 l++;
                 r++;
             }
@@ -25,11 +27,10 @@ public class _1537_GetTheMaximumScore {
         while (l < m) {
             dp1 += nums1[l++];
         }
-
         while (r < n) {
             dp2 += nums2[r++];
         }
 
-        return (int) ((Math.max(dp1, dp2)) % modulo);
+        return (int) (Math.max(dp1, dp2) % modulo);
     }
 }
