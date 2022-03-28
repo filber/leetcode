1f71b66b84676879b79fcd673bd2d921df64062c
filber <filber@foxmail.com>
2022-03-28 15:18:36 +0800

    81. Search In Rotated Sorted Array II

---
+++ problems/src/array_matrix/_81_SearchInRotatedSortedArrayII.java
@@ -0,0 +1,44 @@
+package array_matrix;
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+public class _81_SearchInRotatedSortedArrayII {
+
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            // found target!
+            if (nums[mid] == target) return true;
+
+            while (l < mid && nums[l] == nums[mid]) {
+                l++;
+            }
+            if (l == mid) {
+                l = mid + 1;
+                continue;
+            }
+
+            // right part in order
+            if (nums[mid] <= nums[r]) {
+                // target resides in right part
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            // left part in order
+            else {
+                // target resides in left part
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ test/src/array_matrix/_81_SearchInRotatedSortedArrayIITest.java
@@ -0,0 +1,34 @@
+package array_matrix;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _81_SearchInRotatedSortedArrayIITest {
+
+    _81_SearchInRotatedSortedArrayII target = null;
+
+    @Before
+    public void before() throws Exception {
+        target = new _81_SearchInRotatedSortedArrayII();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+
+    @Test
+    public void testSearch1() throws Exception {
+        boolean ans = target.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 0);
+        assertTrue(ans);
+    }
+
+    @Test
+    public void testSearch2() throws Exception {
+        boolean ans = target.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 3);
+        assertFalse(ans);
+    }
+}
