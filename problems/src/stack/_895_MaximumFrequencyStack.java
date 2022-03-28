7bdabbd62a43a5e8129bdf352baea01729244de8
filber <filber@foxmail.com>
2022-03-28 19:20:21 +0800

    Review

--- problems/src/stack/_895_MaximumFrequencyStack.java
+++ problems/src/stack/_895_MaximumFrequencyStack.java
@@ -1,40 +1,36 @@
 package stack;
 
+//https://leetcode.com/problems/maximum-frequency-stack/
+
 import java.util.*;
 
 public class _895_MaximumFrequencyStack {
 
     public static class FreqStack {
 
-        Map<Integer, Integer> freqMap = new HashMap<>();
+        Map<Integer, Integer> valMap = new HashMap<>();
         Map<Integer, Stack<Integer>> stackMap = new HashMap<>();
-
-        int maxFreq = Integer.MIN_VALUE;
+        int maxCnt = Integer.MIN_VALUE;
 
         public FreqStack() {
         }
 
         public void push(int x) {
-            int count = freqMap.getOrDefault(x, 0) + 1;
-            freqMap.put(x, count);
-            if (count > maxFreq) {
-                maxFreq = count;
-            }
-
-            Stack<Integer> stack = stackMap.computeIfAbsent(count, k -> new Stack<>());
+            int cnt = valMap.getOrDefault(x, 0) + 1;
+            valMap.put(x, cnt);
+            Stack<Integer> stack = stackMap.computeIfAbsent(cnt, k -> new Stack<>());
             stack.push(x);
+            maxCnt = Math.max(maxCnt, cnt);
         }
 
         public int pop() {
-            int res = stackMap.get(maxFreq).pop();
-
-            if (stackMap.get(maxFreq).isEmpty()) {
-                maxFreq--;
+            Stack<Integer> stack = stackMap.get(maxCnt);
+            int val = stack.pop();
+            valMap.put(val, valMap.getOrDefault(val, 1) - 1);
+            if (stack.isEmpty()) {
+                maxCnt--;
             }
-
-            int count = freqMap.get(res) - 1;
-            freqMap.put(res, count);
-            return res;
+            return val;
         }
     }
 }
