fe725247f7b7ed2e530fe395e45372a3ce2381c1
filber <filber@foxmail.com>
2022-03-28 14:58:04 +0800

    33. Search In Rotated Sorted Array

---
+++ test/src/array_matrix/_33_SearchInRotatedSortedArrayTest.java
@@ -0,0 +1,43 @@
+package array_matrix;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _33_SearchInRotatedSortedArrayTest {
+
+    _33_SearchInRotatedSortedArray target = null;
+
+    @Before
+    public void before() throws Exception {
+        target = new _33_SearchInRotatedSortedArray();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+
+    @Test
+    public void testSearch1() throws Exception {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int idx = target.search(nums, 0);
+        assertEquals(4, idx);
+    }
+
+    @Test
+    public void testSearch2() throws Exception {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int idx = target.search(nums, 3);
+        assertEquals(-1, idx);
+    }
+
+    @Test
+    public void testSearch3() throws Exception {
+        int[] nums = {1};
+        int idx = target.search(nums, 0);
+        assertEquals(-1, idx);
+    }
+}
