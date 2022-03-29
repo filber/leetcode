0ca2004e510d860f836499e277cd41a9978d7525
filber <filber@foxmail.com>
2022-03-29 13:48:04 +0800

    Review

--- problems/src/bs/_33_SearchInRotatedSortedArray.java
+++ problems/src/bs/_33_SearchInRotatedSortedArray.java
@@ -8,25 +8,19 @@ public class _33_SearchInRotatedSortedArray {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
             int mid = (l + r) / 2;
-            // found target!
-            if (nums[mid] == target) return mid;
+            if (target == nums[mid]) return mid;
 
-            // right part in order
-            if (nums[mid] <= nums[r]) {
-                // target resides in right part
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target & target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
                 if (nums[mid] < target && target <= nums[r]) {
                     l = mid + 1;
                 } else {
                     r = mid - 1;
-                }
-            }
-            // left part in order
-            else {
-                // target resides in left part
-                if (nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
                 }
             }
         }
