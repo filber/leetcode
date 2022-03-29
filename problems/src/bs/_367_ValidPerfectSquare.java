38b5d95cd1bd34f0c261a82469720d7efab168c2
filber <filber@foxmail.com>
2022-03-29 15:30:15 +0800

    Review

--- problems/src/bs/_367_ValidPerfectSquare.java
+++ problems/src/bs/_367_ValidPerfectSquare.java
@@ -3,18 +3,20 @@ package bs;
 public class _367_ValidPerfectSquare {
 
     public boolean isPerfectSquare(int num) {
-        long l = 1, r = Integer.MAX_VALUE;
+        int l = 1, r = Integer.MAX_VALUE;
         while (l <= r) {
-            long mid = l + (r - l) / 2;
-            long square = mid * mid;
-            if (square == num) {
+            int mid = l + (r - l) / 2;
+            int res = num / mid;
+            int rem = num % mid;
+            if (res == mid && rem == 0) {
                 return true;
-            } else if (square > num) {
-                r = mid - 1;
-            } else {
+            } else if (res >= mid) {
                 l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
+
         return false;
     }
 }
