36c6cfabf91172488acd1b9bd3425d7662a5c6c8
filber <filber@foxmail.com>
2022-04-01 23:38:08 +0800

    Review

--- problems/src/string/_424_LongestRepeatingCharacterReplacement.java
+++ problems/src/string/_424_LongestRepeatingCharacterReplacement.java
@@ -5,44 +5,22 @@ package string;
 public class _424_LongestRepeatingCharacterReplacement {
 
     public int characterReplacement(String s, int k) {
-        int[] charCnt = new int[26];
+        int[] count = new int[26];
+        int l = 0, r = 0;
+        int maxCount = 0;
         char[] chars = s.toCharArray();
         int n = chars.length;
-        int l = 0, r = 0;
-        int len = 0;
-        char majCh = chars[0];
-
         while (r < n) {
-            char rCh = chars[r];
-            int winLen = r - l;
-            int replace = winLen - charCnt[majCh - 'A'];
-            if (charCnt[majCh - 'A'] == charCnt[rCh - 'A'] || replace < k) {
-                // safe to expand window
-                charCnt[rCh - 'A']++;
-                r++;
-                if (charCnt[rCh - 'A'] > charCnt[majCh - 'A']) {
-                    majCh = rCh;
-                }
-            } else {
-                // replacement used up
-                char lCh = chars[l];
-                charCnt[lCh - 'A']--;
-                l++;
-                if (lCh == majCh) {
-                    // majority char might change
-                    int majCnt = charCnt[majCh - 'A'];
-                    for (int i = 0; i < 26; i++) {
-                        if (charCnt[i] > majCnt) {
-                            majCnt = charCnt[i];
-                            majCh = (char) ('A' + i);
-                        }
-                    }
-                }
+            char rCh = chars[r++];
+            int rCnt = ++count[rCh - 'A'];
+            if (rCnt > maxCount) {
+                maxCount = rCnt;
             }
-
-            len = Math.max(len, r - l);
+            if (r - l > maxCount + k) {
+                char lCh = chars[l++];
+                count[lCh - 'A']--;
+            }
         }
-
-        return len;
+        return Math.min(maxCount + k, n);
     }
 }
