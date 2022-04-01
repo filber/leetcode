a2b3cc0f61e02ddd02ab2a6b50e22522b7168bce
filber <filber@foxmail.com>
2022-04-01 23:40:54 +0800

    Review

--- problems/src/string/_424_LongestRepeatingCharacterReplacement.java
+++ problems/src/string/_424_LongestRepeatingCharacterReplacement.java
@@ -5,22 +5,22 @@ package string;
 public class _424_LongestRepeatingCharacterReplacement {
 
     public int characterReplacement(String s, int k) {
-        int[] count = new int[26];
-        int l = 0, r = 0;
-        int maxCount = 0;
         char[] chars = s.toCharArray();
+        int[] count = new int[26];
         int n = chars.length;
+        int l = 0, r = 0, majorityCnt = 0;
         while (r < n) {
             char rCh = chars[r++];
             int rCnt = ++count[rCh - 'A'];
-            if (rCnt > maxCount) {
-                maxCount = rCnt;
+            if (rCnt > majorityCnt) {
+                majorityCnt = rCnt;
             }
-            if (r - l > maxCount + k) {
+            if (r - l > majorityCnt + k) {
                 char lCh = chars[l++];
                 count[lCh - 'A']--;
             }
         }
-        return Math.min(maxCount + k, n);
+
+        return Math.min(n, majorityCnt + k);
     }
 }
