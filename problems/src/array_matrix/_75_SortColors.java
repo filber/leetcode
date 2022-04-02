f9b7daee9f3d60cb6f96769ab1fff143ffd8c334
filber <filber@foxmail.com>
2022-04-02 20:57:18 +0800

    Review

--- problems/src/array_matrix/_75_SortColors.java
+++ problems/src/array_matrix/_75_SortColors.java
@@ -1,30 +1,30 @@
 package array_matrix;
 
+//https://leetcode.com/problems/sort-colors/submissions/
+
+import java.util.Arrays;
+
 public class _75_SortColors {
 
     public void sortColors(int[] nums) {
         int zeroCnt = 0, oneCnt = 0, twoCnt = 0;
         for (int num : nums) {
-            if (num == 0) {
-                zeroCnt++;
-            } else if (num == 1) {
-                oneCnt++;
-            } else {
-                twoCnt++;
+            switch (num) {
+                case 0:
+                    zeroCnt++;
+                    break;
+                case 1:
+                    oneCnt++;
+                    break;
+                case 2:
+                    twoCnt++;
             }
         }
-        int idx = 0;
-        while (zeroCnt > 0) {
-            nums[idx++] = 0;
-            zeroCnt--;
-        }
-        while (oneCnt > 0) {
-            nums[idx++] = 1;
-            oneCnt--;
-        }
-        while (twoCnt > 0) {
-            nums[idx++] = 2;
-            twoCnt--;
-        }
+        int i = 0;
+        Arrays.fill(nums, 0, zeroCnt, 0);
+        i += zeroCnt;
+        Arrays.fill(nums, i, i + oneCnt, 1);
+        i += oneCnt;
+        Arrays.fill(nums, i, i + twoCnt, 2);
     }
 }
