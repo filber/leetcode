54ae96d70550079252d87ab20b5bdb82f2ad7812
filber <filber@foxmail.com>
2022-04-03 16:58:13 +0800

    Review

--- problems/src/dp/_1216_ValidPalindromeIII.java
+++ problems/src/dp/_1216_ValidPalindromeIII.java
@@ -28,15 +28,15 @@ public class _1216_ValidPalindromeIII {
         }
 
         for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
+            for (int i = 0, j = len - 1; j < n; i++, j = i + len - 1) {
                 if (chars[i] == chars[j]) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
                 } else {
                     dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
                 }
             }
         }
+
         return n - dp[0][n - 1] <= k;
     }
 }
