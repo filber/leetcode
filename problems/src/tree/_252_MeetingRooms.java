d2f7e4396ca55c94d9ad8870f2743e0ffef41327
filber <filber@foxmail.com>
2022-04-05 15:01:11 +0800

    Review

--- problems/src/tree/_252_MeetingRooms.java
+++ problems/src/tree/_252_MeetingRooms.java
@@ -2,15 +2,32 @@ package tree;
 
 //https://leetcode.com/problems/meeting-rooms/
 
+import java.util.Arrays;
+
 public class _252_MeetingRooms {
 
 //    Given an array of meeting time intervals consisting of start and end times
 //    [[s1,e1],[s2,e2],...] (si < ei),
 //    determine if a person could attend all meetings.
 
-    private SegmentTree root = null;
 
     public boolean attendAll(int[][] meeting) {
+        Arrays.sort(meeting, (a, b) -> a[0] - b[0]);
+        int n = meeting.length;
+        for (int i = 1; i < n; i++) {
+            int preEnd = meeting[i - 1][1];
+            int curStart = meeting[i][0];
+            if (curStart < preEnd) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private SegmentTree root = null;
+
+    public boolean attendAllST(int[][] meeting) {
 
         for (int[] m : meeting) {
             try {
