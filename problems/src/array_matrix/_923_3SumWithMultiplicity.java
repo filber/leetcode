34783349e6a447e98078d325bc05d1306b7a25cd
filber <filber@foxmail.com>
2022-04-06 23:54:42 +0800

    923. 3 Sum With Multiplicity

---
+++ problems/src/array_matrix/_923_3SumWithMultiplicity.java
@@ -0,0 +1,10 @@
+package array_matrix;
+
+//https://leetcode.com/problems/3sum-with-multiplicity/
+
+public class _923_3SumWithMultiplicity {
+
+    public int threeSumMulti(int[] arr, int target) {
+        return 0;
+    }
+}
---
+++ test/src/array_matrix/_923_3SumWithMultiplicityTest.java
@@ -0,0 +1,21 @@
+package array_matrix;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _923_3SumWithMultiplicityTest {
+
+    _923_3SumWithMultiplicity target = null;
+
+    @Before
+    public void before() throws Exception {
+        target = new _923_3SumWithMultiplicity();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+}
