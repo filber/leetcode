9bb63a7207c46a3bdda14421e2b40a89ec78d627
filber <filber@foxmail.com>
2022-04-11 15:50:37 +0800

    Review

--- problems/src/array_matrix/_923_3SumWithMultiplicity.java
+++ problems/src/array_matrix/_923_3SumWithMultiplicity.java
@@ -2,30 +2,32 @@ package array_matrix;
 
 //https://leetcode.com/problems/3sum-with-multiplicity/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _923_3SumWithMultiplicity {
 
-    long[][] count;
     int modulo = (int) 1e9 + 7;
 
     public int threeSumMulti(int[] arr, int target) {
         int[] bucket = new int[101];
-        int n = 0;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+        int n = 0;
         for (int num : arr) {
             if (bucket[num] == 0) {
                 n++;
             }
             bucket[num]++;
-            if (num > max) {
-                max = num;
-            }
             if (num < min) {
                 min = num;
             }
+            if (num > max) {
+                max = num;
+            }
         }
 
-        count = new long[n][2];
+        long[][] count = new long[n][2];
         int idx = 0;
         for (int i = min; i <= max; i++) {
             if (bucket[i] != 0) {
@@ -36,34 +38,31 @@ public class _923_3SumWithMultiplicity {
         }
 
         long ans = 0;
-
+        int modulo = (int) 1e9 + 7;
         for (int i = 0; i < count.length; i++) {
-            long val = count[i][0];
-            long cnt = count[i][1];
-            // choose one VAL
-            ans = (ans + (cnt * check(i + 1, count.length - 1, target - val)) % modulo) % modulo;
+            // choose 1 Ni
+            ans = (ans + count[i][1] * search(count, i + 1, count.length - 1, target - count[i][0])) % modulo;
 
-            // choose two VAL
-            if (cnt >= 2) {
-                int residue = (int) (target - 2 * val);
-                if (residue > val && residue <= 100) {
-                    long first = cnt * (cnt - 1) % modulo;
-                    long second = (first / 2 * bucket[residue]) % modulo;
-                    ans = (ans + second) % modulo;
+            if (count[i][1] >= 2) {
+                int residue = (int) (target - 2 * count[i][0]);
+                if (residue > count[i][0] && residue <= max) {
+                    long cnt = (count[i][1] * (count[i][1] - 1) / 2) % modulo;
+                    cnt = (cnt * bucket[residue]) % modulo;
+                    ans = (cnt + ans) % modulo;
                 }
             }
-            // choose three VAL
-            if (cnt >= 3 && 3 * val == target) {
-                long first = (cnt * (cnt - 1) / 2) % modulo;
-                long second = (first * (cnt - 2) / 3) % modulo;
-                ans = (ans + second) % modulo;
+
+            if (count[i][1] >= 3 && count[i][0] * 3 == target) {
+                long cnt = (count[i][1] * (count[i][1] - 1) / 2) % modulo;
+                cnt = (cnt * (count[i][1] - 2) / 3) % modulo;
+                ans = (ans + cnt) % modulo;
             }
         }
 
         return (int) ans;
     }
 
-    private long check(int l, int r, long target) {
+    private long search(long[][] count, int l, int r, long target) {
         if (l > r) {
             return 0;
         }
@@ -75,17 +74,18 @@ public class _923_3SumWithMultiplicity {
                 ans = (ans + cnt) % modulo;
                 l++;
                 r--;
-            } else if (sum < target) {
-                l++;
-            } else {
+            } else if (sum > target) {
                 r--;
+            } else {
+                l++;
             }
         }
 
-        if (count[l][1] >= 2 && 2 * count[l][0] == target) {
+        if (l == r && 2 * count[l][0] == target) {
             long cnt = (count[l][1] * (count[l][1] - 1) / 2) % modulo;
             ans = (ans + cnt) % modulo;
         }
+
         return ans;
     }
 }
