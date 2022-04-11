53ba22c7522d9a34c53b4afd509d1447972550a0
filber <filber@foxmail.com>
2022-04-11 13:13:38 +0800

    Review

--- problems/src/greedy/_759_EmployeeFreeTime.java
+++ problems/src/greedy/_759_EmployeeFreeTime.java
@@ -21,33 +21,31 @@ public class _759_EmployeeFreeTime {
 
     public int[][] employeeFreeTime(int[][][] schedule) {
         this.schedule = schedule;
-        int[][] slots = merge(0, schedule.length - 1);
-        int n = slots.length;
-
+        int[][] s = merge(0, schedule.length - 1);
         List<int[]> ans = new ArrayList<>();
-        int end = slots[0][1];
-        for (int j = 1; j < n; j++) {
-            if (end < slots[j][0]) {
-                ans.add(new int[]{slots[j - 1][1], slots[j][0]});
+        int n = s.length;
+        int end = s[0][1];
+        for (int i = 1; i < n; i++) {
+            if (end < s[i][0]) {
+                ans.add(new int[]{end, s[i][0]});
             }
-            end = Math.max(end, slots[j][1]);
+            end = Math.max(end, s[i][1]);
         }
         return ans.toArray(new int[0][]);
     }
 
-    private int[][] merge(int l, int r) {
-        if (l > r) {
-            return null;
-        } else if (l == r) {
+    public int[][] merge(int l, int r) {
+        if (l == r) {
             return schedule[l];
         } else {
             int mid = (l + r) / 2;
             int[][] left = merge(l, mid);
             int[][] right = merge(mid + 1, r);
-            int m = left.length, n = right.length;
-            int i = 0, j = 0;
-            int[][] ans = new int[m + n][];
-            int idx = 0;
+            int m = left.length;
+            int n = right.length;
+            int[][] ans = new int[m + n][2];
+
+            int i = 0, j = 0, idx = 0;
             while (i < m && j < n) {
                 if (left[i][0] == right[j][0]) {
                     if (left[i][1] <= right[j][1]) {
@@ -61,14 +59,15 @@ public class _759_EmployeeFreeTime {
                     ans[idx++] = right[j++];
                 }
             }
+
             while (i < m) {
                 ans[idx++] = left[i++];
             }
-
             while (j < n) {
                 ans[idx++] = right[j++];
             }
             return ans;
         }
     }
+
 }
