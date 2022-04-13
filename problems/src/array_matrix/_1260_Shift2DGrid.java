a672b0957f9542a963d9e328d53c40143d279b7b
filber <filber@foxmail.com>
2022-04-13 14:12:01 +0800

    Review

--- problems/src/array_matrix/_1260_Shift2DGrid.java
+++ problems/src/array_matrix/_1260_Shift2DGrid.java
@@ -11,14 +11,15 @@ public class _1260_Shift2DGrid {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] res = new int[m][n];
+        int[][] newGrid = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int nextJ = (j + k) % n;
                 int nextI = (i + (j + k) / n) % m;
-                res[nextI][nextJ] = grid[i][j];
+                int nextJ = (j + k) % n;
+                newGrid[nextI][nextJ] = grid[i][j];
             }
         }
-        return (List)Arrays.asList(res);
+
+        return (List) Arrays.asList(newGrid);
     }
 }
