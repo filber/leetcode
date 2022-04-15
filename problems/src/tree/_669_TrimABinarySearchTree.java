a95b6b3918852f11f8e6553bdfb207326b268cc8
filber <filber@foxmail.com>
2022-04-15 14:17:47 +0800

    Review

--- problems/src/tree/_669_TrimABinarySearchTree.java
+++ problems/src/tree/_669_TrimABinarySearchTree.java
@@ -7,13 +7,13 @@ public class _669_TrimABinarySearchTree {
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) {
             return null;
+        } else if (high < root.val) {
+            return trimBST(root.left, low, high);
         } else if (root.val < low) {
             return trimBST(root.right, low, high);
-        } else if (root.val > high) {
-            return trimBST(root.left, low, high);
         } else {
-            root.left = trimBST(root.left, low, high);
             root.right = trimBST(root.right, low, high);
+            root.left = trimBST(root.left, low, high);
             return root;
         }
     }
