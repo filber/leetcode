e9bc5e84516ae4a0d8c15db556bf36c2088d58dd
filber <filber@foxmail.com>
2022-04-24 23:09:03 +0800

    Review

--- problems/src/bs/_702_SearchInASortedArrayOfUnknownSize.java
+++ problems/src/bs/_702_SearchInASortedArrayOfUnknownSize.java
@@ -32,39 +32,21 @@ public class _702_SearchInASortedArrayOfUnknownSize {
         }
     }
 
-    ArrayReader ar;
-    int val;
-
     public int searchUnknownSize(ArrayReader ar, int val) {
-        this.ar = ar;
-        this.val = val;
-        return search(0, (int) 2e4);
-    }
-
-    private int search(int l, int r) {
-        if (l > r) {
-            return -1;
-        } else if (l == r) {
-            if (ar.get(l) == val) {
-                return l;
-            } else {
-                return -1;
-            }
-        } else {
+        int l = 0, r = 20000;
+        while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (ar.get(mid) == val) {
+            int mVal = ar.get(mid);
+            if (mVal == Integer.MAX_VALUE) {
+                r = mid - 1;
+            } else if (mVal == val) {
                 return mid;
-            } else if (ar.get(mid) == Integer.MAX_VALUE) {
-                if (mid > 0) {
-                    return search(l, mid - 1);
-                } else {
-                    return search(mid + 1, r);
-                }
-            } else if (ar.get(mid) < val) {
-                return search(mid + 1, r);
+            } else if (mVal < val) {
+                l = mid + 1;
             } else {
-                return search(l, mid - 1);
+                r = mid - 1;
             }
         }
+        return -1;
     }
 }
