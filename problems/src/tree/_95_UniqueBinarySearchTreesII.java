46964930972b9d558550ea61d0f9789fda174080
filber <filber@foxmail.com>
2022-04-24 20:12:39 +0800

    Review

--- problems/src/tree/_95_UniqueBinarySearchTreesII.java
+++ problems/src/tree/_95_UniqueBinarySearchTreesII.java
@@ -3,50 +3,51 @@ package tree;
 //https://leetcode.com/problems/unique-binary-search-trees-ii/
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _95_UniqueBinarySearchTreesII {
+
 
     public List<TreeNode> generateTrees(int n) {
         return backtracking(n, 1);
     }
 
-    private List<TreeNode> backtracking(int n, int l) {
-        List<TreeNode> list = new ArrayList<>();
+    private List<TreeNode> backtracking(int n, int start) {
         if (n == 0) {
-            return list;
+            return null;
         } else if (n == 1) {
-            list.add(new TreeNode(l));
-            return list;
+            return Arrays.asList(new TreeNode(start));
         }
 
-        for (int i = 0; i <= n - 1; i++) {
-            List<TreeNode> leftNodes = backtracking(i, l);
-            List<TreeNode> rightNodes = backtracking(n - 1 - i, l + i + 1);
-            if (leftNodes.isEmpty()) {
+        List<TreeNode> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<TreeNode> leftNodes = backtracking(i, start);
+
+            List<TreeNode> rightNodes = backtracking(n - 1 - i, start + i + 1);
+            if (leftNodes == null) {
                 for (int j = 0; j < rightNodes.size(); j++) {
-                    TreeNode root = new TreeNode(l + i);
+                    TreeNode root = new TreeNode(start + i);
                     root.right = rightNodes.get(j);
-                    list.add(root);
+                    ans.add(root);
                 }
-            } else if (rightNodes.isEmpty()) {
+            } else if (rightNodes == null) {
                 for (int j = 0; j < leftNodes.size(); j++) {
-                    TreeNode root = new TreeNode(l + i);
+                    TreeNode root = new TreeNode(start + i);
                     root.left = leftNodes.get(j);
-                    list.add(root);
+                    ans.add(root);
                 }
             } else {
                 for (int j = 0; j < leftNodes.size(); j++) {
                     for (int k = 0; k < rightNodes.size(); k++) {
-                        TreeNode root = new TreeNode(l + i);
+                        TreeNode root = new TreeNode(start + i);
                         root.left = leftNodes.get(j);
                         root.right = rightNodes.get(k);
-                        list.add(root);
+                        ans.add(root);
                     }
                 }
             }
         }
-
-        return list;
+        return ans;
     }
 }
