fcd5e52928c36d1fc5ffa1d58362db290be9b2ba
filber <filber@foxmail.com>
2022-04-24 23:22:41 +0800

    Review

--- problems/src/bs/_EX_BitonicArrayMaximum.java
+++ problems/src/bs/_EX_BitonicArrayMaximum.java
@@ -7,36 +7,22 @@ public class _EX_BitonicArrayMaximum {
 //    Given an array of integers which is initially increasing and then decreasing,
 //    find the maximum value in the array.
 
-    int[] arr;
-
     public int findMaximum(int arr[]) {
-        this.arr = arr;
-        if (arr.length == 1) {
-            return arr[0];
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            int mVal = arr[mid];
+            boolean gtLeft = mid == 0 || arr[mid - 1] < mVal;
+            boolean gtRight = mid == arr.length - 1 || mVal > arr[mid + 1];
+            if (gtLeft && gtRight) {
+                return arr[mid];
+            } else if (gtLeft) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
-        return search(0, arr.length - 1);
+        return arr[l];
     }
 
-    private int search(int l, int r) {
-        int mid = (l + r) / 2;
-        if (mid == 0) {
-            if (arr[mid] > arr[mid + 1]) {
-                return arr[mid];
-            } else {
-                return search(mid + 1, r);
-            }
-        } else if (mid == arr.length - 1) {
-            if (arr[mid - 1] < arr[mid]) {
-                return arr[mid];
-            } else {
-                return search(l, mid - 1);
-            }
-        } else if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
-            return arr[mid];
-        } else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) {
-            return search(mid + 1, r);
-        } else {
-            return search(l, mid - 1);
-        }
-    }
 }
