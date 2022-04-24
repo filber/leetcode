066479681977921b8ec519d4f4ae9141677c845e
filber <filber@foxmail.com>
2022-04-24 16:58:20 +0800

    Review

--- problems/src/dfs/_22_GenerateParentheses.java
+++ problems/src/dfs/_22_GenerateParentheses.java
@@ -4,39 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 //https://leetcode.com/problems/generate-parentheses/
+
 public class _22_GenerateParentheses {
 
-    int len;
+    int n;
     char[] chars;
     List<String> ans = new ArrayList<>();
 
     public List<String> generateParenthesis(int n) {
-        this.len = 2 * n;
-        chars = new char[len];
-
+        this.n = 2 * n;
+        chars = new char[this.n];
         backtracking(0, 0);
         return ans;
     }
 
     private void backtracking(int i, int stack) {
-        if (i == len) {
+        if (i == n) {
             ans.add(new String(chars));
             return;
         }
         if (stack == 0) {
-            // can only append '('
             chars[i] = '(';
             backtracking(i + 1, stack + 1);
-        } else if (len - i - stack == 0) {
-            // can only append ')'
+        } else if (n - i == stack) {
             chars[i] = ')';
             backtracking(i + 1, stack - 1);
         } else {
-            // try '('
             chars[i] = '(';
             backtracking(i + 1, stack + 1);
 
-            // try ')'
             chars[i] = ')';
             backtracking(i + 1, stack - 1);
         }
