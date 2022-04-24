5be493ed7aeda73caaeaa22d5734a91d436a4b95
filber <filber@foxmail.com>
2022-04-24 17:34:07 +0800

    Review

--- problems/src/tree/_96_UniqueBinarySearchTrees.java
+++ problems/src/tree/_96_UniqueBinarySearchTrees.java
@@ -9,10 +9,8 @@ public class _96_UniqueBinarySearchTrees {
         dp[0] = 1;
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            for (int j = 0; j <= i - 1; j++) {
-                int leftCnt = dp[j];
-                int rightCnt = dp[i - 1 - j];
-                dp[i] += leftCnt * rightCnt;
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
             }
         }
         return dp[n];
