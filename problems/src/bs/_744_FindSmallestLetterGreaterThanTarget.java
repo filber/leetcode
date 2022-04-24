0b3f2bce7d946be8cec94c6e640f1ce90a9019fd
filber <filber@foxmail.com>
2022-04-24 21:49:23 +0800

    Review

--- problems/src/bs/_744_FindSmallestLetterGreaterThanTarget.java
+++ problems/src/bs/_744_FindSmallestLetterGreaterThanTarget.java
@@ -7,37 +7,21 @@ public class _744_FindSmallestLetterGreaterThanTarget {
 
     //Note that the letters wrap around.
     // 'a' < 'b' < 'c' < ... < 'z' < 'a' < 'b' < ...
-    char[] letters;
-    char target;
 
     public char nextGreatestLetter(char[] letters, char target) {
-        this.letters = letters;
-        this.target = target;
-        int idx = search(0, letters.length - 1);
-        if (idx == -1) {
+        if (target < letters[0] || target > letters[letters.length - 1]) {
             return letters[0];
-        } else {
-            return letters[idx];
         }
+        int l = 0, r = letters.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (letters[mid] > target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return letters[l];
     }
 
-    private int search(int l, int r) {
-        if (l > r) {
-            return -1;
-        } else if (l == r) {
-            if (target < letters[l]) {
-                return l;
-            } else {
-                return -1;
-            }
-        } else {
-            int mid = (l + r) / 2;
-            if (letters[mid] <= target) {
-                return search(mid + 1, r);
-            } else {
-                // target < letters[mid]
-                return search(l, mid);
-            }
-        }
-    }
 }
