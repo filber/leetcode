61ecc32fc43730c8990aa25b232b3f27363077df
filber <filber@foxmail.com>
2022-04-25 15:04:56 +0800

    Review

--- problems/src/array_matrix/_832_FlippingAnImage.java
+++ problems/src/array_matrix/_832_FlippingAnImage.java
@@ -6,13 +6,12 @@ public class _832_FlippingAnImage {
 
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image.length;
-        int[][] res = new int[n][n];
-
+        int[][] ans = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                res[i][j] = 1 ^ image[i][n - 1 - j];
+                ans[i][j] = 1 ^ image[i][n - 1 - j];
             }
         }
-        return res;
+        return ans;
     }
 }
