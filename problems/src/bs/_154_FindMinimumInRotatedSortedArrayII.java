1beb1d3b820275d16cf706dd5f7554e27579d257
filber <filber@foxmail.com>
2022-04-25 14:58:31 +0800

    Review

--- problems/src/bs/_154_FindMinimumInRotatedSortedArrayII.java
+++ problems/src/bs/_154_FindMinimumInRotatedSortedArrayII.java
@@ -6,36 +6,36 @@ public class _154_FindMinimumInRotatedSortedArrayII {
 
     public int findMin(int[] nums) {
         int l = 0, r = nums.length - 1;
-        int min = nums[0];
+        int min = Integer.MAX_VALUE;
         while (l <= r) {
-            int mid = (l + r) / 2;
-            min = Math.min(min, nums[mid]);
+            int mid = l + (r - l) / 2;
+            int mVal = nums[mid];
+            min = Math.min(min, mVal);
 
-            while (l < mid && nums[l] == nums[mid]) {
+            while (l < mid && nums[l] == mVal) {
                 l++;
-            }
-            while (mid < r && nums[r] == nums[mid]) {
-                r--;
             }
             if (l == mid) {
-                l++;
+                l = mid + 1;
                 continue;
             }
-            if (r == mid) {
+            while (mid < r && nums[r] == mVal) {
                 r--;
+            }
+            if (r == mid) {
+                r = mid - 1;
                 continue;
             }
 
-            if (nums[l] <= nums[mid]) {
-                // left part in order
+            if (nums[l] < mVal) {
                 min = Math.min(min, nums[l]);
                 l = mid + 1;
             } else {
-                // right part in order
                 min = Math.min(min, nums[mid]);
                 r = mid - 1;
             }
         }
+
         return min;
     }
 }
