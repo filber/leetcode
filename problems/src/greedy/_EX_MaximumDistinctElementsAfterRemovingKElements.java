5073ecf10a75570070de8c8a6cce1b33bd1a432c
filber <filber@foxmail.com>
2022-04-30 12:42:54 +0800

    Review

--- problems/src/greedy/_EX_MaximumDistinctElementsAfterRemovingKElements.java
+++ problems/src/greedy/_EX_MaximumDistinctElementsAfterRemovingKElements.java
@@ -18,33 +18,30 @@ public class _EX_MaximumDistinctElementsAfterRemovingKElements {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(
-                (a, b) -> a[1] - b[1]
-        );
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
         int distinctCnt = 0;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if (entry.getValue() == 1) {
                 distinctCnt++;
             } else {
-                priorityQueue.add(new int[]{entry.getKey(), entry.getValue()});
+                priorityQueue.add(entry.getValue());
             }
         }
 
         while (!priorityQueue.isEmpty() && k > 0) {
-            int[] peek = priorityQueue.poll();
-            k--;
-            peek[1]--;
-            if (peek[1] == 1) {
-                distinctCnt++;
+            int peek = priorityQueue.poll();
+            if (k < peek) {
+                if (peek == k + 1) {
+                    distinctCnt++;
+                }
+                k = 0;
             } else {
-                priorityQueue.add(peek);
+                k -= (peek - 1);
+                distinctCnt++;
             }
         }
 
-        if (priorityQueue.isEmpty()) {
-            distinctCnt -= k;
-        }
-        return distinctCnt;
+        return distinctCnt - k;
     }
 }
