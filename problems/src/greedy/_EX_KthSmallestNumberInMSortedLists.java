1d00d16d8d82da8b77fd35f9765f8519c9068bba
filber <filber@foxmail.com>
2022-05-01 18:20:06 +0800

    Review

--- problems/src/greedy/_EX_KthSmallestNumberInMSortedLists.java
+++ problems/src/greedy/_EX_KthSmallestNumberInMSortedLists.java
@@ -7,26 +7,22 @@ import java.util.PriorityQueue;
 public class _EX_KthSmallestNumberInMSortedLists {
 
     public int kthSmallest(int[][] arr, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         for (int i = 0; i < arr.length; i++) {
-            int val = arr[i][0];
-            // list id, cursor of list i, current head of list
-            pq.add(new int[]{i, 0, val});
+            int[] elem = new int[]{arr[i][0], i, 0};
+            pq.add(elem);
         }
 
         int ans = 0;
         for (int i = 0; i < k; i++) {
-            int[] triple = pq.poll();
-            ans = triple[2];
-
-            triple[1]++;
-            if (triple[1] < arr[triple[0]].length) {
-                triple[2] = arr[triple[0]][triple[1]];
-                pq.add(triple);
+            int[] peek = pq.poll();
+            ans = peek[0];
+            peek[2]++;
+            if (peek[2] != arr[peek[1]].length) {
+                peek[0] = arr[peek[1]][peek[2]];
+                pq.add(peek);
             }
         }
-
         return ans;
     }
 }
