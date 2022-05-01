1dcd245a011e82e17fc4b611fe0ef69a05c46bb7
filber <filber@foxmail.com>
2022-05-01 11:19:31 +0800

    Review

--- problems/src/string/_844_BackspaceStringCompare.java
+++ problems/src/string/_844_BackspaceStringCompare.java
@@ -6,44 +6,40 @@ public class _844_BackspaceStringCompare {
 
     public boolean backspaceCompare(String s, String t) {
         char[] sChars = s.toCharArray();
-        int l = sChars.length - 1;
-        char[] tChars = t.toCharArray();
-        int r = tChars.length - 1;
-
-        while (l >= 0 || r >= 0) {
-            l = move(sChars, l);
-            r = move(tChars, r);
-            if (l == -1 && r == -1) {
-                return true;
-            } else if (l >= 0 && r >= 0) {
-                if (sChars[l] == tChars[r]) {
-                    l--;
-                    r--;
-                } else {
-                    return false;
+        char[] sStack = new char[sChars.length];
+        int sTop = -1;
+        for (char ch : sChars) {
+            if (ch == '#') {
+                if (sTop >= 0) {
+                    sTop--;
                 }
             } else {
+                sStack[++sTop] = ch;
+            }
+        }
+
+        char[] tChars = t.toCharArray();
+        char[] tStack = new char[tChars.length];
+        int tTop = -1;
+        for (char ch : tChars) {
+            if (ch == '#') {
+                if (tTop >= 0) {
+                    tTop--;
+                }
+            } else {
+                tStack[++tTop] = ch;
+            }
+        }
+
+        if (sTop != tTop) {
+            return false;
+        }
+        for (int i = 0; i <= sTop; i++) {
+            if (sStack[i] != tStack[i]) {
                 return false;
             }
         }
-
         return true;
     }
 
-    private int move(char[] chars, int i) {
-        int skip = 0;
-        while (i >= 0) {
-            if (chars[i] == '#') {
-                skip++;
-                i--;
-            } else if (skip > 0) {
-                skip--;
-                i--;
-            } else {
-                return i;
-            }
-        }
-
-        return i;
-    }
 }
