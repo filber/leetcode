4e7ecfcf0aa5567325cd1ba769e90c933391c50a
filber <filber@foxmail.com>
2022-05-02 13:20:27 +0800

    Review

--- problems/src/dfs/_EX_SubsetSum.java
+++ problems/src/dfs/_EX_SubsetSum.java
@@ -4,41 +4,39 @@ package dfs;
 
 public class _EX_SubsetSum {
 
-    public boolean isSubsetSum(int[] nums, int sum) {
+    public boolean isSubsetSumDFS(int[] nums, int sum) {
         Boolean[] memo = new Boolean[sum + 1];
         memo[0] = true;
-
         return dfs(nums, memo, 0, sum);
     }
 
     private boolean dfs(int[] nums, Boolean[] memo, int i, int sum) {
-        if (i == nums.length) {
+        if (sum < 0) {
+            return false;
+        } else if (i == nums.length) {
             return sum == 0;
         } else if (memo[sum] != null) {
             return memo[sum];
         }
-
-        if (sum - nums[i] >= 0 && dfs(nums, memo, i + 1, sum - nums[i])) {
+        if (dfs(nums, memo, i + 1, sum - nums[i])) {
+            memo[sum] = true;
             return true;
         }
-
         memo[sum] = dfs(nums, memo, i + 1, sum);
         return memo[sum];
     }
 
-    public boolean isSubsetSumKnapsack(int[] nums, int sum) {
-        int n = nums.length;
+    public boolean isSubsetSum(int[] nums, int sum) {
         boolean[] dp = new boolean[sum + 1];
-        dp[0] = true; // the sum of an empty set is 0
-        for (int i = 0; i < n; i++) {
-            for (int j = sum; j > 0; j--) {
-                int w = nums[i];
-                if (j - w >= 0 && dp[j - w]) {
+        dp[0] = true;
+        for (int i = 0; i < nums.length; i++) {
+            int w = nums[i];
+            for (int j = sum; j >= w; j--) {
+                if (dp[j - w]) {
                     dp[j] = true;
                 }
             }
         }
-
         return dp[sum];
     }
 }
