2c4b61279ce8ae1df316a359e96546e22d380f5e
filber <filber@foxmail.com>
2022-05-02 12:15:44 +0800

    Review

--- problems/src/hmap/_705_DesignHashSet.java
+++ problems/src/hmap/_705_DesignHashSet.java
@@ -19,62 +19,39 @@ public class _705_DesignHashSet {
         Node[] nodes = new Node[size];
 
         public MyHashSet() {
+            for (int i = 0; i < size; i++) {
+                nodes[i] = new Node(i); // dummy node
+            }
+        }
+
+        private Node searchPre(int key) {
+            int id = key % 100;
+            Node pre = nodes[id];
+            while (pre.next != null && pre.next.val != key) {
+                pre = pre.next;
+            }
+            return pre;
         }
 
         public void add(int key) {
-            int idx = key % size;
-            if (nodes[idx] == null) {
-                nodes[idx] = new Node(key);
-                return;
-            }
-            Node pre = null;
-            Node cur = nodes[idx];
-            while (cur != null && cur.val != key) {
-                pre = cur;
-                cur = cur.next;
-            }
-            if (cur == null && pre != null) {
-                // reach the end of the list and didn't find key
-                // add to the end
+            Node pre = searchPre(key);
+            if (pre.next == null) {
                 pre.next = new Node(key);
             }
         }
 
         public void remove(int key) {
-            int idx = key % size;
-            if (nodes[idx] == null) {
-                return;
-            }
-            Node pre = null;
-            Node cur = nodes[idx];
-            while (cur != null && cur.val != key) {
-                pre = cur;
-                cur = cur.next;
-            }
-
-            if (cur != null) {
-                // find key
-                if (pre == null) {
-                    // delete head node
-                    nodes[idx] = nodes[idx].next;
-                } else {
-                    pre.next = cur.next;
-                }
-                cur.next = null; // prevent memory leak
+            Node pre = searchPre(key);
+            if (pre.next != null) {
+                Node node = pre.next;
+                pre.next = node.next;
+                node.next = null;
             }
         }
 
         public boolean contains(int key) {
-            int idx = key % size;
-            if (nodes[idx] == null) {
-                return false;
-            }
-            Node cur = nodes[idx];
-            while (cur != null && cur.val != key) {
-                cur = cur.next;
-            }
-
-            return cur != null;
+            Node pre = searchPre(key);
+            return pre.next != null;
         }
     }
 
