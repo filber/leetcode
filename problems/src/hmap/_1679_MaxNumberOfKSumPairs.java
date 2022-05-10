1ac18f1fd152e2495d73c2223fccfa0b3bd6caf8
filber <filber@foxmail.com>
2022-05-10 13:08:37 -0400

    Review

--- problems/src/hmap/_1679_MaxNumberOfKSumPairs.java
+++ problems/src/hmap/_1679_MaxNumberOfKSumPairs.java
@@ -2,12 +2,32 @@ package hmap;
 
 //https://leetcode.com/problems/max-number-of-k-sum-pairs/
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class _1679_MaxNumberOfKSumPairs {
 
     public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0, r = nums.length - 1;
+        int operations = 0;
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+            if (sum == k) {
+                operations += 1;
+                l++;
+                r--;
+            } else if (sum < k) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return operations;
+    }
+
+    public int maxOperationsHM(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int val : nums) {
             map.put(val, map.getOrDefault(val, 0) + 1);
