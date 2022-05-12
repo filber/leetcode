93fc26478434d7f46885cd9ad59c07abb5d97ace
filber <filber@foxmail.com>
2022-05-12 15:30:27 -0400

    47. Permutations II

--- problems/src/dfs/_47_PermutationsII.java
+++ problems/src/dfs/_47_PermutationsII.java
@@ -8,30 +8,43 @@ import java.util.List;
 
 public class _47_PermutationsII {
 
+    List<List<Integer>> ans = new ArrayList<>();
+    int[] nums;
+    int n;
+    boolean[] used;
+    int[] seq;
+
     public List<List<Integer>> permuteUnique(int[] nums) {
+        this.nums = nums;
+        this.n = nums.length;
+        used = new boolean[n];
+        seq = new int[n];
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        int[] seq = new int[nums.length];
-        boolean[] used = new boolean[nums.length];
-        backtracking(ans, nums, used, seq, 0);
+
+        backtracking(0);
+
         return ans;
     }
 
-    private void backtracking(List<List<Integer>> ans, int[] nums, boolean[] used, int[] seq, int i) {
-        if (i == nums.length) {
-            List<Integer> list = new ArrayList<>(seq.length);
-            for (int elem : seq) {
-                list.add(elem);
+    private void backtracking(int i) {
+        if (i == n) {
+            List<Integer> list = new ArrayList<>();
+            for (int val : seq) {
+                list.add(val);
             }
             ans.add(list);
             return;
         }
-        for (int j = 0; j < nums.length; j++) {
-            if (used[j]) continue;
-            if (j > 0 && nums[j] == nums[j - 1] && !used[j - 1]) continue;
-            used[j] = true;
+        for (int j = 0; j < n; j++) {
+            if (used[j]) {
+                continue;
+            }
+            if (j > 0 && nums[j - 1] == nums[j] && !used[j - 1]) {
+                continue;
+            }
             seq[i] = nums[j];
-            backtracking(ans, nums, used, seq, i + 1);
+            used[j] = true;
+            backtracking(i + 1);
             used[j] = false;
         }
     }
