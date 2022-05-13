1197177324e204f2b7c8e30748705cd8c92264b0
filber <filber@foxmail.com>
2022-05-13 18:26:56 -0400

    1584. Min Cost To Connect All Points

--- problems/src/tree/_1584_MinCostToConnectAllPoints.java
+++ problems/src/tree/_1584_MinCostToConnectAllPoints.java
@@ -7,113 +7,89 @@ import java.util.PriorityQueue;
 
 public class _1584_MinCostToConnectAllPoints {
 
-
     // Prime
     public int minCostConnectPoints(int[][] points) {
-        if (points.length < 2) {
-            return 0;
-        }
-
         int n = points.length;
-        boolean[] inMST = new boolean[n];
-        int[] curMinDistToEachNode = new int[n];
-        int cost = 0;
+        int[] minDistances = new int[n];
+        Arrays.fill(minDistances, Integer.MAX_VALUE);
+        boolean[] merged = new boolean[n];
+        merged[0] = true;
+        int i = 0;
         int edges = 0;
-        int cur = 0;
-
+        int cost = 0;
         while (edges < n - 1) {
-            int minIndex = 0;
-            int minCost = Integer.MAX_VALUE;
-            inMST[cur] = true;
+            int minJ = 0, minCost = Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {
+                if (merged[j]) {
+                    continue;
+                }
 
-            for (int i = 0; i < n; ++i) {
-                if (!inMST[i]) {
-                    int dist = Math.abs(points[cur][0] - points[i][0]) + Math.abs(points[cur][1] - points[i][1]);
-                    if (cur == 0 || curMinDistToEachNode[i] > dist) {
-                        curMinDistToEachNode[i] = dist;
-                    }
+                int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                minDistances[j] = Math.min(minDistances[j], dist);
 
-                    if (curMinDistToEachNode[i] < minCost) {
-                        minCost = curMinDistToEachNode[i];
-                        minIndex = i;
-                    }
+                if (minDistances[j] < minCost) {
+                    minCost = minDistances[j];
+                    minJ = j;
                 }
             }
-
             cost += minCost;
-            cur = minIndex;
+            i = minJ;
+            merged[i] = true;
             edges++;
         }
-
-
         return cost;
     }
 
     // Kruskal
     public int minCostConnectPointsKruskal(int[][] points) {
         int n = points.length;
-        int[][] edges = new int[n * (n - 1) / 2][];
-
-        id = new int[n];
-        sz = new int[n];
-
-        int idx = 0;
+        int[][] edges = new int[n * (n - 1) / 2][3];
+        int edgeIdx = 0;
+        int[] id = new int[n];
+        int[] sz = new int[n];
         for (int i = 0; i < n; i++) {
-            // union find init
-            sz[i] = 1;
             id[i] = i;
-
+            sz[i] = 1;
             for (int j = i + 1; j < n; j++) {
-                int[] edge = new int[3];
-                edge[0] = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                edge[1] = i;
-                edge[2] = j;
-                edges[idx++] = edge;
+                int dis = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges[edgeIdx][0] = dis;
+                edges[edgeIdx][1] = i;
+                edges[edgeIdx][2] = j;
+                edgeIdx++;
             }
         }
 
         Arrays.sort(edges, (a, b) -> a[0] - b[0]);
-
         int count = 0;
         int minCost = 0;
-        for (int[] edge : edges) {
+        for (int i = 0; count < n - 1; i++) {
+            int[] edge = edges[i];
+            int cost = edge[0];
             int a = edge[1];
             int b = edge[2];
-            if (root(a) != root(b)) {
-                union(a, b);
+            int aRoot = root(id, a);
+            int bRoot = root(id, b);
+            if (aRoot != bRoot) {
                 count++;
-                minCost += edge[0];
-            }
-            if (count == n - 1) {
-                break;
+                minCost += cost;
+                union(id, sz, aRoot, bRoot);
             }
         }
-
         return minCost;
     }
 
-    private int[] id;
-    private int[] sz;
-
-    public void union(int p, int q) {
-        int pid = root(p);
-        int qid = root(q);
-
-        //Weighted-Quick-Union
-        if (sz[p] < sz[q]) {
-            // p is smaller
-            id[pid] = qid; // attach p to q
-            sz[q] += sz[p];
-        } else {
-            // q is smaller
-            id[qid] = pid; // attach q to p
+    private void union(int[] id, int[] sz, int p, int q) {
+        if (sz[p] > sz[q]) {
+            id[q] = p;
             sz[p] += sz[q];
+        } else {
+            id[p] = q;
+            sz[q] += sz[p];
         }
     }
 
-    public int root(int i) {
-        while (i != id[i]) {
-            // path compression
+    int root(int[] id, int i) {
+        while (id[i] != i) {
             id[i] = id[id[i]];
             i = id[i];
         }
