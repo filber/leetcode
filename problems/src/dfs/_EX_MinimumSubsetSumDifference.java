5cca0031c21cd2ffb0fcf0dce26688c9d8fa0a6f
filber <filber@foxmail.com>
2022-05-14 18:14:22 -0400

    Review

--- problems/src/dfs/_EX_MinimumSubsetSumDifference.java
+++ problems/src/dfs/_EX_MinimumSubsetSumDifference.java
@@ -5,43 +5,42 @@ import java.util.Arrays;
 public class _EX_MinimumSubsetSumDifference {
 
     int minDiff = Integer.MAX_VALUE;
-    int sum;
-    int[] nums;
+    int totalSum = 0;
 
-    // Time Complexity: O(n^2)
-    public int minSumDiff(int[] nums) {
-        this.nums = nums;
-        sum = Arrays.stream(nums).sum();
-        dfs(0, 0);
+    public int minSumDiffDFS(int[] nums) {
+        for (int val : nums) {
+            totalSum += val;
+        }
+        dfs(nums, 0, 0);
         return minDiff;
     }
 
-    private void dfs(int i, int curSum) {
+    private void dfs(int[] nums, int sum, int i) {
         if (i == nums.length) {
-            minDiff = Math.min(minDiff, Math.abs(2 * curSum - sum));
+            minDiff = Math.min(minDiff, Math.abs(totalSum - 2 * sum));
             return;
         }
-        dfs(i + 1, curSum);
-        dfs(i + 1, curSum + nums[i]);
+        dfs(nums, sum + nums[i], i + 1);
+        dfs(nums, sum, i + 1);
     }
 
-    // Time Complexity : O(n*sum)
-    public int minSumDiffKnapsack(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
+    public int minSumDiff(int[] nums) {
+        int sum = 0;
+        for (int val : nums) {
+            sum += val;
+        }
         boolean[] dp = new boolean[sum + 1];
-        dp[0] = true;
-        int minDiff = Integer.MAX_VALUE;
-
+        dp[0] = true; // EMPTY SET
+        int diff = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
+            int w = nums[i];
             for (int j = sum; j > 0; j--) {
-                int w = nums[i];
-                if (j >= w && dp[j - w]) {
-                    dp[j] = true;
-                    minDiff = Math.min(minDiff, Math.abs(2 * j - sum));
+                if (j >= w) {
+                    dp[j] = dp[j - w];
+                    diff = Math.min(diff, Math.abs(sum - j - j));
                 }
             }
         }
-
-        return minDiff;
+        return diff;
     }
 }
--- test/src/dfs/_EX_MinimumSubsetSumDifferenceTest.java
+++ test/src/dfs/_EX_MinimumSubsetSumDifferenceTest.java
@@ -19,33 +19,37 @@ public class _EX_MinimumSubsetSumDifferenceTest {
     public void after() throws Exception {
     }
 
-
     @Test
     public void testMinSumDiff1() throws Exception {
+        // [1,5,6] vs [11]
         int diff = target.minSumDiff(new int[]{1, 6, 11, 5});
         assertEquals(1, diff);
     }
 
     @Test
     public void testMinSumDiff2() throws Exception {
+        // [1,5,6] vs [12]
         int diff = target.minSumDiff(new int[]{1, 6, 12, 5});
         assertEquals(0, diff);
     }
 
     @Test
     public void testMinSumDiff3() throws Exception {
+        // [1,2,3] vs [5]
         int diff = target.minSumDiff(new int[]{1, 2, 3, 5});
         assertEquals(1, diff);
     }
 
     @Test
     public void testMinSumDiff4() throws Exception {
+        // [1,4] vs [2,3]
         int diff = target.minSumDiff(new int[]{1, 2, 3, 4});
         assertEquals(0, diff);
     }
 
     @Test
     public void testMinSumDiff5() throws Exception {
+        // [1] vs [2]
         int diff = target.minSumDiff(new int[]{1, 2});
         assertEquals(1, diff);
     }
