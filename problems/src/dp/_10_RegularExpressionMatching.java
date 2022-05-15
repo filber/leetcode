8f1b3469fba090f277576e52a81a48ec86b08a67
filber <filber@foxmail.com>
2022-05-15 12:35:17 -0400

    Review

--- problems/src/dp/_10_RegularExpressionMatching.java
+++ problems/src/dp/_10_RegularExpressionMatching.java
@@ -3,63 +3,68 @@ package dp;
 //https://leetcode.com/problems/regular-expression-matching/\
 
 
+import java.util.Arrays;
+
 public class _10_RegularExpressionMatching {
 
-    char[] sChars;
-    char[] pChars;
 
     public boolean isMatch(String s, String p) {
-        sChars = s.toCharArray();
-        pChars = convert(p);
-        int m = sChars.length;
-        int n = pChars.length;
-
+        char[] P = convertPattern(p.toCharArray());
+        char[] S = s.toCharArray();
+        int m = S.length;
+        int n = P.length;
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
         for (int j = 1; j <= n; j++) {
-            dp[0][j] = dp[0][j - 1] && (pChars[j - 1] == '#' || Character.isUpperCase(pChars[j - 1]));
+            if (dp[0][j - 1] && isUpper(P[j - 1])) {
+                dp[0][j] = true;
+            } else {
+                break;
+            }
         }
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                char pCh = pChars[j - 1];
-                if (pCh == '.' || Character.isLowerCase(pCh)) {
-                    dp[i][j] = dp[i - 1][j - 1] && (pCh == '.' || sChars[i - 1] == pCh);
+                char pCh = P[j - 1];
+                if (isLower(pCh)) {
+                    dp[i][j] = dp[i - 1][j - 1] && (pCh == '.' || S[i - 1] == pCh);
                 } else {
                     int k = i;
-                    pCh = pCh == '#' ? pCh : Character.toLowerCase(pCh);
-                    while (k > 0 && (pCh == '#' || sChars[k - 1] == pCh) && !dp[k][j - 1]) {
+                    while (k > 0 && !dp[k][j - 1] && (S[k - 1] - 32 == pCh || pCh == '#')) {
                         k--;
                     }
                     dp[i][j] = dp[k][j - 1];
                 }
             }
         }
-
         return dp[m][n];
     }
 
-    private char[] convert(String pattern) {
-        char[] pChars = pattern.toCharArray();
-        int startCnt = 0;
-        for (char ch : pChars) {
-            if (ch == '*') {
-                startCnt++;
-            }
-        }
-        int n = pChars.length - startCnt;
-        char[] rst = new char[n];
-        int i = n - 1;
-        for (int j = pChars.length - 1; j >= 0; j--) {
-            char pch = pChars[j];
-            if (pch == '*') {
-                pch = pChars[--j];
-                rst[i--] = pch == '.' ? '#' : Character.toUpperCase(pch);
-            } else {
-                rst[i--] = pch;
-            }
-        }
+    private boolean isUpper(char ch) {
+        return !isLower(ch);
+    }
 
-        return rst;
+    private boolean isLower(char ch) {
+        return ch == '.' || (ch >= 'a' && ch <= 'z');
+    }
+
+    char[] convertPattern(char[] pattern) {
+        char[] ans = new char[pattern.length];
+        int idx = ans.length - 1;
+        for (int i = pattern.length - 1; i >= 0; i--) {
+            char ch = pattern[i];
+            if (ch == '*') {
+                char preceding = pattern[i - 1];
+                if (preceding == '.') {
+                    ans[idx--] = '#';
+                } else {
+                    ans[idx--] = (char) (preceding - 32);
+                }
+                i--;
+            } else {
+                ans[idx--] = ch;
+            }
+        }
+        return Arrays.copyOfRange(ans, idx + 1, ans.length);
     }
 }
