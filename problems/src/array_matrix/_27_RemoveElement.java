c3444e2db433edeec3089d25c2ea46d6bc9797a4
filber <filber@foxmail.com>
2022-05-15 17:11:39 -0400

    Review

--- problems/src/array_matrix/_27_RemoveElement.java
+++ problems/src/array_matrix/_27_RemoveElement.java
@@ -5,12 +5,14 @@ package array_matrix;
 public class _27_RemoveElement {
 
     public int removeElement(int[] nums, int val) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[j++] = nums[i];
+        int l = 0,r=0;
+        while(r<nums.length) {
+            if(nums[r]!=val) {
+                nums[l] = nums[r];
+                l++;
             }
+            r++;
         }
-        return j;
+        return l;
     }
 }
