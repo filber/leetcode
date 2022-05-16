41540e115266f2e5f7fec35d12057d89ccc4a140
filber <filber@foxmail.com>
2022-05-16 16:21:34 -0400

    Review

--- problems/src/graph/_743_NetworkDelayTime.java
+++ problems/src/graph/_743_NetworkDelayTime.java
@@ -13,50 +13,43 @@ public class _743_NetworkDelayTime {
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
-
-        for (int[] time : times) {
-            int source = time[0];
-            int target = time[1];
-            int weight = time[2];
-            List<int[]> edges = graph[source];
-            edges.add(new int[]{target, weight});
+        for (int[] edge : times) {
+            int source = edge[0];
+            int target = edge[1];
+            int weight = edge[2];
+            graph[source].add(new int[]{target, weight});
         }
 
-        // 2. Breadth-First Search
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         queue.add(new int[]{k, 0});
         while (!queue.isEmpty()) {
-            // PQ always poll the shortest Edge
             int[] node = queue.poll();
-            int source = node[0];
-            int time = node[1];
-            if (time > dp[source]) {
-                continue; // no need to traverse edges of source
+            int i = node[0];
+            int t = node[1];
+            if (t >= dp[i]) {
+                continue;
             }
-            dp[source] = time;
-            for (int[] edge : graph[source]) {
-                int target = edge[0];
-                int weight = edge[1];
-                int nextTime = time + weight;
-                if (dp[target] > nextTime) {
-                    queue.add(new int[]{target, nextTime});
-                    dp[target] = nextTime;
+            dp[i] = t;
+            for (int[] entry : graph[i]) {
+                int target = entry[0];
+                int w = entry[1];
+                int nextT = t + w;
+                if (nextT < dp[target]) {
+                    queue.add(new int[]{target, nextT});
                 }
             }
         }
 
-        // 3. Traverse dp for max time
-        int maxTime = Integer.MIN_VALUE;
-        for (int time : dp) {
-            if (time == Integer.MAX_VALUE) {
+        int maxDelay = Integer.MIN_VALUE;
+        for (int delay : dp) {
+            if (delay == Integer.MAX_VALUE) {
                 return -1;
-            } else if (time > maxTime) {
-                maxTime = time;
             }
+            maxDelay = Math.max(maxDelay, delay);
         }
-        return maxTime;
+        return maxDelay;
     }
 }
