e03fd8bb79af0f43bf460f627341a743add02303
filber <filber@foxmail.com>
2022-05-25 23:21:14 -0400

    191. Number of 1 bits

---
+++ problems/src/math/_191_NumberOf1Bits.java
@@ -0,0 +1,15 @@
+package math;
+
+//https://leetcode.com/problems/number-of-1-bits/
+
+public class _191_NumberOf1Bits {
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+}
