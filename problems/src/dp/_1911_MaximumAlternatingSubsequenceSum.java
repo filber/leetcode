1e8ee2a6199939f5b45d9cb387714e7d905def19
filber <filber@foxmail.com>
2022-06-11 14:46:00 -0400

    Review

--- problems/src/dp/_1911_MaximumAlternatingSubsequenceSum.java
+++ problems/src/dp/_1911_MaximumAlternatingSubsequenceSum.java
@@ -7,11 +7,11 @@ public class _1911_MaximumAlternatingSubsequenceSum {
     public long maxAlternatingSum(int[] nums) {
         long odd = nums[0];
         long even = 0;
-
         for (int i = 1; i < nums.length; i++) {
-            odd = Math.max(nums[i], Math.max(even + nums[i], odd));
-            even = Math.max(even, odd - nums[i]);
+            long tmpOdd = odd;
+            odd = Math.max(odd, Math.max(even + nums[i], nums[i]));
+            even = Math.max(even, tmpOdd - nums[i]);
         }
-        return Math.max(odd, even);
+        return Math.max(even, odd);
     }
 }
