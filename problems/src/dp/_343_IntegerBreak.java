3cbee328db8d613d518b2059d4482a0ad287bce6
filber <filber@foxmail.com>
2022-06-11 16:45:20 -0400

    Reivew

--- problems/src/dp/_343_IntegerBreak.java
+++ problems/src/dp/_343_IntegerBreak.java
@@ -8,12 +8,9 @@ public class _343_IntegerBreak {
         int[] dp = new int[n + 1];
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            int max = 1;
-            for (int j = i - 1; j >= 1; j--) {
-                int residual = i - j;
-                max = Math.max(max, Math.max(residual * j, residual * dp[j]));
+            for (int j = 1; j <= i - 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * dp[i - j], j * (i - j)));
             }
-            dp[i] = max;
         }
         return dp[n];
     }
