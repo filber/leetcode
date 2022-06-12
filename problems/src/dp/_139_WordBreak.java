aaa44e103c68c54fbea47f853ea22265a496fd12
filber <filber@foxmail.com>
2022-06-11 21:10:45 -0400

    Reivew

--- problems/src/dp/_139_WordBreak.java
+++ problems/src/dp/_139_WordBreak.java
@@ -1,5 +1,7 @@
 package dp;
 
+//https://leetcode.com/problems/word-break/
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,27 +9,21 @@ import java.util.Set;
 public class _139_WordBreak {
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        int wordLen = 0;
-        Set<String> set = new HashSet<>();
-        for (String word : wordDict) {
-            wordLen = Math.max(wordLen, word.length());
-            set.add(word);
-        }
-
+        Set<String> set = new HashSet<>(wordDict);
         char[] chars = s.toCharArray();
         int n = chars.length;
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
         for (int i = 1; i <= n; i++) {
-            for (int len = 1; len <= wordLen && i - len >= 0; len++) {
-                int j = i - len;
-                String word = new String(chars, j, len);
-                if (set.contains(word) && dp[j]) {
+            for (int j = i - 1; j >= 0; j--) {
+                String str = s.substring(j, i);
+                if (wordDict.contains(str) && dp[j]) {
                     dp[i] = true;
                     break;
                 }
             }
         }
+
         return dp[n];
     }
 }
