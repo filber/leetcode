05f1da697a3664efa282859fb88447040ee8a37d
filber <filber@foxmail.com>
2022-07-14 19:14:26 -0400

    1696. Jump Game VI

---
+++ test/src/dp/_1696_JumpGameVITest.java
@@ -0,0 +1,40 @@
+package dp;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _1696_JumpGameVITest {
+
+    _1696_JumpGameVI target = null;
+
+    @Before
+    public void before() throws Exception {
+        target = new _1696_JumpGameVI();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+
+    @Test
+    public void testMaxResult1() throws Exception {
+        int score = target.maxResult(new int[]{1, -1, -2, 4, -7, 3}, 2);
+        assertEquals(7, score);
+    }
+
+    @Test
+    public void testMaxResult2() throws Exception {
+        int score = target.maxResult(new int[]{10, -5, -2, 4, 0, 3}, 3);
+        assertEquals(17, score);
+    }
+
+    @Test
+    public void testMaxResult3() throws Exception {
+        int score = target.maxResult(new int[]{1, -5, -20, 4, -1, 3, -6, -3}, 2);
+        assertEquals(0, score);
+    }
+}
