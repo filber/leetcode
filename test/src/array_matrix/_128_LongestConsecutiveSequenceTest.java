da2699701907e49229b6fad03b51cc629d9eda53
Filber <filber@foxmail.com>
2022-07-13 23:31:24 -0400

    128. Longest Consecutive Sequence

---
+++ test/src/array_matrix/_128_LongestConsecutiveSequenceTest.java
@@ -0,0 +1,46 @@
+package array_matrix;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+public class _128_LongestConsecutiveSequenceTest {
+
+    _128_LongestConsecutiveSequence target;
+
+    @Before
+    public void before() throws Exception {
+        target = new _128_LongestConsecutiveSequence();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+
+    @Test
+    public void testLongestConsecutive1() throws Exception {
+        int len = target.longestConsecutive(new int[]{100, 4, 200, 1, 3, 2});
+        assertEquals(4, len);
+    }
+
+    @Test
+    public void testLongestConsecutive2() throws Exception {
+        int len = target.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1});
+        assertEquals(9, len);
+    }
+
+    @Test
+    public void testLongestConsecutive3() throws Exception {
+        int len = target.longestConsecutive(new int[]{0, 0, 1, 1, 1, 2});
+        assertEquals(3, len);
+    }
+
+    @Test
+    public void testLongestConsecutive4() throws Exception {
+        int len = target.longestConsecutive(new int[]{0, 0, 1, 1, 1, 2, 5});
+        assertEquals(3, len);
+    }
+}
