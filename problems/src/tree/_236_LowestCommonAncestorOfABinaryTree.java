1acc9042f96d3d3663bff3898b3717f231b58b15
filber <filber@foxmail.com>
2022-07-25 22:24:03 -0400

    Review

--- problems/src/tree/_236_LowestCommonAncestorOfABinaryTree.java
+++ problems/src/tree/_236_LowestCommonAncestorOfABinaryTree.java
@@ -2,57 +2,20 @@ package tree;
 
 //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
-import java.util.Stack;
-
 public class _236_LowestCommonAncestorOfABinaryTree {
 
-    Stack<TreeNode> pStack = new Stack<>();
-    boolean pFound = false;
-
-    Stack<TreeNode> qStack = new Stack<>();
-    boolean qFound = false;
-
-    TreeNode p;
-    TreeNode q;
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        this.p = p;
-        this.q = q;
-        traverse(root);
-
-        int i = 0;
-        while (i < pStack.size() && i < qStack.size() && pStack.get(i) == qStack.get(i)) {
-            i++;
+        if (root == null || p == root || q == root) {
+            return root;
         }
-
-        return pStack.get(i - 1);
-    }
-
-    private void traverse(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        if (!pFound) {
-            pStack.push(root);
-            pFound = root.val == p.val;
-        }
-
-        if (!qFound) {
-            qStack.push(root);
-            qFound = root.val == q.val;
-        }
-
-        if (!pFound || !qFound) {
-            traverse(root.left);
-            traverse(root.right);
-        }
-
-        if (!pFound) {
-            pStack.pop();
-        }
-        if (!qFound) {
-            qStack.pop();
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else {
+            return right;
         }
     }
 }
