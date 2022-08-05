18c0cc1ac0a4e1961f738b498975c1de2c5cfb99
filber <filber@foxmail.com>
2022-08-05 14:34:26 -0400

    Review

--- problems/src/dp/_377_CombinationSumIV.java
+++ problems/src/dp/_377_CombinationSumIV.java
@@ -7,15 +7,13 @@ public class _377_CombinationSumIV {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
         dp[0] = 1;
-
-        for (int i = 1; i <= target; i++) {
-            for (int num : nums) {
-                if (i - num >= 0) {
-                    dp[i] += dp[i - num];
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
                 }
             }
         }
-
         return dp[target];
     }
 }
