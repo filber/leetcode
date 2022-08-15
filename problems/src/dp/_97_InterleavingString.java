261259c07fa48181325e78d017a5d831508fdb25
filber <filber@foxmail.com>
2022-08-15 09:11:51 -0400

    Review

--- problems/src/dp/_97_InterleavingString.java
+++ problems/src/dp/_97_InterleavingString.java
@@ -10,10 +10,12 @@ public class _97_InterleavingString {
         char[] qChars = q.toCharArray();
         int n = qChars.length;
         char[] sChars = s.toCharArray();
-        if ((m + n) != sChars.length) {
+        if (m + n != sChars.length) {
             return false;
         }
         boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // Initialize DP
         dp[0][0] = true;
         for (int i = 1; i <= m && pChars[i - 1] == sChars[i - 1]; i++) {
             dp[i][0] = true;
@@ -21,14 +23,21 @@ public class _97_InterleavingString {
         for (int j = 1; j <= n && qChars[j - 1] == sChars[j - 1]; j++) {
             dp[0][j] = true;
         }
+
+        // Populate DP
         for (int i = 1; i <= m; i++) {
+            char pCh = pChars[i - 1];
             for (int j = 1; j <= n; j++) {
+                char qCh = qChars[j - 1];
                 char sCh = sChars[i + j - 1];
-                if (sCh == pChars[i - 1] && dp[i - 1][j] || sCh == qChars[j - 1] && dp[i][j - 1]) {
+                if (pCh == sCh && dp[i - 1][j] ||
+                        qCh == sCh && dp[i][j - 1]) {
                     dp[i][j] = true;
                 }
             }
         }
+
+        // Final Result
         return dp[m][n];
     }
 }
