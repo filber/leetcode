0ab5e745a6d2f6986d0664dff08f28cebac66772
filber <filber@foxmail.com>
2022-08-15 13:16:54 -0400

    Review

--- problems/src/ps/_238_ProductOfArrayExceptSelf.java
+++ problems/src/ps/_238_ProductOfArrayExceptSelf.java
@@ -7,23 +7,20 @@ public class _238_ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] prefix = new int[n + 1];
-        int[] suffix = new int[n + 1];
-
         prefix[0] = 1;
-        for (int i = 1; i <= n && nums[i - 1] != 0; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
-        }
-
+        int[] suffix = new int[n + 1];
         suffix[n] = 1;
-        for (int i = n - 1; i >= 0 && nums[i] != 0; i--) {
-            suffix[i] = suffix[i + 1] * nums[i];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+
+            int j = n - i;
+            suffix[j] = suffix[j + 1] * nums[j];
         }
 
-        int[] ans = new int[n];
-
+        int[] product = new int[n];
         for (int i = 0; i < n; i++) {
-            ans[i] = prefix[i] * suffix[i + 1];
+            product[i] = prefix[i] * suffix[i + 1];
         }
-        return ans;
+        return product;
     }
 }
