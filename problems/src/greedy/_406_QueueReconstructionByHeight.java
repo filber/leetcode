cb0065fa21316d23cf3cd197b7cf2a2d87a5768b
filber <filber@foxmail.com>
2022-08-16 20:56:53 -0400

    Review

--- problems/src/greedy/_406_QueueReconstructionByHeight.java
+++ problems/src/greedy/_406_QueueReconstructionByHeight.java
@@ -8,16 +8,11 @@ import java.util.LinkedList;
 public class _406_QueueReconstructionByHeight {
 
     public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, (a, b) -> {
-            if (a[0] == b[0]) return a[1] - b[1];
-            return b[0] - a[0];
-        });
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
         LinkedList<int[]> list = new LinkedList<>();
-
         for (int[] p : people) {
-            list.add(p[1],p);
+            list.add(p[1], p);
         }
-
-        return list.toArray(new int[people.length][]);
+        return list.toArray(new int[0][2]);
     }
 }
