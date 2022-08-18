612700b57df84de16c21b957a6ad3d10ad783c17
filber <filber@foxmail.com>
2022-08-17 23:45:04 -0400

    Review

--- problems/src/dp/_120_Triangle.java
+++ problems/src/dp/_120_Triangle.java
@@ -9,29 +9,21 @@ public class _120_Triangle {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[][] dp = new int[n][];
-        for (int i = 0; i < n; i++) {
-            dp[i] = new int[i + 1];
-        }
-
+        dp[0] = new int[1];
         dp[0][0] = triangle.get(0).get(0);
         for (int i = 1; i < n; i++) {
             List<Integer> list = triangle.get(i);
-            for (int j = 0; j <= i; j++) {
-                int min = Integer.MAX_VALUE;
-                if (j > 0) {
-                    min = Math.min(min, dp[i - 1][j - 1]);
-                }
-                if (j < i) {
-                    min = Math.min(min, dp[i - 1][j]);
-                }
-                dp[i][j] = min + list.get(j);
+            dp[i] = new int[i + 1];
+            dp[i][0] = dp[i - 1][0] + list.get(0);
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + list.get(j);
             }
+            dp[i][i] = dp[i - 1][i - 1] + list.get(i);
         }
-
-        int ans = Integer.MAX_VALUE;
+        int sum = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            ans = Math.min(ans, dp[n - 1][i]);
+            sum = Math.min(sum, dp[n - 1][i]);
         }
-        return ans;
+        return sum;
     }
 }
