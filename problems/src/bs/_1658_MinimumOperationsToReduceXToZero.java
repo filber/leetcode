02b2fc77ddbb7d040474677ac192fe082d3c78ec
filber <filber@foxmail.com>
2022-08-19 16:05:55 -0400

    Review

--- problems/src/bs/_1658_MinimumOperationsToReduceXToZero.java
+++ problems/src/bs/_1658_MinimumOperationsToReduceXToZero.java
@@ -2,41 +2,37 @@ package bs;
 
 //https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class _1658_MinimumOperationsToReduceXToZero {
 
     public int minOperations(int[] nums, int x) {
-        int n = nums.length;
-        Map<Integer, Integer> suffixMap = new HashMap<>();
-
         int sum = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            sum += nums[i];
-            suffixMap.put(sum, i);
+        for (int num : nums) {
+            sum += num;
         }
-
-        int[] prefixSum = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        if (sum < x) {
+            return -1;
+        } else if (sum == x) {
+            return nums.length;
         }
-
-        int minLen = Integer.MAX_VALUE;
-        for (int i = 0; i <= n; i++) {
-            int prefix = prefixSum[i];
-            if (prefix == x) {
-                minLen = Math.min(minLen, i);
-            } else if (prefix > x) {
-                break;
-            } else {
-                int val = x - prefix;
-                Integer rightIdx = suffixMap.get(val);
-                if (rightIdx != null && i <= rightIdx) {
-                    minLen = Math.min(minLen, i + n - rightIdx);
-                }
+        int target = sum - x;
+        int l = 0;
+        int ws = 0;
+        int len = 0;
+        for (int r = 0; r < nums.length; r++) {
+            while (ws + nums[r] > target) {
+                ws -= nums[l];
+                l++;
+            }
+            ws += nums[r];
+            if (ws == target) {
+                len = Math.max(len, r - l + 1);
             }
         }
-        return minLen == Integer.MAX_VALUE ? -1 : minLen;
+
+        if (len == 0) {
+            return -1;
+        } else {
+            return nums.length - len;
+        }
     }
 }
