fa2b5892882318ecc08261ec541302cea4eb3a71
filber <filber@foxmail.com>
2022-08-19 19:18:58 -0400

    Review

--- problems/src/array_matrix/_EX_FindFirstKNaturalNumbersMissingInGivenArray.java
+++ problems/src/array_matrix/_EX_FindFirstKNaturalNumbersMissingInGivenArray.java
@@ -2,52 +2,36 @@ package array_matrix;
 
 //https://www.geeksforgeeks.org/find-first-k-natural-numbers-missing-given-array/
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class _EX_FindFirstKNaturalNumbersMissingInGivenArray {
 
+    // Natural numbers start from 1
+
     public int[] findKMissing(int nums[], int k) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0 || nums[i] > n || nums[i] == i + 1) {
-                continue;
-            }
-            int to = nums[i];
-            while (to > 0 && to <= n && nums[to - 1] != to) {
-                int tmp = nums[to - 1];
-                nums[to - 1] = to;
-                to = tmp;
-            }
-            if (nums[i] != i + 1) {
-                nums[i] = to;
-            }
+        Arrays.sort(nums);
+        int l = 0;
+        while (l < nums.length && nums[l] <= 0) {
+            l++;
         }
 
-        int[] ans = new int[k];
-        int idx = 0;
-        Set<Integer> set = new HashSet<>();
-        int lastNatural = 0;
-        for (int i = 0; i < n && idx < k; i++) {
-            if (nums[i] != i + 1) {
-                ans[idx++] = i + 1;
-                lastNatural = i + 1;
-                if (nums[i] > 0) {
-                    set.add(nums[i]);
-                }
-            } else {
-                lastNatural = nums[i];
+        int[] rst = new int[k];
+        int r = 0;
+        int expect = 1;
+
+        while (l < nums.length && r < k) {
+            while (r < k && nums[l] != expect) {
+                rst[r++] = expect++;
             }
+            expect++;
+            l++;
         }
 
-        for (int natural = lastNatural + 1; idx < k; natural++) {
-            if (set.isEmpty() || !set.contains(natural)) {
-                ans[idx++] = natural;
-            } else {
-                set.remove(natural);
-            }
+        while (r < k) {
+            rst[r++] = expect++;
         }
-
-        return ans;
+        return rst;
     }
 }
