cfc1332668262292b7601c2a0b84435c34540be0
filber <filber@foxmail.com>
2022-08-19 20:14:05 -0400

    Review

--- problems/src/tree/_1214_TwoSumBSTs.java
+++ problems/src/tree/_1214_TwoSumBSTs.java
@@ -7,17 +7,27 @@ public class _1214_TwoSumBSTs {
     // Given two binary search trees, return True if and only if there is a node
     // in the first tree and a node in the second tree whose values sum up to a given integer target.
 
+    private int k;
+
     public boolean findTarget(TreeNode L, TreeNode R, int k) {
+        this.k = k;
+        return dfs(L, R);
+    }
+
+    private boolean dfs(TreeNode L, TreeNode R) {
         if (L == null || R == null) {
             return false;
-        }
-        int sum = L.val + R.val;
-        if (sum == k) {
+        } else if (L.val + R.val == k) {
             return true;
-        } else if (sum < k) {
-            return findTarget(L.right, R, k) || findTarget(L, R.right, k);
+        } else if (L.val + R.val > k) {
+            if (dfs(L, R.left) || dfs(L.left, R)) {
+                return true;
+            }
         } else {
-            return findTarget(L, R.left, k) || findTarget(L.left, R, k);
+            if (dfs(L, R.right) || dfs(L.right, R)) {
+                return true;
+            }
         }
+        return false;
     }
 }
