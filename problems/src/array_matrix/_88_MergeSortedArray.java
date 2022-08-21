51ba320c1572ceeb9e5703decfcb4093dfc4248f
filber <filber@foxmail.com>
2022-08-20 20:41:57 -0400

    Review

--- problems/src/array_matrix/_88_MergeSortedArray.java
+++ problems/src/array_matrix/_88_MergeSortedArray.java
@@ -5,41 +5,28 @@ package array_matrix;
 public class _88_MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int idx = m % nums1.length;
-        int i = 0, j = 0;
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                nums1[idx] = nums1[i++];
-            } else {
-                nums1[idx] = nums2[j++];
+        if (n == 0) {
+            return;
+        } else if (m == 0) {
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
             }
-            idx = (idx + 1) % nums1.length;
+            return;
+        }
+        int l = m - 1, r = n - 1;
+        int idx = m + n - 1;
+        while (l >= 0 && r >= 0) {
+            int L = nums1[l], R = nums2[r];
+            if (L >= R) {
+                nums1[idx--] = nums1[l--];
+            } else {
+                nums1[idx--] = nums2[r--];
+            }
         }
 
-        while (i < m) {
-            nums1[idx] = nums1[i++];
-            idx = (idx + 1) % nums1.length;
+        while (r >= 0) {
+            nums1[idx--] = nums2[r--];
         }
-        while (j < n) {
-            nums1[idx] = nums2[j++];
-            idx = (idx + 1) % nums1.length;
-        }
-
-        // reverse nums1
-        reverse(nums1, 0, m + n - 1);
-        // reverse first n elements
-        reverse(nums1, 0, n - 1);
-        // reverse last m elements
-        reverse(nums1, n, m + n - 1);
     }
 
-    private void reverse(int[] nums, int l, int r) {
-        while (l < r) {
-            int tmp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = tmp;
-            l++;
-            r--;
-        }
-    }
 }
