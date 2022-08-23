bf4c516a63b82c2c07c6597f045f956d36b8f7cf
filber <filber@foxmail.com>
2022-08-23 14:19:08 -0400

    Review

--- problems/src/dp/_1035_UncrossedLines.java
+++ problems/src/dp/_1035_UncrossedLines.java
@@ -5,17 +5,14 @@ package dp;
 public class _1035_UncrossedLines {
 
     public int maxUncrossedLines(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
+        int m = A.length, n = B.length;
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
-            int a = A[i - 1];
             for (int j = 1; j <= n; j++) {
-                int b = B[j - 1];
-                if (a == b) {
+                if (A[i - 1] == B[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
