dc57f782cf1bbeb9c28828a36467868447c325cf
filber <filber@foxmail.com>
2022-08-23 14:53:48 -0400

    Review

--- problems/src/dp/_718_MaximumLengthOfRepeatedSubarray.java
+++ problems/src/dp/_718_MaximumLengthOfRepeatedSubarray.java
@@ -5,20 +5,23 @@ package dp;
 public class _718_MaximumLengthOfRepeatedSubarray {
 
     public int findLength(int[] A, int[] B) {
-        int m = A.length, n = B.length;
+        int m = A.length;
+        int n = B.length;
         int[][] dp = new int[m + 1][n + 1];
-        int maxLen = 0;
+        int len = 0;
         for (int i = 1; i <= m; i++) {
             int a = A[i - 1];
             for (int j = 1; j <= n; j++) {
                 int b = B[j - 1];
                 if (a == b) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                    len = Math.max(len, dp[i][j]);
                 }
-                maxLen = Math.max(maxLen, dp[i][j]);
+//                else {
+//                    dp[i][j] = 0;
+//                }
             }
         }
-
-        return maxLen;
+        return len;
     }
 }
