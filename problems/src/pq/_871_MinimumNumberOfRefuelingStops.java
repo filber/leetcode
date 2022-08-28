7bddc2ce525d7b0c6d2e4f18e1f7ec79abf8d507
filber <filber@foxmail.com>
2022-08-28 15:53:27 -0400

    Review

--- problems/src/pq/_871_MinimumNumberOfRefuelingStops.java
+++ problems/src/pq/_871_MinimumNumberOfRefuelingStops.java
@@ -8,6 +8,7 @@ import java.util.PriorityQueue;
 public class _871_MinimumNumberOfRefuelingStops {
 
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        // Guard condition
         if (stations.length == 0) {
             if (startFuel >= target) {
                 return 0;
@@ -16,29 +17,30 @@ public class _871_MinimumNumberOfRefuelingStops {
             }
         }
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        int stops = 0;
         int fuel = startFuel;
+        int stops = 0;
+        PriorityQueue<Integer> F = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < stations.length; i++) {
-            int position = stations[i][0];
-            while (fuel < position) {
-                Integer refuel = queue.poll();
-                if (refuel == null) {
+            int distance = stations[i][0];
+            int refuel = stations[i][1];
+            while (fuel < distance) {
+                if (F.isEmpty()) {
                     return -1;
+                } else {
+                    stops++;
+                    fuel += F.poll();
                 }
-                fuel += refuel;
-                stops++;
             }
-            queue.add(stations[i][1]);
+            F.add(refuel);
         }
 
         while (fuel < target) {
-            Integer refuel = queue.poll();
-            if (refuel == null) {
+            if (F.isEmpty()) {
                 return -1;
+            } else {
+                stops++;
+                fuel += F.poll();
             }
-            fuel += refuel;
-            stops++;
         }
         return stops;
     }
