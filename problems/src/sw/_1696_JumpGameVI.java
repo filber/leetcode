dc750bfe554a60977dcda931e9b38a90ad2f58c9
filber <filber@foxmail.com>
2022-09-23 19:39:54 -0400

    1696. Jump Game VI

--- problems/src/sw/_1696_JumpGameVI.java
+++ problems/src/sw/_1696_JumpGameVI.java
@@ -1,12 +1,35 @@
-package pq;
+package sw;
 
 //https://leetcode.com/problems/jump-game-vi/
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class _1696_JumpGameVI {
 
     public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] score = new int[n];
+        score[0] = nums[0];
+        Deque<Integer> dq = new LinkedList<>();
+        dq.offerLast(0);
+        for (int i = 1; i < n; i++) {
+            // pop the old index
+            while (!dq.isEmpty() && dq.peekFirst() < i - k) {
+                dq.pollFirst();
+            }
+            score[i] = score[dq.peek()] + nums[i];
+            // pop the smaller value
+            while (!dq.isEmpty() && score[i] >= score[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        return score[n - 1];
+    }
+
+    public int maxResultPQ(int[] nums, int k) {
         int n = nums.length;
         int[] dp = new int[n];
         dp[n - 1] = nums[n - 1];
--- test/src/sw/_1696_JumpGameVITest.java
+++ test/src/sw/_1696_JumpGameVITest.java
@@ -1,4 +1,4 @@
-package pq;
+package sw;
 
 import org.junit.Test;
 import org.junit.Before;
