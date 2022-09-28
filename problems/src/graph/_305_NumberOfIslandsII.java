59ea55134b1d9d43f2e58797f19a5194f8f86f61
Filber <filber@foxmail.com>
2022-09-27 22:50:19 -0400

    305. Number of islands II

--- problems/src/graph/_305_NumberOfIslandsII.java
+++ problems/src/graph/_305_NumberOfIslandsII.java
@@ -5,6 +5,11 @@ import java.util.Set;
 
 public class _305_NumberOfIslandsII {
 
+    // A 2d grid map of m rows and n columns is initially filled with water.
+    // We may perform an addLand operation which turns the water at position (row, col) into a land.
+    // Given a list of positions to operate, count the number of islands after each addLand operation.
+    // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+    // You may assume all four edges of the grid are all surrounded by water.
 
     int[] ids;
     int[] sz;
