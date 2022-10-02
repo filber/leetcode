6765116be2c9fa848e6b02e40c3b62b020b1bd9b
filber <filber@foxmail.com>
2022-10-02 16:05:44 -0400

    Review

--- problems/src/dp/_139_WordBreak.java
+++ problems/src/dp/_139_WordBreak.java
@@ -8,22 +8,29 @@ import java.util.Set;
 
 public class _139_WordBreak {
 
+
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
+        Set<String> set = new HashSet<>();
+        int maxLen = 0;
+        for (String word : wordDict) {
+            maxLen = Math.max(maxLen, word.length());
+            set.add(word);
+        }
+
         char[] chars = s.toCharArray();
         int n = chars.length;
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
+
         for (int i = 1; i <= n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                String str = s.substring(j, i);
-                if (wordDict.contains(str) && dp[j]) {
+            for (int j = i; i - j + 1 <= maxLen && j > 0; j--) {
+                String subStr = new String(chars, j - 1, i - j + 1);
+                if (dp[j - 1] && set.contains(subStr)) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-
         return dp[n];
     }
 }
