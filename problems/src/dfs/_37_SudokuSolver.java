4e0e5b01d28cc807ca377d5afbd2fa3f0e206fb3
filber <filber@foxmail.com>
2022-10-03 16:43:08 -0400

    Review

--- problems/src/dfs/_37_SudokuSolver.java
+++ problems/src/dfs/_37_SudokuSolver.java
@@ -2,6 +2,9 @@ package dfs;
 
 //https://leetcode.com/problems/sudoku-solver/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _37_SudokuSolver {
 
     // A sudoku solution must satisfy all of the following rules:
@@ -9,90 +12,62 @@ public class _37_SudokuSolver {
     //    Each of the digits 1-n must occur exactly once in each column.
     //    Each of the digits 1-n must occur exactly once in each of the n 3x3 sub-boxes of the grid.
 
-    // Constraints:
-    //    board.length == n
-    //    board[i].length == n
-    //    board[i][j] is a digit or '.'.
-    //    It is guaranteed that the input board has only one solution.
-
-    private int m;
-    private int n;
-    private char[][] board;
-    private boolean[][] rowCandidate;
-    private boolean[][] colCandidate;
-    private boolean[][][] boxCandidate;
-    private int[][] emptyLocations;
-    private int emptyCnt;
-
     public void solveSudoku(char[][] board) {
-
-        this.board = board;
-        m = board.length;
-        n = board[0].length;
-        rowCandidate = new boolean[m][9];
-        colCandidate = new boolean[n][9];
-        boxCandidate = new boolean[m / 3][n / 3][9];
-        emptyLocations = new int[m * n][2];
-        emptyCnt = 0;
-
-        // 1. Traverse for candidates
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                char ch = board[i][j];
-                if (ch != '.') {
-                    rowCandidate[i][ch - '1'] = true;
-                    colCandidate[j][ch - '1'] = true;
-                    boxCandidate[i / 3][j / 3][ch - '1'] = true;
-                } else {
-                    emptyLocations[emptyCnt][0] = i;
-                    emptyLocations[emptyCnt][1] = j;
-                    emptyCnt++;
+        // whether digit j has been used for row i, if yes, r_r[i][j-1] = true
+        boolean[][] row_record = new boolean[9][9];
+        // whether digit j has been used for col i, if yes, c_r[i][j-1] = true
+        boolean[][] col_record = new boolean[9][9];
+        // whether digit j has been used for grid[p][q], if yes, g_r[p][q] = true
+        boolean[][][] grid_record = new boolean[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int digit = board[i][j] - '0';
+                    row_record[i][digit - 1] = true;
+                    col_record[j][digit - 1] = true;
+                    grid_record[i / 3][j / 3][digit - 1] = true;
                 }
             }
         }
 
-        // 2. Backtracking from (0,0)
-        boolean res = backtracking(0);
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    list.add(new int[]{i, j});
+                }
+            }
+        }
+        backtracking(board, row_record, col_record, grid_record, list, 0);
     }
 
-    private boolean backtracking(int pos) {
-        if (pos == emptyCnt) {
+    private boolean backtracking(char[][] board, boolean[][] row_record, boolean[][] col_record, boolean[][][] grid_record, List<int[]> list, int i) {
+        if (i == list.size()) {
             return true;
         }
-        int i = emptyLocations[pos][0];
-        int j = emptyLocations[pos][1];
-
-        // 1. Retrieve candidate for (i,j)
-        boolean[] rowCand = rowCandidate[i];
-        boolean[] colCand = colCandidate[j];
-        boolean[] boxCand = boxCandidate[i / 3][j / 3];
-
-        // 2. Traverse all candidates and backtrack next 2 positions
-        for (int k = 0; k < 9; k++) {
-            // intersect with boolean bitwise operations
-            boolean inter = !rowCand[k] && !colCand[k] && !boxCand[k];
-            if (!inter) {
+        int[] pair = list.get(i);
+        int x = pair[0];
+        int y = pair[1];
+        for (char cand = '1'; cand <= '9'; cand++) {
+            if (row_record[x][cand - '1'] || col_record[y][cand - '1'] || grid_record[x / 3][y / 3][cand - '1']) {
                 continue;
             }
-
-            // Set states
-            board[i][j] = (char) ('1' + k); // update board
-            // update candidate
-            rowCand[k] = true;
-            colCand[k] = true;
-            boxCand[k] = true;
-            if (backtracking(pos + 1)) {
+            // set states
+            row_record[x][cand - '1'] = true;
+            col_record[y][cand - '1'] = true;
+            grid_record[x / 3][y / 3][cand - '1'] = true;
+            board[x][y] = cand;
+            if (backtracking(board, row_record, col_record, grid_record, list, i + 1)) {
                 return true;
-            } else {
-                // Reverse states
-                board[i][j] = '.';
-                rowCand[k] = false;
-                colCand[k] = false;
-                boxCand[k] = false;
             }
+
+            // reverse states
+            row_record[x][cand - '1'] = false;
+            col_record[y][cand - '1'] = false;
+            grid_record[x / 3][y / 3][cand - '1'] = false;
+            board[x][y] = '.';
         }
 
-        // 3. No valid candidates found
         return false;
     }
 }
