17ca47ee16256ed229ddfc8dece7b6217a1b91b4
Filber <filber@foxmail.com>
2022-10-02 22:02:12 -0400

    Review

--- problems/src/stack/_739_DailyTemperatures.java
+++ problems/src/stack/_739_DailyTemperatures.java
@@ -4,21 +4,16 @@ public class _739_DailyTemperatures {
 
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        int[] waits = new int[n];
         int[] stack = new int[n];
-        int top = 0;
-        stack[top] = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            int temp = temperatures[i];
-            while (top != -1 && temperatures[stack[top]] <= temp) {
+        int top = -1;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (top > -1 && temperatures[stack[top]] < temperatures[i]) {
+                ans[stack[top]] = i - stack[top];
                 top--;
-            }
-            if (top != -1) {
-                waits[i] = stack[top] - i;
             }
             stack[++top] = i;
         }
-
-        return waits;
+        return ans;
     }
 }
