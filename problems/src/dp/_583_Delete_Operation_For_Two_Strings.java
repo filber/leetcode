9208705f4dae9d3f79015a617c045ae283807973
filber <filber@foxmail.com>
2022-10-03 15:00:36 -0400

    Review

--- problems/src/dp/_583_Delete_Operation_For_Two_Strings.java
+++ problems/src/dp/_583_Delete_Operation_For_Two_Strings.java
@@ -2,33 +2,28 @@ package dp;
 
 public class _583_Delete_Operation_For_Two_Strings {
 
-    public int minDistance(String word1, String word2) {
-        char[] chars1 = word1.toCharArray();
-        int m = chars1.length;
-        char[] chars2 = word2.toCharArray();
-        int n = chars2.length;
-
-        // DP[i][j] the operations needed for word1.substring(0,i) and word2.substring(0,j)
+    public int minDistance(String AStr, String BStr) {
+        char[] A = AStr.toCharArray();
+        int m = A.length;
+        char[] B = BStr.toCharArray();
+        int n = B.length;
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            // word2.substring is always empty
+        // dp[0][0] = 0;
+        for (int i = 1; i <= m; i++) {
             dp[i][0] = i;
         }
-
-        for (int j = 0; j <= n; j++) {
-            // word1.substring is always empty
+        for (int j = 1; j <= n; j++) {
             dp[0][j] = j;
         }
 
         for (int i = 1; i <= m; i++) {
+            char a = A[i - 1];
             for (int j = 1; j <= n; j++) {
-                if (chars1[i - 1] == chars2[j - 1]) {
+                char b = B[j - 1];
+                if (a == b) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    int bothDelete = dp[i - 1][j - 1] + 2;
-                    int deleteLeft = dp[i - 1][j] + 1;
-                    int deleteRight = dp[i][j - 1] + 1;
-                    dp[i][j] = Math.min(bothDelete, Math.min(deleteLeft, deleteRight));
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 2);
                 }
             }
         }
