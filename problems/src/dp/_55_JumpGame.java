e7461d2170f8b8394ab08bc4ef0cc9ef69e2c457
da.li <da.li@bytedance.com>
2023-12-22 15:13:06 -0800

    55. Jump Game

--- problems/src/dp/_55_JumpGame.java
+++ problems/src/dp/_55_JumpGame.java
@@ -5,17 +5,22 @@ public class _55_JumpGame {
 
     public static boolean canJump(int[] nums) {
         int n = nums.length;
-        int mostRightPos = 0;
-        for (int i = 0; i < n; i++) {
-            if (i <= mostRightPos) {
-                mostRightPos = Math.max(mostRightPos,i + nums[i]);
-            }
 
-            if (mostRightPos >= n - 1) {
+        int lastIndex = n - 1;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (dp[i - 1] < i) {
+                // can't reach i
+                return false;
+            }
+            dp[i] = Math.max(dp[i - 1], i + nums[i]);
+            if (dp[i] >= lastIndex) {
+                // could reach lastIndex
                 return true;
             }
         }
-
-        return false;
+        // single element
+        return true;
     }
 }
