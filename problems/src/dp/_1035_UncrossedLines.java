54b90dc4aa34de42cf5c3e1a6a9327fca4246fbf
da.li <da.li@bytedance.com>
2024-01-03 15:13:03 -0800

    1035. Uncrossed Lines

--- problems/src/dp/_1035_UncrossedLines.java
+++ problems/src/dp/_1035_UncrossedLines.java
@@ -10,7 +10,7 @@ public class _1035_UncrossedLines {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (A[i - 1] == B[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + 1, Math.max(dp[i - 1][j], dp[i][j - 1]));
                 } else {
                     dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
