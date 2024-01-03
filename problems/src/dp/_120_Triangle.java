b762fae0be9a0251ca91f5b96883805dc2a717b1
da.li <da.li@bytedance.com>
2024-01-02 18:55:42 -0800

    120. Triangle

--- problems/src/dp/_120_Triangle.java
+++ problems/src/dp/_120_Triangle.java
@@ -8,22 +8,24 @@ public class _120_Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][];
-        dp[0] = new int[1];
-        dp[0][0] = triangle.get(0).get(0);
+        int[][] dp = new int[n][n];
+        // dp[i][j]: minimum path sum when starting from top to reach triangle[i][j]
+        dp[0][0] = triangle.get(0).get(0); // top
         for (int i = 1; i < n; i++) {
-            List<Integer> list = triangle.get(i);
-            dp[i] = new int[i + 1];
-            dp[i][0] = dp[i - 1][0] + list.get(0);
+            List<Integer> arr = triangle.get(i);
+            dp[i][0] = arr.get(0) + dp[i - 1][0]; // most left
             for (int j = 1; j < i; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + list.get(j);
+                // middle
+                dp[i][j] = arr.get(j) + Math.min(dp[i - 1][j - 1], dp[i - 1][j]);
             }
-            dp[i][i] = dp[i - 1][i - 1] + list.get(i);
+            dp[i][i] = arr.get(i) + dp[i - 1][i - 1]; // most right
         }
-        int sum = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            sum = Math.min(sum, dp[n - 1][i]);
+
+        // result = min(dp[n-1][j])
+        int result = Integer.MAX_VALUE;
+        for (int val : dp[n - 1]) {
+            result = Math.min(result, val);
         }
-        return sum;
+        return result;
     }
 }
