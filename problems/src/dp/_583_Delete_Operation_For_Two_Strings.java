dcab80230cb9bfaaa8102903863844261c5c35f0
da.li <da.li@bytedance.com>
2024-01-04 13:22:23 -0800

    583. Delete operation for two strings

--- problems/src/dp/_583_Delete_Operation_For_Two_Strings.java
+++ problems/src/dp/_583_Delete_Operation_For_Two_Strings.java
@@ -1,33 +1,29 @@
 package dp;
 
+//https://leetcode.com/problems/delete-operation-for-two-strings/
+
 public class _583_Delete_Operation_For_Two_Strings {
 
-    public int minDistance(String AStr, String BStr) {
-        char[] A = AStr.toCharArray();
+    public int minDistance(String a, String b) {
+        char[] A = a.toCharArray();
+        char[] B = b.toCharArray();
         int m = A.length;
-        char[] B = BStr.toCharArray();
         int n = B.length;
-        int[][] dp = new int[m + 1][n + 1];
-        // dp[0][0] = 0;
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = i;
-        }
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = j;
-        }
 
+        // dp[i][j] is the longest common subsequence of A[0..i] and B[0..j]
+        int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
-            char a = A[i - 1];
             for (int j = 1; j <= n; j++) {
-                char b = B[j - 1];
-                if (a == b) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + 1, Math.max(dp[i - 1][j], dp[i][j - 1]));
                 } else {
-                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 2);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        return dp[m][n];
+        // total deletions of both A and B
+        int result = m - dp[m][n] + n - dp[m][n];
+        return result;
     }
 }
