a70110fe1c07a2fa5d21cb11f55c385ebc9d7155
da.li <da.li@bytedance.com>
2024-01-03 16:33:38 -0800

    718. Maximum length of repeated subarray

--- problems/src/dp/_718_MaximumLengthOfRepeatedSubarray.java
+++ problems/src/dp/_718_MaximumLengthOfRepeatedSubarray.java
@@ -8,20 +8,15 @@ public class _718_MaximumLengthOfRepeatedSubarray {
         int m = A.length;
         int n = B.length;
         int[][] dp = new int[m + 1][n + 1];
-        int len = 0;
+        int result = Integer.MIN_VALUE;
         for (int i = 1; i <= m; i++) {
-            int a = A[i - 1];
             for (int j = 1; j <= n; j++) {
-                int b = B[j - 1];
-                if (a == b) {
+                if (A[i - 1] == B[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    len = Math.max(len, dp[i][j]);
                 }
-//                else {
-//                    dp[i][j] = 0;
-//                }
+                result = Math.max(result, dp[i][j]);
             }
         }
-        return len;
+        return result;
     }
 }
