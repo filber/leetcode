d96aa09033bbdd1ac13ebcae9992fdb91b5df37d
da.li <da.li@bytedance.com>
2024-01-04 22:18:45 -0800

    300. Longest increasing subsequence

--- problems/src/bs/_300_LongestIncreasingSubsequence.java
+++ problems/src/bs/_300_LongestIncreasingSubsequence.java
@@ -9,29 +9,29 @@ public class _300_LongestIncreasingSubsequence {
     // O(N*logN)
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        int len = 0;
-        for (int i = 0; i < n; i++) {
-            int j = search(dp, len, nums[i]);
-            if (j + 1 > len) {
-                len = j + 1;
-            }
+        int[] A = new int[n];
+        A[0] = nums[0];
+        int len = 1;
+        for (int i = 1; i < n; i++) {
+            int j = searchInsert(A, len, nums[i]);
+            len = Math.max(len, j + 1);
         }
         return len;
     }
 
-    private int search(int[] dp, int len, int target) {
-        int l = 0;
-        int r = len;
+    private int searchInsert(int[] A, int len, int target) {
+        int l = 0, r = len;
         while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (dp[mid] >= target) {
+            int mid = (l + r) / 2;
+            if (A[mid] >= target) {
                 r = mid;
             } else {
+                // A[mid] < target
                 l = mid + 1;
             }
         }
-        dp[l] = target;
+
+        A[l] = target;
         return l;
     }
 }
