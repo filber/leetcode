04e4ea2f6a3ea13af948602b51d3cb62dfb62734
da.li <da.li@bytedance.com>
2024-01-05 11:54:53 -0800

    91. Decode ways

--- problems/src/dp/_91_DecodeWays.java
+++ problems/src/dp/_91_DecodeWays.java
@@ -4,55 +4,38 @@ package dp;
 
 public class _91_DecodeWays {
 
+    // A-1, B-2, C-3, D-4, E-5, F-6, G-7, H-8, I-9,
+    // J-10, K-11, L-12, M-13, N-14, O-15, P-16, Q-17, R-18, S-19,
+    // T-20, U-21, V-22, W-23, X-24, Y-25, Z-26
     public int numDecodings(String s) {
-        char[] chars = s.toCharArray();
-        if (chars[0] == '0') {
+        char[] A = s.toCharArray();
+        if (A[0] == '0') {
             return 0;
         }
-        // dp[i]: number of decodings for s[0,i]
-        int[] dp = new int[chars.length + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i < dp.length; i++) {
-            char pre = chars[i - 2];
-            char cur = chars[i - 1];
-            if (pre == '0') {
-                if (cur == '0') {
-                    // [100]
-                    return 0;
-                } else {
-                    // [102] => [[10],2]
-                    // [202] => [[20],2]
-                    dp[i] = dp[i - 1];
-                }
-            } else if (pre == '1') {
-                if (cur == '0') {
-                    // [210] => [[2],10]
-                    dp[i] = dp[i - 2];
-                } else {
-                    //[112] = [[11],2] + [[1],12]
-                    dp[i] = dp[i - 1] + dp[i - 2];
-                }
-            } else if (pre == '2') {
-                if (cur == '0') {
-                    // [120] =>[[1],20]
-                    dp[i] = dp[i - 2];
-                } else if (cur >= '1' && cur <= '6') {
-                    // [123] =>[[1],23] + [[12],3]
-                    dp[i] = dp[i - 1] + dp[i - 2];
-                } else {
-                    // [127] =>[[12],7]
-                    dp[i] = dp[i - 1];
-                }
+
+        int n = A.length;
+        // dp[i]: ways to decode s[0..i]
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1; // empty
+        dp[1] = 1; // first char
+        for (int i = 2; i <= n; i++) {
+            char cur = A[i - 1];
+            char previous = A[i - 2];
+            if (cur == '0' && previous != '1' && previous != '2') {
+                return 0;
+            } else if (cur == '0' && (previous == '1' || previous == '2')) {
+                dp[i] = dp[i - 2];
+            } else if (cur >= '7' && cur <= '9' && previous == '1') {
+                dp[i] = dp[i - 1] + dp[i - 2]; // could combine with previous '1' or exist singly
+            } else if (cur >= '7' && cur <= '9') {
+                dp[i] = dp[i - 1]; // can't combine with previous char
+            } else if (cur >= '1' && cur <= '6' && (previous == '1' || previous == '2')) {
+                dp[i] = dp[i - 1] + dp[i - 2]; // could combine with previous '1'/'2' or exist singly
             } else {
-                if (cur=='0') {
-                    return 0;
-                } else {
-                    // [134] =>[[13],4]
-                    dp[i] = dp[i - 1];
-                }
+                dp[i] = dp[i - 1]; // can't combine with previous char
             }
         }
-        return dp[chars.length];
+        return dp[n];
     }
 }
