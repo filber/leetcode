bbb7f2c15cd50dd251b25eca9840fb7e53fc6173
da.li <da.li@bytedance.com>
2024-01-09 00:08:27 -0800

    188. Best time to buy and sell stock IV

--- problems/src/dp/_188_BestTimeToBuyAndSellStockIV.java
+++ problems/src/dp/_188_BestTimeToBuyAndSellStockIV.java
@@ -3,33 +3,29 @@ package dp;
 
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 
-import java.util.Arrays;
 public class _188_BestTimeToBuyAndSellStockIV {
-    public static int maxProfit(int k, int[] prices) {
-        if (k == 0 || prices.length < 2) {
-            return 0;
+
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][k + 1][2];
+        int maxProfit = 0;
+        // no transactions at all
+        dp[0][0][0] = 0;
+        dp[0][0][1] = 0;
+
+        // 1-k transactions at day 1
+        for (int j = 1; j <= k; j++) {
+            dp[0][j][0] = -prices[0];
+            dp[0][j][1] = 0;
         }
 
-        int[] buy = new int[k + 1];
-        Arrays.fill(buy, Integer.MIN_VALUE);
-        int[] sell = new int[k + 1];
-
-        for (int p : prices) {
-            for (int i = 1; i <= k; i++) {
-                buy[i] = Math.max(buy[i], sell[i - 1] - p);
-                sell[i] = Math.max(sell[i], buy[i] + p);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j - 1][1] - prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j][0] + prices[i]);
+                maxProfit = Math.max(maxProfit, dp[i][j][1]);
             }
         }
-
-        return sell[k];
-    }
-
-    public static void main(String[] args) {
-        int m1 = maxProfit(2, new int[]{3, 3, 5, 0, 0, 3, 1, 4}); // 6
-        int m2 = maxProfit(2, new int[]{1, 2, 3, 4, 5}); //4
-        int m3 = maxProfit(2, new int[]{7, 6, 4, 3, 1}); //0
-        int m4 = maxProfit(2, new int[]{1, 2, 4, 2, 5, 7, 2, 4, 9, 0}); //13
-        int m5 = maxProfit(2, new int[]{2, 4, 1}); //2
-        int m6 = maxProfit(2, new int[]{3,2,6,5,0,3}); //7
+        return maxProfit;
     }
 }
---
+++ test/src/dp/_188_BestTimeToBuyAndSellStockIVTest.java
@@ -0,0 +1,64 @@
+package dp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class _188_BestTimeToBuyAndSellStockIVTest {
+
+    _188_BestTimeToBuyAndSellStockIV target;
+
+    @Before
+    public void setUp() throws Exception {
+        target = new _188_BestTimeToBuyAndSellStockIV();
+    }
+
+    @Test
+    public void testMaxProfit1() {
+        int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        int k = 2;
+        int profit = target.maxProfit(k, prices);
+        assertEquals(6, profit);
+    }
+
+    @Test
+    public void testMaxProfit2() {
+        int[] prices = {1, 2, 3, 4, 5};
+        int k = 2;
+        int profit = target.maxProfit(k, prices);
+        assertEquals(4, profit);
+    }
+
+    @Test
+    public void testMaxProfit3() {
+        int[] prices = {7, 6, 4, 3, 1};
+        int k = 2;
+        int profit = target.maxProfit(k, prices);
+        assertEquals(0, profit);
+    }
+
+    @Test
+    public void testMaxProfit4() {
+        int[] prices = {1, 2, 4, 2, 5, 7, 2, 4, 9, 0};
+        int k = 2;
+        int profit = target.maxProfit(k, prices);
+        assertEquals(13, profit);
+    }
+
+    @Test
+    public void testMaxProfit5() {
+        int[] prices = {2, 4, 1};
+        int k = 2;
+        int profit = target.maxProfit(k, prices);
+        assertEquals(2, profit);
+    }
+
+    @Test
+    public void testMaxProfit6() {
+        int[] prices = {3, 2, 6, 5, 0, 3};
+        int k = 2;
+        int profit = target.maxProfit(k, prices);
+        assertEquals(7, profit);
+    }
+}
