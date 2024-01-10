08ed2665c92bb19ccc1b277bfe83f763892e0899
da.li <da.li@bytedance.com>
2024-01-10 12:30:53 -0800

    213. House Robber II

--- problems/src/dp/_213_HouseRobberII.java
+++ problems/src/dp/_213_HouseRobberII.java
@@ -5,26 +5,23 @@ package dp;
 public class _213_HouseRobberII {
 
     public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
         int n = nums.length;
-        int[] robFirst = new int[n];
-        robFirst[0] = 0;
-        robFirst[1] = nums[0]; // rob first
-        for (int i = 2; i < n; i++) {
-            int value = nums[i - 1];
-            robFirst[i] = Math.max(robFirst[i - 1], robFirst[i - 2] + value);
-        }
+        int[] SkipFirst = new int[n + 1];
+        int[] RobFirst = new int[n + 1];
 
-        int[] skipFirst = new int[n + 1];
-        skipFirst[0] = 0;
-        skipFirst[1] = 0; // skip first
+        // initialization
+        SkipFirst[0] = 0;
+        SkipFirst[1] = 0;
+        RobFirst[0] = 0;
+        RobFirst[1] = nums[0];
+
+        // iterate
         for (int i = 2; i <= n; i++) {
-            int value = nums[i - 1];
-            skipFirst[i] = Math.max(skipFirst[i - 1], skipFirst[i - 2] + value);
+            int amt = nums[i - 1];
+            SkipFirst[i] = Math.max(SkipFirst[i - 1], SkipFirst[i - 2] + amt);
+            RobFirst[i] = Math.max(RobFirst[i - 1], RobFirst[i - 2] + amt);
         }
-        return Math.max(robFirst[n - 1], skipFirst[n]);
+        int result = Math.max(SkipFirst[n], RobFirst[n - 1]);
+        return result;
     }
 }
