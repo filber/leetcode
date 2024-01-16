802dbcb6ab29f88e8c7f7ae7cfa7ecdd560b2d6e
da.li <da.li@bytedance.com>
2024-01-15 18:28:48 -0800

    380. Insert Delete Get Random

--- problems/src/hmap/_380_InsertDeleteGetRandom.java
+++ problems/src/hmap/_380_InsertDeleteGetRandom.java
@@ -30,10 +30,10 @@ public class _380_InsertDeleteGetRandom {
             }
             int idx = map.get(val);
             int last = list.get(list.size()-1);
-            list.set(idx,last);
-            list.remove(list.size() - 1);
+            list.set(idx,last); // set element in array list, O(1)
+            list.remove(list.size() - 1); // delete last element, O(1)
             map.put(last, idx);
-            map.remove(val);
+            map.remove(val); // remove val at last, in case val is the last element
             return true;
         }
 
