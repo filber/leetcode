f833c1069825e26161e17dab31a9127ec8834f91
da.li <da.li@bytedance.com>
2024-02-07 14:47:23 -0800

    518. Coin Change II

--- problems/src/dp/_518_CoinChangeII.java
+++ problems/src/dp/_518_CoinChangeII.java
@@ -1,43 +1,30 @@
 package dp;
 
-import java.util.Arrays;
-
 // https://leetcode.com/problems/coin-change-2/
 
 public class _518_CoinChangeII {
 
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-        for (int v : coins) {
-            for (int i = v; i <= amount; i++) {
-                dp[i] = dp[i] + dp[i - v];
-            }
-        }
-        return dp[amount];
-    }
-
-    public int change2D(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
+        int[][] dp = new int[amount+1][n+1];
+        // init
+        // dp[i][0] = 0;
+        for(int j=0;j<=n;j++) {
+            dp[0][j] = 1;
         }
-        for (int j = 0; j <= amount; j++) {
-            if (j % coins[0] == 0) {
-                dp[0][j] = 1;
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            int v = coins[i];
-            for (int j = 0; j <= amount; j++) {
-                if (j >= v) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - v];
+        // iterate
+        for(int i=1;i<=amount;i++) {
+            for(int j=1;j<=n;j++) {
+                int v = coins[j-1];
+                if(i>=v) {
+                    dp[i][j] = dp[i-v][j] + dp[i][j-1];
                 } else {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[i][j] = dp[i][j-1];
                 }
             }
         }
-        return dp[n - 1][amount];
+
+        return dp[amount][n];
     }
+
 }
