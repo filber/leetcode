64f17a3f6f56610baec988d50e4b9e9c3b912275
da.li <da.li@bytedance.com>
2024-03-17 21:26:06 -0700

    57. Insert Interval

--- problems/src/greedy/_57_InsertInterval.java
+++ problems/src/greedy/_57_InsertInterval.java
@@ -2,116 +2,85 @@ package greedy;
 
 //https://leetcode.com/problems/insert-interval/
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class _57_InsertInterval {
 
-    int[][] intervals;
-
-    public int[][] insertDQ(int[][] intervals, int[] newInterval) {
-        this.intervals = intervals;
-        int n = intervals.length;
-        if (n == 0) {
-            return new int[][]{newInterval};
-        }
-        int[] firstR = intervals[0];
-        int[] lastR = intervals[n - 1];
-        int S = newInterval[0], E = newInterval[1];
-        int[][] ans = null;
-        if (E < firstR[0]) {
-            ans = new int[n + 1][2];
-            ans[0][0] = S;
-            ans[0][1] = E;
-            fill(ans, 1, intervals, 0, n);
-        } else if (lastR[1] < S) {
-            ans = new int[n + 1][2];
-            fill(ans, 0, intervals, 0, n);
-            ans[n][0] = S;
-            ans[n][1] = E;
-        } else if (S <= firstR[0] && lastR[1] <= E) {
-            ans = new int[][]{{S, E}};
-        } else if (S <= firstR[0]) {
-            int[] interval = search(E, 0, n - 1);
-            int L = interval[0];
-
-            ans = new int[n - L][2];
-            fill(ans, 1, intervals, L + 1, n - L - 1);
-            ans[0][0] = S;
-            ans[0][1] = Math.max(intervals[L][1], E);
-        } else if (lastR[1] <= E) {
-            int[] interval = search(S, 0, n - 1);
-            int R = interval[1];
-            ans = new int[R + 1][2];
-            fill(ans, 0, intervals, 0, R);
-            ans[R][0] = Math.min(S, intervals[R][0]);
-            ans[R][1] = E;
-        } else {
-            int[] sInter = search(S, 0, n - 1);
-            int rS = sInter[1];
-            int[] eInter = search(E, 0, n - 1);
-            int lE = eInter[0];
-
-            ans = new int[rS + n - lE][2];
-            fill(ans, 0, intervals, 0, rS);
-            ans[rS][0] = Math.min(S, intervals[rS][0]);
-            ans[rS][1] = Math.max(E, intervals[lE][1]);
-            fill(ans, rS + 1, intervals, lE + 1, n - lE - 1);
-        }
-        return ans;
-    }
-
-    private void fill(int[][] target, int tS, int[][] from, int fS, int len) {
-        for (int i = 0; i < len; i++) {
-            target[tS + i] = from[fS + i];
-        }
-    }
-
-    public int[] search(int val, int l, int r) {
-        if (l == r) {
-            int[] range = intervals[l];
-            if (range[0] <= val && val <= range[1]) {
-                return new int[]{l, l};
-            } else if (val < range[0]) {
-                return new int[]{l - 1, l};
-            } else {
-                return new int[]{l, l + 1};
-            }
-        } else {
-            int m = (l + r) / 2;
-            int[] range = intervals[m];
-            if (range[0] <= val && val <= range[1]) {
-                return new int[]{m, m};
-            } else if (val < range[0]) {
-                return search(val, Math.min(l, m - 1), m - 1);
-            } else {
-                return search(val, m + 1, Math.max(r, m + 1));
-            }
-        }
-    }
-
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>();
-        int i = 0;
-        int n = intervals.length;
-        int S = newInterval[0], E = newInterval[1];
-        while (i < n && intervals[i][1] < S) {
-            list.add(intervals[i]);
-            i++;
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int[][] arr = new int[intervals.length + 1][0];
+        int j = 0;
+        boolean discard = false;
+        for (int i = 0; i < intervals.length; i++) {
+            int[] inter = intervals[i];
+            if (discard) {
+                arr[j++] = inter;
+                continue;
+            }
+
+            if (inter[0] < start) {
+                /**
+                 s       e
+                 i[0]i[1]        append<i>, process <s,e> later
+                 i[0].   i[1]    s' = i[0], e' = e
+                 i[0].       i[1].     s' = i[0], e' = e
+                 i[0].            i[1] s' = i[0], e' = e
+                 i[0].                i[1], append<i[0],i[1]>, discard <s,e>
+                 */
+                if (inter[1] < start) {
+                    arr[j++] = inter;
+                } else if (inter[1] == start) {
+                    start = inter[0];
+                } else if (start < inter[1] && inter[1] < end) {
+                    start = inter[0];
+                } else if (end == inter[1]) {
+                    start = inter[0];
+                } else {
+                    arr[j++] = inter;
+                    discard = true;
+                }
+            } else if (inter[0] == start) {
+                /**
+                 s       e
+                 i[0]i[1]          ignore i
+                 i[0].  i[1]       ignore i
+                 i[0].      i[1]   append<s,i[1]>, discard <s,e>
+                 */
+                if (end < inter[1]) {
+                    arr[j++] = inter;
+                    discard = true;
+                }
+            } else if (start < inter[0] && inter[0] < end) {
+                /**
+                 s          e
+                 i[0]i[1]        ignore i
+                 i[0].  i[1]     ignore i
+                 i[0].       i[1]  e' = i[1]
+                 */
+                end = Math.max(end, inter[1]);
+            } else if (inter[0] == end) {
+                /**
+                 s       e
+                 i[0]i[1]    e' = [1]
+                 */
+                end = Math.max(end, inter[1]);
+            } else if (end < inter[0]) {
+                /**
+                 s       e
+                 i[0]i[1] append<s,e> append<i>
+                 discard <s,e>
+                 */
+                arr[j++] = new int[]{start, end};
+                arr[j++] = inter;
+                discard = true;
+            }
         }
 
-        while (i < n && intervals[i][0] <= E) {
-            S = Math.min(S, intervals[i][0]);
-            E = Math.max(E, intervals[i][1]);
-            i++;
+        // if <s,e> was not discarded, append <s,e>
+        if (!discard) {
+            arr[j++] = new int[]{start, end};
         }
-        list.add(new int[]{S, E});
-
-        while (i < n) {
-            list.add(intervals[i]);
-            i++;
-        }
-
-        return list.toArray(new int[list.size()][2]);
+        return Arrays.copyOf(arr, j);
     }
 }
